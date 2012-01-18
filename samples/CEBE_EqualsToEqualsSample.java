9ed310e1ecbeafb58d2cabddd5f0d50a709900f9
Bhaskar Maddala <bhaskarm@gmail.com>
2012-01-18 10:20:30 -0800

    Added a new detector for detecting incorrect usage of jakarta-commons equals builder

---
+++ samples/CEBE_EqualsToEqualsSample.java
@@ -0,0 +1,31 @@
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+public class CEBE_EqualsToEqualsSample {
+    public final String name;
+    public final int age;
+
+    CEBE_EqualsToEqualsSample(String name, int age) {
+	this.name = name;
+	this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+	return new HashCodeBuilder().append(name).append(age).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	CEBE_EqualsToEqualsSample other = (CEBE_EqualsToEqualsSample) obj;
+	return new EqualsBuilder().append(this.name, other.name)
+		.append(this.age, other.age).equals(obj);
+    }
+
+}
---
+++ src/com/mebigfatguy/fbcontrib/detect/CommonsEqualsBuilderToEquals.java
@@ -0,0 +1,79 @@
+package com.mebigfatguy.fbcontrib.detect;
+
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.LocalVariableTable;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.OpcodeStack;
+
+/**
+ * Find usage of EqualsBuilder from Apache commons, where the code invoke
+ * equals() on the constructed object rather than toEquals()
+ * 
+ * <pre>
+ * new EqualsBuilder().append(this.name, other.name).equals(other);
+ * </pre>
+ */
+public class CommonsEqualsBuilderToEquals extends BytecodeScanningDetector {
+
+    private final OpcodeStack stack;
+    private final BugReporter bugReporter;
+
+    /**
+     * constructs a CEBE detector given the reporter to report bugs on.
+     * 
+     * @param bugReporter
+     *            the sync of bug reports
+     */
+    public CommonsEqualsBuilderToEquals(final BugReporter bugReporter) {
+	stack = new OpcodeStack();
+	this.bugReporter = bugReporter;
+    }
+
+    /**
+     * implements the visitor to pass through constructors and static
+     * initializers to the byte code scanning code. These methods are not
+     * reported, but are used to build SourceLineAnnotations for fields, if
+     * accessed.
+     * 
+     * @param obj
+     *            the context object of the currently parsed code attribute
+     */
+    @Override
+    public void visitCode(Code obj) {
+	stack.resetForMethodEntry(this);
+	LocalVariableTable lvt = getMethod().getLocalVariableTable();
+	if (lvt != null) {
+	    super.visitCode(obj);
+	}
+    }
+
+    @Override
+    public void sawOpcode(int seen) {
+	try {
+	    switch (seen) {
+	    case INVOKEVIRTUAL:
+		String methodName = getNameConstantOperand();
+		if ("equals".equals(methodName)
+			&& "(Ljava/lang/Object;)Z"
+				.equals(getSigConstantOperand())) {
+		    String calledClass = stack.getStackItem(1).getSignature();
+		    if ("Lorg/apache/commons/lang3/builder/EqualsBuilder;"
+			    .equals(calledClass)
+			    || "org/apache/commons/lang/builder/EqualsBuilder"
+				    .equals(calledClass)) {
+			bugReporter.reportBug(new BugInstance(this,
+				"CEBE_COMMONS_EQUALS_BUILDER_EQUALS",
+				HIGH_PRIORITY).addClass(this).addMethod(this)
+				.addSourceLine(this));
+		    }
+		}
+	    }
+	} finally {
+	    super.sawOpcode(seen);
+	    stack.sawOpcode(this, seen);
+	}
+    }
+}
