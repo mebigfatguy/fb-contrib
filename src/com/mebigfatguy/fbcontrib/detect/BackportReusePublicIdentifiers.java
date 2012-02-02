4440d54408b027d238bdeca7fcbd4d260c96de23
Dave Brosius <dbrosius@mebigfatguy.com>
2012-02-02 12:41:43 -0500

    only run BackportReusePublicIdentifiers if code base is 1.5 or better

--- src/com/mebigfatguy/fbcontrib/detect/BackportReusePublicIdentifiers.java
+++ src/com/mebigfatguy/fbcontrib/detect/BackportReusePublicIdentifiers.java
@@ -1,7 +1,30 @@
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2012 Bhaskar Maddala
+ * Copyright (C) 2005-2012 Dave Brosius
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.mebigfatguy.fbcontrib.detect;
+
+import org.apache.bcel.Constants;
+import org.apache.bcel.classfile.JavaClass;
 
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 
 /**
@@ -17,6 +40,14 @@ public class BackportReusePublicIdentifiers extends OpcodeStackDetector {
 
     public BackportReusePublicIdentifiers(final BugReporter bugReporter) {
         this.bugReporter = bugReporter;
+    }
+    
+    @Override
+    public void visitClassContext(ClassContext classContext) {
+        JavaClass cls = classContext.getJavaClass();
+        if (cls.getMajor() >= Constants.MAJOR_1_5) {
+            super.visitClassContext(classContext);
+        }
     }
 
     @Override
