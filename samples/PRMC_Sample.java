9cf71782a48aeee5c534820dd0f7c2a0e30cabc0
Dave Brosius <dbrosius@mebigfatguy.com>
2012-09-25 00:18:56 -0400

    more tests

--- samples/PRMC_Sample.java
+++ samples/PRMC_Sample.java
@@ -1,12 +1,14 @@
 import java.nio.ByteBuffer;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @SuppressWarnings("all")
-public class PRMC_Sample 
+public class PRMC_Sample
 {
 	String data;
-	
+
 	public boolean test1(Calendar c)
 	{
 		Date d = c.getTime();
@@ -15,23 +17,23 @@ public class PRMC_Sample
 		long j = e.getTime();
 		return l == j;
 	}
-    
+
     public void rmcFP(ByteBuffer bb)
     {
         int i = bb.getInt();
         int j = bb.getInt();
     }
-    
+
     @Override
     public boolean equals(Object o)
     {
     	PRMC_Sample rmc = (PRMC_Sample)o;
     	if (data.equals("INF") || rmc.data.equals("INF"))
     		return false;
-    	
+
     	return data.equals(rmc.data);
     }
-    
+
     public void staticPRMC()
     {
     	Factory.getInstance().fee();
@@ -39,43 +41,55 @@ public class PRMC_Sample
     	Factory.getInstance().fo();
     	Factory.getInstance().fum();
     }
-    
+
     static class Factory
     {
     	private static Factory f = new Factory();
-    	
+
     	private Factory()
     	{
     	}
-    	
+
     	public static Factory getInstance()
     	{
     		return f;
     	}
-    	
+
     	public void fee()
     	{
     	}
-    	
+
     	public void fi()
     	{
     	}
-    	
+
     	public void fo()
     	{
     	}
-    	
+
     	public void fum()
     	{
     	}
     }
-    
+
     public long fpCurrentTimeMillis(Object o)
     {
     	long time = -System.currentTimeMillis();
     	o.hashCode();
     	time += System.currentTimeMillis();
-    	
+
     	return time;
     }
+
+    public void fpEnumToString(FPEnum e)
+    {
+        Set<String> s = new HashSet<String>();
+
+        s.add(FPEnum.fee.toString());
+        s.add(FPEnum.fi.toString());
+        s.add(FPEnum.fo.toString());
+        s.add(FPEnum.fum.toString());
+    }
+
+    enum FPEnum { fee, fi, fo, fum };
 }
