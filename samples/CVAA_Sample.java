f3fca64838fb483937c417664638cbd05525ad22
Dave Brosius <dbrosius@mebigfatguy.com>
2012-10-25 23:24:47 -0400

    add fp

--- samples/CVAA_Sample.java
+++ samples/CVAA_Sample.java
@@ -1,13 +1,15 @@
+import java.util.Set;
+
 
 public class CVAA_Sample
 {
 	Base[] b2;
 	class Base {}
-	
+
 	class Derived extends Base {}
-	
+
 	public void cvaa()
-	{		
+	{
 		Derived[] d2 = new Derived[4];
 		Base[] b = d2;
 		b2 = d2;
@@ -16,12 +18,16 @@ public class CVAA_Sample
 		b[1] = doDerived();
 		b[2] = null;
 		b[3] = d;
-		
+
 		Integer[] a = new Integer[1];
 		System.arraycopy(a[0], 0, a, 1, 1);
 	}
-    
+
 	private Derived doDerived(){
 		return new Derived();
+	}
+
+	public String fpVarArgs(String s1, Set<String> s2) {
+	    return String.format("s1=%s s2=%s", s1, s2);
 	}
 }
