ba7825d0cdd4871378d8d50b9b3f18e3adcbe109
Dave Brosius <dbrosius@mebigfatguy.com>
2012-11-03 17:16:16 -0400

    warnings

--- samples/UVA_Sample.java
+++ samples/UVA_Sample.java
@@ -4,35 +4,37 @@ import java.util.Date;
 
 public class UVA_Sample<T> extends ArrayList<T>
 {
-	public void testNormalUVA(String[] foo)
+	private static final long serialVersionUID = -7860932685876390245L;
+
+    public void testNormalUVA(String[] foo)
 	{}
-	
+
 	public void testLowUVA1(int boo, String[] hoo)
 	{}
-	
+
 	public static void testStaticUVA(Date[] d)
 	{}
-	
+
 	public void fpNoParms()
 	{}
-	
+
 	public void fpHasOtherArrayUVA1(String[] one, int[] two)
 	{}
-	
+
 	public void fpTooManyArgs(int i, char j, long k, String[] moo)
 	{}
-	
+
 	public void fpNotAtEnd(String[] foo, int bar)
 	{}
-	
+
 	public void fpAlreadyVarArg(String...darnit)
 	{}
-	
+
 	public void fpSimilarVarArg(String info, String...data)
 	{}
-	
+
 	@Override
-	public <T> T[] toArray(T[] a)
+	public <E> E[] toArray(E[] a)
 	{
 		return null;
 	}
