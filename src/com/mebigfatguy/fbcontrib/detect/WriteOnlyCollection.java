b5c37f3d8641bf45777c74ba3c00cf9bc27bed23
Dave Brosius <dbrosius@mebigfatguy.com>
2012-12-26 14:44:46 -0500

    revert iterator being writable

--- src/com/mebigfatguy/fbcontrib/detect/WriteOnlyCollection.java
+++ src/com/mebigfatguy/fbcontrib/detect/WriteOnlyCollection.java
@@ -84,7 +84,6 @@ public class WriteOnlyCollection extends BytecodeScanningDetector {
 		writeMethods.add("clone");
 		writeMethods.add("ensureCapacity");
 		writeMethods.add("insertElementAt");
-		writeMethods.add("iterator");
 		writeMethods.add("put");
 		writeMethods.add("putAll");
 		writeMethods.add("remove");
