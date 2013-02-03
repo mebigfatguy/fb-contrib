dce90626cc6a79180b85424da78c2c641879dc4d
Dave Brosius <dbrosius@mebigfatguy.com>
2013-02-03 15:46:06 -0500

    more tests

--- samples/STB_Sample.java
+++ samples/STB_Sample.java
@@ -18,6 +18,38 @@ public class STB_Sample {
 		}
 	}
 
+   public void testSTB2(File f1, File f2) throws STBException {
+        try {
+            InputStream is = new FileInputStream(f1);
+        } catch (IOException ioe) {
+            throw new STBException("It's broken");
+        }
+
+        try {
+            InputStream is = new FileInputStream(f2);
+        } catch (IOException ioe) {
+            throw new STBException("It's broken");
+        }
+    }
+
+   public void testSTB3(File f1, File f2) throws STBException {
+       try {
+           InputStream is = new FileInputStream(f1);
+       } catch (IOException ioe) {
+           STBException se = new STBException("It's broken");
+           se.initCause(ioe);
+           throw se;
+       }
+
+       try {
+           InputStream is = new FileInputStream(f2);
+       } catch (IOException ioe) {
+           STBException se = new STBException("It's broken");
+           se.initCause(ioe);
+           throw se;
+       }
+   }
+
 	public void fpTestMethodDeclaresThrownType(File f1, File f2) throws STBException, IOException {
 		try {
 			InputStream is = new FileInputStream(f1);
@@ -31,7 +63,7 @@ public class STB_Sample {
 			throw new STBException();
 		}
 	}
-	
+
 	public void fpTestDiffMessages(File f1, File f2) throws STBException {
        try {
             InputStream is = new FileInputStream(f1);
@@ -45,7 +77,7 @@ public class STB_Sample {
             throw new STBException("Couldn't open file 2");
         }
 	}
-	
+
 	   public void fpTestDiffMessagesByAppending(File f1, File f2) throws STBException {
 	       try {
 	            InputStream is = new FileInputStream(f1);
@@ -61,10 +93,10 @@ public class STB_Sample {
 	    }
 
 	static class STBException extends Exception {
-	    
+
 	    public STBException() {
 	    }
-	    
+
 	    public STBException(String message) {
 	        super(message);
 	    }
