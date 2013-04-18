00365152c9b6decec99f09f43f73633c230829d6
Dave Brosius <dbrosius@mebigfatguy.com>
2013-04-18 14:15:26 -0400

    exclude com.sun.xsom from IICU as per SF Bug: 3611273

--- src/com/mebigfatguy/fbcontrib/detect/IncorrectInternalClassUse.java
+++ src/com/mebigfatguy/fbcontrib/detect/IncorrectInternalClassUse.java
@@ -44,6 +44,7 @@ public class IncorrectInternalClassUse implements Detector
         internalPackages.add("org/apache/xerces/");
         internalPackages.add("org/apache/xalan/");
         externalPackages.add("com/sun/jersey");
+        externalPackages.add("com/sun/xsom");
         externalPackages.add("org/apache/xerces/xni/");
         externalPackages.add("org/apache/xerces/xs/");
         externalPackages.add("org/apache/xalan/extensions");
