7ce3f76464954600f265821c7beec22703cb838e
Dave Brosius <dbrosius@mebigfatguy.com>
2013-05-28 21:46:37 -0400

    make sure the stack has items

--- src/com/mebigfatguy/fbcontrib/detect/LoggerOddities.java
+++ src/com/mebigfatguy/fbcontrib/detect/LoggerOddities.java
@@ -267,12 +267,14 @@ public class LoggerOddities extends BytecodeScanningDetector {
                 				}
                 			}
                      } else if ("(Ljava/lang/Object;)V".equals(sig)) {
-                        final JavaClass clazz = stack.getStackItem(0).getJavaClass();
-                        if(clazz.instanceOf(THROWABLE_CLASS)) {
-                           bugReporter.reportBug(new BugInstance(this, "LO_LOGGER_LOST_EXCEPTION_STACK_TRACE", NORMAL_PRIORITY)
-                                 .addClass(this)
-                                 .addMethod(this)
-                                 .addSourceLine(this));
+                        if (stack.getStackDepth() > 0) {
+                            final JavaClass clazz = stack.getStackItem(0).getJavaClass();
+                            if(clazz.instanceOf(THROWABLE_CLASS)) {
+                               bugReporter.reportBug(new BugInstance(this, "LO_LOGGER_LOST_EXCEPTION_STACK_TRACE", NORMAL_PRIORITY)
+                                     .addClass(this)
+                                     .addMethod(this)
+                                     .addSourceLine(this));
+                            }
                         }
                      }
                 	}
