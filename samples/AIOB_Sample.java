f3657241eac8ac6c6b2ac59c64e9ccc714227ca9
Dave Brosius <dbrosius@mebigfatguy.com>
2013-06-27 00:36:38 -0400

    more AIOB tests

--- samples/AIOB_Sample.java
+++ samples/AIOB_Sample.java
@@ -1,10 +1,22 @@
 
 public class AIOB_Sample 
 {
-    public void testAOB()
+    int[] fa = new int[4];
+    int[] fb;
+    
+    public void testOutOfBounds()
     {
         int[] a = new int[4];
         
         a[4] = 2;
+        fa[4] = 2;
+    }
+    
+    public void testUnallocated()
+    {
+        int[] b = null;
+        
+        b[4] = 4;
+        fb[4] = 4;
     }
 }
