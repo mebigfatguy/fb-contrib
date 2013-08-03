7d80ff0b6465296d6213f56a8386f89bf75305e7
Dave Brosius <dbrosius@mebigfatguy.com>
2013-08-02 23:02:17 -0400

    fix fp with LO_INCORRECT_NUMBER_OF_ANCHOR_PARAMETERS

--- src/com/mebigfatguy/fbcontrib/detect/LoggerOddities.java
+++ src/com/mebigfatguy/fbcontrib/detect/LoggerOddities.java
@@ -292,7 +292,7 @@ public class LoggerOddities extends BytecodeScanningDetector {
                                             if (actualParms != -1) {
                                                 int expectedParms = countAnchors((String) con);
                                                 boolean hasEx = hasExceptionOnStack();
-                                                if ((expectedParms != actualParms) || (hasEx && (expectedParms != (actualParms - 1)))) {
+                                                if ((!hasEx && (expectedParms != actualParms)) || (hasEx && ((expectedParms != (actualParms - 1)) && (expectedParms != actualParms)))) {
                                                     bugReporter.reportBug(new BugInstance(this, "LO_INCORRECT_NUMBER_OF_ANCHOR_PARAMETERS", NORMAL_PRIORITY)
                                                             .addClass(this)
                                                             .addMethod(this)
