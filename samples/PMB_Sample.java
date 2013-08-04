0bc096b751111f277799f8c29aa9f41778b9a0e0
Dave Brosius <dbrosius@mebigfatguy.com>
2013-08-03 22:59:43 -0400

    add fp for PMB

--- samples/PMB_Sample.java
+++ samples/PMB_Sample.java
@@ -7,7 +7,7 @@ public class PMB_Sample
 {
 	private static Set<String> bl_data = new HashSet<String>();
 	private static List<String> data = new ArrayList<String>();
-	
+    private static Set<String> inner_data = new HashSet<String>();
 	public void add(String s)
 	{
 		bl_data.add(s);
@@ -17,5 +17,15 @@ public class PMB_Sample
 	public void remove(String s)
 	{
 		data.remove(s);
+	}
+	
+	public void fpInnerDoesRemove() {
+	    inner_data.add("Hello");
+	    Runnable r = new Runnable() {
+	        public void run() {
+	            inner_data.remove("Hello");
+	        }
+	    };
+	    r.run();
 	}
 }
