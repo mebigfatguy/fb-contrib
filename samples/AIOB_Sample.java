3780fb65aafc6dbe3bc316f117687aeb516321f2
Dave Brosius <dbrosius@mebigfatguy.com>
2013-09-02 13:54:33 -0400

    add fp for AIOB

--- samples/AIOB_Sample.java
+++ samples/AIOB_Sample.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 
 public class AIOB_Sample 
 {
@@ -18,5 +20,18 @@ public class AIOB_Sample
         
         b[4] = 4;
         fb[4] = 4;
+    }
+    
+    public int[] fpPlusPlusNotRecognized(List<String> l) {
+        int size = 0;
+        
+        for (String s : l) {
+            size++;
+        }
+        
+        int[] data = new int[size];
+        
+        data[0] = 0;
+        return data;
     }
 }
