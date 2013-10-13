0b0451d330a7534062c501cc49f67f6d28ccc3ce
Dave Brosius <dbrosius@mebigfatguy.com>
2013-10-13 17:38:38 -0400

    add fp for WOC

--- samples/WOC_Sample.java
+++ samples/WOC_Sample.java
@@ -11,106 +11,111 @@ import java.util.Map;
 import java.util.Set;
 import java.util.Vector;
 
+public class WOC_Sample {
+    private final Set<String> memberSet = new HashSet<String>();
+    private Set<String> fpSet;
+    private final List<String> fpList = new ArrayList<String>();
 
-public class WOC_Sample
-{
-	private final Set<String> memberSet = new HashSet<String>();
-	private Set<String> fpSet;
-	private final List<String> fpList = new ArrayList<String>();
+    public void testWOCSimple() {
+        Set<String> s = new HashSet<String>();
+        s.add("Foo");
+        memberSet.add("fee");
+        if (fpSet.retainAll(new HashSet<String>())) {
+            System.out.println("woops");
+        }
+    }
 
-	public void testWOCSimple()
-	{
-		Set<String> s = new HashSet<String>();
-		s.add("Foo");
-		memberSet.add("fee");
-		if (fpSet.retainAll(new HashSet<String>())) {
-			System.out.println("woops");
-		}
-	}
+    public Map<String, String> testFPWOCReturn() {
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("Foo", "Bar");
+        memberSet.add("fi");
+        fpSet = new HashSet<String>();
+        return m;
+    }
 
-	public Map<String, String> testFPWOCReturn()
-	{
-		Map<String, String> m = new HashMap<String, String>();
-		m.put("Foo", "Bar");
-		memberSet.add("fi");
-		fpSet = new HashSet<String>();
-		return m;
-	}
+    public void testFPWOCAsParm() {
+        Map<String, String> m = new HashMap<String, String>();
+        m.put("Foo", "Bar");
+        memberSet.add("fo");
+        fpSet.add("boo");
+        helper(0, m);
+    }
 
-	public void testFPWOCAsParm()
-	{
-		Map<String, String> m = new HashMap<String, String>();
-		m.put("Foo", "Bar");
-		memberSet.add("fo");
-		fpSet.add("boo");
-		helper(0, m);
-	}
+    public void testFPWOCCopy() {
+        Set<String> s = new LinkedHashSet<String>();
+        s.add("foo");
+        Set<String> c = s;
+        memberSet.add("fum");
+    }
 
-	public void testFPWOCCopy()
-	{
-		Set<String> s = new LinkedHashSet<String>();
-		s.add("foo");
-		Set<String> c = s;
-		memberSet.add("fum");
-	}
+    public void testFPWOCInArray() {
+        Vector<Integer> v = new Vector<Integer>();
+        v.addElement(Integer.valueOf(0));
+        Object[] o = new Object[] { v };
+    }
 
-	public void testFPWOCInArray()
-	{
-		Vector<Integer> v = new Vector<Integer>();
-		v.addElement(Integer.valueOf(0));
-		Object[] o = new Object[] { v };
-	}
+    public void testFPWOCUseReturnVal() {
+        LinkedList<String> l = new LinkedList<String>();
+        l.add("Foo");
+        l.add("Bar");
 
-	public void testFPWOCUseReturnVal()
-	{
-		LinkedList<String> l = new LinkedList<String>();
-		l.add("Foo");
-		l.add("Bar");
+        if (l.remove("Foo")) {
+            System.out.println("Dont' report");
+        }
+    }
 
-		if (l.remove("Foo"))
-		{
-			System.out.println("Dont' report");
-		}
-	}
+    public Set<String> testFPTrinary(boolean b) {
+        Set<String> s = new HashSet<String>();
+        s.add("foo");
+        s.add("bar");
 
-	public Set<String> testFPTrinary(boolean b)
-	{
-		Set<String> s = new HashSet<String>();
-		s.add("foo");
-		s.add("bar");
+        return b ? s : Collections.<String> emptySet();
+    }
 
-		return b ? s : Collections.<String>emptySet();
-	}
+    private void helper(int i, Map<String, String> x) {
+    }
 
-	private void helper(int i, Map<String, String> x)
-	{
-	}
+    public void testFPInnerClass(final Set<String> data) {
+        ActionListener al = new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                data.add("Woot");
+            }
+        };
+    }
 
-	public void testFPInnerClass(final Set<String> data)
-	{
-		ActionListener al = new ActionListener()
-		{
-			public void actionPerformed(ActionEvent ae)
-			{
-				data.add("Woot");
-			}
-		};
-	}
+    public List<String> fpOtherInstance(WOC_Sample ws) {
+        return ws.fpList;
+    }
+    
+    public String fpCheckReference(boolean b) {
+        List<String> s = null;
+        
+        if (b) {
+            s = new ArrayList<String>();
+            s.add("foo");
+        }
+        
+        String result;
+        if (s != null) {
+            result = "yes";
+        } else {
+            result = "no";
+        }
+        
+        return result;
+    }
 
-	public List<String> fpOtherInstance(WOC_Sample ws)
-	{
-		return ws.fpList;
-	}
-
-   public static class FpContains {
+    public static class FpContains {
         private List<String> fpSet;
 
         public FpContains() {
             fpSet = new ArrayList<String>();
         }
+
         public void add() {
             fpSet.add("Foo");
         }
+
         protected void contains() {
             for (int i = 0; i < 10; i++) {
                 if (fpSet.get(i) != null) {
