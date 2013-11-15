b89ab86c099aa51893d96a229f24ef1e5bfc9c72
Dave Brosius <dbrosius@mebigfatguy.com>
2013-11-14 22:05:03 -0500

    add sample for Invalid Constant Argument

---
+++ samples/ICA_Sample.java
@@ -0,0 +1,12 @@
+import java.awt.image.ImageObserver;
+
+import javax.swing.JOptionPane;
+
+
+public class ICA_Sample {
+
+    public void testJOptionPane() 
+    {
+        JOptionPane.showMessageDialog(null, "hi", "there", ImageObserver.ERROR);
+    }
+}
