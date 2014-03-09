3ed8db7b1903d7e1a85252a503901dcbb72a2a83
Dave Brosius <dbrosius@mebigfatguy.com>
2014-03-09 15:00:48 -0400

    stub in a nonworking initial MUC version

--- src/com/mebigfatguy/fbcontrib/detect/ModifyingUnmodifiableCollection.java
+++ src/com/mebigfatguy/fbcontrib/detect/ModifyingUnmodifiableCollection.java
@@ -18,12 +18,18 @@
  */
 package com.mebigfatguy.fbcontrib.detect;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
 
 import com.mebigfatguy.fbcontrib.collect.ImmutabilityType;
 import com.mebigfatguy.fbcontrib.collect.MethodInfo;
 import com.mebigfatguy.fbcontrib.collect.Statistics;
 
+import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.OpcodeStack;
@@ -33,8 +39,33 @@ import edu.umd.cs.findbugs.ba.ClassContext;
 @CustomUserValue
 public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
 
+    private static Set<String> MODIFYING_METHODS = null;
+    private static JavaClass LIST_CLASS = null;
+    private static JavaClass SET_CLASS = null;
+    private static JavaClass MAP_CLASS = null;
+    
+    static {
+        try {
+            MODIFYING_METHODS = new HashSet<String>();
+            MODIFYING_METHODS.add("add(Ljava/lang/Object;)Z");
+            MODIFYING_METHODS.add("remove(Ljava/lang/Object;)Z");
+            MODIFYING_METHODS.add("addAll(Ljava/util/Collection;)Z");
+            MODIFYING_METHODS.add("retainAll(Ljava/util/Collection;)Z");
+            MODIFYING_METHODS.add("removeAll(Ljava/util/Collection;)Z");
+            MODIFYING_METHODS.add("put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;");
+            MODIFYING_METHODS.add("remove(Ljava/lang/Object;)Ljava/lang/Object;");
+            MODIFYING_METHODS.add("putAll(Ljava/util/Map;)V;");
+            
+            LIST_CLASS = Repository.lookupClass("java.util.List");
+            SET_CLASS = Repository.lookupClass("java.util.Set");
+            MAP_CLASS = Repository.lookupClass("java.util.Map");
+        } catch (ClassNotFoundException cnfe) {
+        }
+    }
+    
     private BugReporter bugReporter;
     private OpcodeStack stack;
+    private ImmutabilityType reportedType;
     
     public ModifyingUnmodifiableCollection(BugReporter reporter) {
         bugReporter = reporter;
@@ -53,9 +84,15 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
     @Override
     public void visitCode(Code obj) {
         stack.resetForMethodEntry(this);
+        reportedType = ImmutabilityType.UNKNOWN;
+        super.visitCode(obj);
     }
     
     public void sawOpcode(int seen) {
+        
+        if (reportedType == ImmutabilityType.IMMUTABLE) {
+            return;
+        }
         ImmutabilityType imType = null;
 
         try {
@@ -72,6 +109,21 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
                     
                     MethodInfo mi = Statistics.getStatistics().getMethodStatistics(className, methodName, signature);
                     imType = mi.getImmutabilityType();
+                    
+                    if ((seen == INVOKEINTERFACE) && MODIFYING_METHODS.contains(methodName + signature) && (isCollection(className))) {
+                        if (stack.getStackDepth() > 0) {
+                            OpcodeStack.Item item = stack.getStackItem(0);
+                            ImmutabilityType type = (ImmutabilityType) item.getUserValue();
+                            
+                            if ((type == ImmutabilityType.IMMUTABLE) || ((type == ImmutabilityType.POSSIBLY_IMMUTABLE) && (reportedType != ImmutabilityType.POSSIBLY_IMMUTABLE))) {
+                                bugReporter.reportBug(new BugInstance(this, "MUC_MODIFYING_UNMODIFIABLE_COLLECTION", (type == ImmutabilityType.IMMUTABLE) ? HIGH_PRIORITY : NORMAL_PRIORITY)
+                                                          .addClass(this)
+                                                          .addMethod(this)
+                                                          .addSourceLine(this));
+                                reportedType = type;
+                            }
+                        }
+                    }
                 }
                 break;
             }
@@ -85,6 +137,17 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
                 }
             }
             
+        }
+    }
+    
+    private boolean isCollection(String clsName) {
+        
+        try {
+            JavaClass cls = Repository.lookupClass(clsName);
+            return (cls.implementationOf(LIST_CLASS) || cls.implementationOf(SET_CLASS) || cls.implementationOf(MAP_CLASS));
+        } catch (ClassNotFoundException cnfe) {
+            bugReporter.reportMissingClass(cnfe);
+            return false;
         }
     }
 }
