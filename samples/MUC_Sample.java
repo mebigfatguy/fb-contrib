d4c04c58e5d0c8b99975c7bb54574db35181d2f9
Dave Brosius <dbrosius@mebigfatguy.com>
2014-03-09 20:55:36 -0400

    add test for Possibly Immutable

--- samples/MUC_Sample.java
+++ samples/MUC_Sample.java
@@ -1,5 +1,9 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import edu.emory.mathcs.backport.java.util.Collections;
 
 
 public class MUC_Sample {
@@ -10,7 +14,22 @@ public class MUC_Sample {
         l.add("Uhoh");
     }
     
+    public void testPossiblyMUC() {
+        Set<String> s = getPossiblyImmutableSet(Math.random() > 0.5);
+        
+        s.add("Yowsers");
+    }
+    
     public List<String> getImmutableList() {
         return Arrays.asList("foo");
     }
+    
+    public Set<String> getPossiblyImmutableSet(boolean b) {
+        if (b)
+            return Collections.unmodifiableSet(new HashSet<String>());
+        
+        return new HashSet<String>();
+    }
+    
+    
 }
