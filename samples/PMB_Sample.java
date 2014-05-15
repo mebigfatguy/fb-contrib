93cdd96a5e63521e0ebe400c2d00aabe17e09999
Kevin Lubick <kjlubick@ncsu.edu>
2014-05-14 21:29:10 -0400

    Unnecessary Annotation
    Both of the addFields() work the same way.
    
    Also, changed reports to tag, to be consistent elsewhere.
    
    Conflicts:
    	samples/PMB_Sample.java

--- samples/PMB_Sample.java
+++ samples/PMB_Sample.java
@@ -6,11 +6,56 @@ import java.util.List;
 import java.util.Set;
 
 public class PMB_Sample {
-    private static Set<String> bl_data = new HashSet<String>();
-    private static List<String> data = new ArrayList<String>();
-    private static Set<String> inner_data = new HashSet<String>();
+    private static Set<String> bl_data = new HashSet<String>();		//tag
+    private static List<String> data = new ArrayList<String>();		//no tag
+    private static Set<String> inner_data = new HashSet<String>();	//no tag
+    private static StringBuilder return_data = new StringBuilder();	//no tag
+    
+    private static final Set<String> bloatableSigs = new HashSet<String>();
+	static {
+		bloatableSigs.add("Ljava/util/concurrent/ArrayBlockingQueue;");
+		bloatableSigs.add("Ljava/util/ArrayList;");
+		bloatableSigs.add("Ljava/util/concurrent/BlockingQueue;");		
+		bloatableSigs.add("Ljava/util/Collection;");
+		bloatableSigs.add("Ljava/util/concurrent/ConcurrentHashMap;");
+		bloatableSigs.add("Ljava/util/concurrent/ConcurrentSkipListMap;");
+		bloatableSigs.add("Ljava/util/concurrent/ConcurrentSkipListSet;");
+		bloatableSigs.add("Ljava/util/concurrent/CopyOnWriteArraySet;");
+		bloatableSigs.add("Ljava/util/EnumSet;");
+		bloatableSigs.add("Ljava/util/EnumMap;");
+		bloatableSigs.add("Ljava/util/HashMap;");
+		bloatableSigs.add("Ljava/util/HashSet;");
+		bloatableSigs.add("Ljava/util/Hashtable;");
+		bloatableSigs.add("Ljava/util/IdentityHashMap;");
+		bloatableSigs.add("Ljava/util/concurrent/LinkedBlockingQueue;");
+		bloatableSigs.add("Ljava/util/LinkedHashMap;");
+		bloatableSigs.add("Ljava/util/LinkedHashSet;");
+		bloatableSigs.add("Ljava/util/LinkedList;");
+		bloatableSigs.add("Ljava/util/List;");
+		bloatableSigs.add("Ljava/util/concurrent/PriorityBlockingQueue;");
+		bloatableSigs.add("Ljava/util/PriorityQueue;");
+		bloatableSigs.add("Ljava/util/Map;");
+		bloatableSigs.add("Ljava/util/Queue;");
+		bloatableSigs.add("Ljava/util/Set;");
+		bloatableSigs.add("Ljava/util/SortedSet;");
+		bloatableSigs.add("Ljava/util/SortedMap;");
+		bloatableSigs.add("Ljava/util/Stack;");
+		bloatableSigs.add("Ljava/lang/StringBuffer;");
+		bloatableSigs.add("Ljava/lang/StringBuilder;");
+		bloatableSigs.add("Ljava/util/TreeMap;");
+		bloatableSigs.add("Ljava/util/TreeSet;");
+		bloatableSigs.add("Ljava/util/Vector;");
+	}
 
-    private final ThreadLocal<DateFormat> local = new ThreadLocal<DateFormat>() {
+    //tag
+	private ThreadLocal<DateFormat> local = new ThreadLocal<DateFormat>() {
+        @Override
+        protected DateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
+    
+    private static ThreadLocal<DateFormat> staticLocal = new ThreadLocal<DateFormat>() {
         @Override
         protected DateFormat initialValue() {
             return new SimpleDateFormat();
@@ -20,6 +65,8 @@ public class PMB_Sample {
     public void add(String s) {
         bl_data.add(s);
         data.add(s);
+        System.out.println(staticLocal);
+        System.out.println(local);
     }
 
     public void remove(String s) {
