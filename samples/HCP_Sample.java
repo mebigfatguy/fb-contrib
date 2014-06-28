d2818151226d6dd8386c9458544ffb58f0b45863
Kevin Lubick <kjlubick@ncsu.edu>
2014-06-28 00:59:21 -0400

    Made a test for fields
    Also tested the logging bit.
    
    (real talk: I wrote this test and had to add a few things to the
    whitelist in HCP)

--- samples/HCP_Sample.java
+++ samples/HCP_Sample.java
@@ -4,6 +4,7 @@ import java.net.URISyntaxException;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -12,15 +13,14 @@ import org.apache.http.impl.client.HttpClients;
 
 public class HCP_Sample {
 
-	private CloseableHttpClient client = HttpClients.createDefault();
+	private static CloseableHttpClient client = HttpClients.createDefault();
 	
 	public void forgotToResetGet() throws URISyntaxException {
-		//tag
+		//tag HCP_HTTP_REQUEST_RESOURCES_NOT_FREED_LOCAL 
 		HttpGet httpGet = new HttpGet(new URI("http://www.example.com"));
+		
 		try
-		{
-			StringEntity content = new StringEntity("Some random content");
-			content.setContentType("application/json");
+		{ 
 			try(CloseableHttpResponse response = client.execute(httpGet);)
 			{
 				System.out.println("response: " +response);
@@ -33,7 +33,7 @@ public class HCP_Sample {
 	}
 	
 	public void forgotToResetPut() throws URISyntaxException {
-		//tag
+		//tag HCP_HTTP_REQUEST_RESOURCES_NOT_FREED_LOCAL 
 		HttpPut httpPut = new HttpPut(new URI("http://www.example.com"));
 		try
 		{
@@ -57,8 +57,6 @@ public class HCP_Sample {
 		HttpGet httpGet = new HttpGet(new URI("http://www.example.com"));
 		try
 		{
-			StringEntity content = new StringEntity("Some random content");
-			content.setContentType("application/json");
 			try(CloseableHttpResponse response = client.execute(httpGet);)
 			{
 				System.out.println("response: " +response);
@@ -78,8 +76,6 @@ public class HCP_Sample {
 		HttpGet httpGet = new HttpGet(new URI("http://www.example.com"));
 		try
 		{
-			StringEntity content = new StringEntity("Some random content");
-			content.setContentType("application/json");
 			try(CloseableHttpResponse response = client.execute(httpGet);)
 			{
 				System.out.println("response: " +response);
@@ -139,6 +135,31 @@ public class HCP_Sample {
 		}
 		finally {
 			httpPut.releaseConnection();
+		}
+	}
+	
+	
+	//tag HCP_HTTP_REQUEST_RESOURCES_NOT_FREED_FIELD 
+	private static HttpPost httpPost;
+	
+	public static void main() throws URISyntaxException {
+		
+		httpPost = new HttpPost(new URI("http://www.example.com"));
+		System.out.println(httpPost + " is ready to be called");
+		try
+		{
+			StringEntity content = new StringEntity("Some random content");
+			content.setContentType("application/json");
+
+			httpPost.setEntity(content);
+			try(CloseableHttpResponse response = client.execute(httpPost);)
+			{
+				System.out.println("response: " +response);
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
 		}
 	}
 }
