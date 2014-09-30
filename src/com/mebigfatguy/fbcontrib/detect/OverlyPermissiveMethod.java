945791c5f5ded2ed6309ee4115cf4749bb5ce9d5
Dave Brosius <dbrosius@mebigfatguy.com>
2014-09-30 19:52:00 -0400

    fix illegal state exception

--- src/com/mebigfatguy/fbcontrib/detect/OverlyPermissiveMethod.java
+++ src/com/mebigfatguy/fbcontrib/detect/OverlyPermissiveMethod.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2014 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -43,11 +43,11 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 	private OpcodeStack stack;
 	private String callingPackage;
 	private String callingClass;
-	
+
 	public OverlyPermissiveMethod(BugReporter bugReporter) {
 		this.bugReporter = bugReporter;
 	}
-	
+
 	@Override
 	public void visitClassContext(ClassContext classContext) {
 		try {
@@ -61,13 +61,13 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 			stack = null;
 		}
 	}
-	
+
 	@Override
 	public void visitCode(Code obj) {
 		stack.resetForMethodEntry(this);
 		super.visitCode(obj);
 	}
-	
+
 	@Override
 	public void sawOpcode(int seen) {
 		try {
@@ -94,7 +94,7 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 							}
 							boolean sameClass = calledClass.equals(callingClass);
 							boolean samePackage = calledPackage.equals(callingPackage);
-							
+
 							if (sameClass) {
 								mi.addCallingAccess(Constants.ACC_PRIVATE);
 							} else if (samePackage) {
@@ -111,7 +111,7 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 						}
 					}
 					break;
-					
+
 				default:
 					break;
 			}
@@ -119,22 +119,22 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 			stack.sawOpcode(this, seen);
 		}
 	}
-	
+
 	public boolean isCallingOnThis(String sig) {
 		Type[] argTypes = Type.getArgumentTypes(sig);
 		if (stack.getStackDepth() < argTypes.length) {
 			return false;
 		}
-		
+
 		OpcodeStack.Item item = stack.getStackItem(argTypes.length);
 		return item.getRegisterNumber() == 0;
 	}
-	
+
 	@Override
-	public void report() {		
+	public void report() {
 		for (Map.Entry<StatisticsKey, MethodInfo> entry : Statistics.getStatistics()) {
 			MethodInfo mi = entry.getValue();
-			
+
 			int declaredAccess = mi.getDeclaredAccess();
 			if ((declaredAccess & Constants.ACC_PRIVATE) != 0) {
 				continue;
@@ -143,12 +143,12 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 			if (mi.wasCalledPublicly() || !mi.wasCalled()) {
 				continue;
 			}
-			
+
 			StatisticsKey key = entry.getKey();
-			
+
 			if (isOverlyPermissive(declaredAccess, mi)) {
-				if (!isDerived(getClassContext().getJavaClass(), getMethod())) {
-				
+				if (!isDerived(getClassContext().getJavaClass(), key)) {
+
 					bugReporter.reportBug(new BugInstance(this, "OPM_OVERLY_PERMISSIVE_METHOD", NORMAL_PRIORITY)
 									.addClass(key.getClassName())
 									.addMethod(key.getClassName(), key.getMethodName(), key.getSignature(), (declaredAccess & Constants.ACC_STATIC) != 0));
@@ -156,38 +156,38 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
 			}
 		}
 	}
-		
+
 	private static boolean isOverlyPermissive(int declaredAccess, MethodInfo mi) {
 		if ((declaredAccess & Constants.ACC_PUBLIC) != 0) {
 			return true;
 		}
-		
+
 		//TODO: add more permission checks
 		return false;
 	}
-	
-	private boolean isDerived(JavaClass cls, Method m) {
+
+	private boolean isDerived(JavaClass cls, StatisticsKey key) {
 		try {
 			for (JavaClass infCls : cls.getInterfaces()) {
 				for (Method infMethod : infCls.getMethods()) {
-					if (m.getName().equals(infMethod.getName()) && m.getSignature().equals(infMethod.getSignature())) {
+					if (key.getMethodName().equals(infMethod.getName()) && key.getSignature().equals(infMethod.getSignature())) {
 						return true;
 					}
 				}
 			}
-			
+
 			JavaClass superClass = cls.getSuperClass();
 			if ("java/lang/Object".equals(superClass.getClassName())) {
 				return false;
 			}
-			
+
 			for (Method superMethod : superClass.getMethods()) {
-				if (m.getName().equals(superMethod.getName()) && m.getSignature().equals(superMethod.getSignature())) {
+				if (key.getMethodName().equals(superMethod.getName()) && key.getSignature().equals(superMethod.getSignature())) {
 					return true;
 				}
 			}
-			
-			return isDerived(superClass, m);
+
+			return isDerived(superClass, key);
 		} catch (ClassNotFoundException cnfe) {
 			bugReporter.reportMissingClass(cnfe);
 			return true;
