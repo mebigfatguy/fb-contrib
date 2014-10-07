b89f2a5abb5ffe5fac32cc86cc9a61a49146d257
Dave Brosius <dbrosius@mebigfatguy.com>
2014-10-07 00:16:16 -0400

    simple twiddles

--- src/com/mebigfatguy/fbcontrib/detect/UnrelatedReturnValues.java
+++ src/com/mebigfatguy/fbcontrib/detect/UnrelatedReturnValues.java
@@ -163,7 +163,7 @@ public class UnrelatedReturnValues extends BytecodeScanningDetector
 	 * @param classes the set of classes to look for a common super class or interface
 	 * @return the type that is the common interface or superclass (not Object, tho).
 	 */
-	private JavaClass findCommonType(Set<JavaClass> classes) throws ClassNotFoundException {
+	private static JavaClass findCommonType(Set<JavaClass> classes) throws ClassNotFoundException {
 		Set<JavaClass> possibleCommonTypes = new HashSet<JavaClass>();
 
 		boolean populate = true;
