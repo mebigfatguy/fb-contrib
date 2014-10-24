07f06f72854af9b5833985d532948601831e3b2e
Dave Brosius <dbrosius@mebigfatguy.com>
2014-10-23 21:42:18 -0400

    add map field sample

--- samples/CIS_Sample.java
+++ samples/CIS_Sample.java
@@ -1,9 +1,13 @@
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class CIS_Sample {
 
 	enum Sample {Hi, Lo};
+	
+	Map<String, Object> map = new HashMap<String, Object>();
 	
 	String val;
 	
@@ -16,6 +20,11 @@ public class CIS_Sample {
 		s.append(d);
 		s.append(i);
 		val = s.toString();
+	}
+	
+	public Object testSBToMapField(Date d1, Date d2) {
+		map.put("a-v",  d1 + ":" + d2);
+		return map.get(d1 + "-" + d2);
 	}
 	
 	public void fpTestToStringToFieldSB(String s) {
