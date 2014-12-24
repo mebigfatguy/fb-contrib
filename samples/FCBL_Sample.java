14642b11231aa240e4d39c747d59486e5c88927f
Kevin Lubick <kjlubick@ncsu.edu>
2014-12-23 18:47:43 -0600

    Improves FCBL_Sample
    Adds some tags about what should and should not be detected.
    Additionally adds a few cases (like nested ifs).

--- samples/FCBL_Sample.java
+++ samples/FCBL_Sample.java
@@ -2,28 +2,30 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.util.Locale;
 
 import javax.swing.SwingUtilities;
 
 @SuppressWarnings("all")
 public final class FCBL_Sample {
-    public int foo;
-    private int moo;
-    protected int boo;
-    int hoo;
-    private int fp;
-    private int multiMethodFP;
-    private String test;
-    private int x = 1;
-    private int y = 2;
-    private boolean ret;
+    public int foo;				//no warning (scope)
+    private int moo;			//warning
+    private int boo;			//warning
+    int hoo;					//no warning (scope)
+    private int fp;				//no warning (read in method test2)	
+    private int multiMethodFP;	//no warning (used in a couple methods)
+    private String test;		//warning
+    private String testNestedIfs;//no warning (in one branch of the if, this is read first)
+    private int x = 1;			//warning
+    private int y = 2;			//no warning (read first in method1)
+    private boolean ret;		//warning (shielded in foo)
     @FooAnnotation
-    private String notUsed;
+    private String notUsed;		//warning 
     @FooAnnotation
-    private String used;
+    private String used;		//no warning (annotation+stored)
 
-    private String fooS = "Foo";
-    private String[] fooSS = { fooS };
+    private String fooS = "Foo";	//warning
+    private String[] fooSS = { fooS };	//no warning (read in testInArray)
 
     public FCBL_Sample() {
         foo = 0;
@@ -32,6 +34,10 @@ public final class FCBL_Sample {
         hoo = 3;
         fp = 4;
         used = "Hello";
+        ret = false;
+        
+        //to mask standard URF_UNREAD_FIELD 
+        System.out.println(foo + hoo + used + moo + boo + ret);
     }
 
     public void method1() {
@@ -58,17 +64,39 @@ public final class FCBL_Sample {
 
     public void test3(String in) {
         boolean found = false;
-        if (in.equals("boo"))
+        if ("boo".equals(in))
             test = "boo";
-        else if (in.equals("hoo"))
+        else if ("hoo".equals(in))
             test = "hoo";
-        else if (in.equals("moo")) {
-            if (test.equals("loo") && !found) {
+        else if ("moo".equals(in)) {
+            if ("loo".equals(in) && !found) {
                 found = true;
             }
         }
 
-        test = "woowoo";
+        test = "WooWoo".toLowerCase(Locale.ENGLISH);		//hides the PME warning and the standard DM_CONVERT_CASE 
+        if (in.length() > 1) {
+        	test = "woowoo";
+        	System.out.println(test);
+        }
+    }
+    
+    public void testNestedIfs(String in) {
+        boolean found = false;
+        if ("boo".equals(in))
+            testNestedIfs = "boo";
+        else if ("hoo".equals(in))
+        	testNestedIfs = "hoo";
+        else if ("moo".equals(in)) {
+            if ("loo".equals(in) && !found) {
+                System.out.println(in+testNestedIfs);
+            }
+        }
+
+        testNestedIfs = "WooWoo".toLowerCase(Locale.ENGLISH);		//hides the PME warning and the standard DM_CONVERT_CASE 
+        if (in.length() > 1) {
+        	testNestedIfs = "woowoo";
+        }
     }
 
     public void mm1FP(int i) {
@@ -84,7 +112,7 @@ public final class FCBL_Sample {
         multiMethodFP = i;
     }
 
-    class Foo {
+    static class Foo {
         boolean ret;
 
         public boolean testFPAnon() {
