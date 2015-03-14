820a5d006f05e8ae97cab808d477a59aa3442abc
Dave Brosius <dbrosius@mebigfatguy.com>
2015-03-14 14:30:50 -0400

    avoid infinite loops

--- src/com/mebigfatguy/fbcontrib/utils/ToString.java
+++ src/com/mebigfatguy/fbcontrib/utils/ToString.java
@@ -20,44 +20,75 @@ package com.mebigfatguy.fbcontrib.utils;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * an automatic toString() builder using reflection
  */
 public class ToString {
 
+	private static class VisitedInfo {
+		Set<Integer> visited = new HashSet<Integer>();
+		int count = 0;
+	}
+	private static final ThreadLocal<VisitedInfo> visited = new ThreadLocal<VisitedInfo>() {
+
+		@Override
+		protected VisitedInfo initialValue() {
+			return new VisitedInfo();
+		}
+	};
+	
 	private ToString() {
 	}
 	
 	public static String build(Object o) {
+		VisitedInfo vi = visited.get();
+		try {
+			vi.count++;
+			return generate(o, vi.visited);
+		} finally {
+			if (--vi.count == 0) {
+				vi.visited.clear();
+			}
+		}
+	}
+	
+	private static String generate(Object o, Set<Integer> visitedObjects) {
+		
 		StringBuilder sb = new StringBuilder(100);
 		Class<?> cls = o.getClass();
-		sb.append(cls.getSimpleName()).append('[');
+		int identityHC = System.identityHashCode(o);
+		sb.append(cls.getSimpleName()).append('[').append(identityHC).append("]{");
 		
-		try {
-			String sep = "";
-			for (Field f : cls.getDeclaredFields()) {
-				sb.append(sep);
-				sep = ", ";
-				sb.append(f.getName()).append('=');
-				try {
-    				f.setAccessible(true);
-    				Object value = f.get(o);
-    				if (value == null) {
-    					sb.append((String) null);
-    				} else if (value.getClass().isArray()) {
-    					sb.append(Arrays.toString((Object[]) value));
-    				} else {
-    					sb.append(value);
-    				}
-				} catch (SecurityException e) {
-				    sb.append("*SECURITY_EXCEPTION*");
+		if (!visitedObjects.contains(identityHC)) {	
+			try {
+				visitedObjects.add(identityHC);
+				String sep = "";
+				for (Field f : cls.getDeclaredFields()) {
+					sb.append(sep);
+					sep = ", ";
+					sb.append(f.getName()).append('=');
+					try {
+	    				f.setAccessible(true);
+	    				Object value = f.get(o);
+	    				if (value == null) {
+	    					sb.append((String) null);
+	    				} else if (value.getClass().isArray()) {
+	    					sb.append(Arrays.toString((Object[]) value));
+	    				} else {
+	    					sb.append(value);
+	    				}
+					} catch (SecurityException e) {
+					    sb.append("*SECURITY_EXCEPTION*");
+					}
 				}
+			} catch (Exception e) {
 			}
-		} catch (Exception e) {
 		}
 		
-		sb.append(']');
+		sb.append('}');
 		return sb.toString();
 	}
 }
