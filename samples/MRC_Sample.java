be5807c8fa4415007cf9a93ad8ef2cb066a3d2f9
Dave Brosius <dbrosius@mebigfatguy.com>
2015-04-01 00:06:49 -0400

    add fp for MRC with arrays

--- samples/MRC_Sample.java
+++ samples/MRC_Sample.java
@@ -5,95 +5,104 @@ import java.util.List;
 
 @SuppressWarnings("all")
 public class MRC_Sample {
-    private Component[] components;
+	private Component[] components;
 
-    private int getValue() {
-        return 1;
-    }
+	private int getValue() {
+		return 1;
+	}
 
-    private String getStringValue() {
-        return "Hello";
-    }
+	private String getStringValue() {
+		return "Hello";
+	}
 
-    public float getFP() {
-        return 0.0f;
-    }
+	public float getFP() {
+		return 0.0f;
+	}
 
-    private double getTwoValuesFP(boolean b) {
-        if (b)
-            return 1.0;
-        else
-            return 0.0;
-    }
+	private double getTwoValuesFP(boolean b) {
+		if (b)
+			return 1.0;
+		else
+			return 0.0;
+	}
 
-    private String getTwoDupsBySwitch(int i) {
-        switch (i) {
-        case 1:
-            return "Hello";
+	private String getTwoDupsBySwitch(int i) {
+		switch (i) {
+		case 1:
+			return "Hello";
 
-        case 2:
-            return "Hello";
+		case 2:
+			return "Hello";
 
-        default:
-            return "Hello";
-        }
-    }
+		default:
+			return "Hello";
+		}
+	}
 
-    private String fpStringBuilder() {
-        StringBuilder sb = new StringBuilder();
-        fooIt(sb);
+	private String fpStringBuilder() {
+		StringBuilder sb = new StringBuilder();
+		fooIt(sb);
 
-        return sb.toString();
-    }
+		return sb.toString();
+	}
 
-    private void fooIt(StringBuilder sb) {
-        sb.append("Foo");
-    }
+	private void fooIt(StringBuilder sb) {
+		sb.append("Foo");
+	}
 
-    private int getCount(List<String> l) {
-        int count = 0;
-        Iterator<String> it = l.iterator();
-        while (it.hasNext()) {
-            if ("Foo".equals(it.next())) {
-                count += 1;
-            }
-        }
+	private int getCount(List<String> l) {
+		int count = 0;
+		Iterator<String> it = l.iterator();
+		while (it.hasNext()) {
+			if ("Foo".equals(it.next())) {
+				count += 1;
+			}
+		}
 
-        return count;
-    }
+		return count;
+	}
 
-    private int fpInc() {
-        int i = 0;
-        i++;
-        return i;
-    }
+	private long[] fpGetHash(String key) {
+		long[] hash = new long[2];
+		hash(key, hash);
+		return hash;
+	}
 
-    private long fpCountChars(Collection<String> c) {
-        long totLength = 0;
-        for (String s : c) {
-            totLength += s.length();
-        }
-        return totLength;
-    }
+	private void hash(String key, long[] hash) {
+	}
 
-    private int getFPLoopVar(List<String> c) {
-        for (int i = 0; i < c.size(); i++) {
-            if (c.get(i) == null) {
-                return i;
-            }
-        }
+	private int fpInc() {
+		int i = 0;
+		i++;
+		return i;
+	}
 
-        throw new RuntimeException();
-    }
+	private long fpCountChars(Collection<String> c) {
+		long totLength = 0;
+		for (String s : c) {
+			totLength += s.length();
+		}
+		return totLength;
+	}
 
-    private int fpFindComponent(Component component) {
-        int index = 0;
-        while (this.components[index] != component) {
-            index++;
-            if (index >= this.components.length) {
-                return 0;
-            }
-        }
-        return index;
-    }
+	private int getFPLoopVar(List<String> c) {
+		for (int i = 0; i < c.size(); i++) {
+			if (c.get(i) == null) {
+				return i;
+			}
+		}
+
+		throw new RuntimeException();
+	}
+
+	private int fpFindComponent(Component component) {
+		int index = 0;
+		while (this.components[index] != component) {
+			index++;
+			if (index >= this.components.length) {
+				return 0;
+			}
+		}
+		return index;
+	}
 }
