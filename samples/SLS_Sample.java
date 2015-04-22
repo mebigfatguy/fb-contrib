a34b81d83dbde0dcc635ed349f5998d21e5b9726
Dave Brosius <dbrosius@mebigfatguy.com>
2015-04-21 22:09:12 -0400

    add another test pattern for SLS

--- samples/SLS_Sample.java
+++ samples/SLS_Sample.java
@@ -3,12 +3,25 @@ import java.util.List;
 
 public class SLS_Sample {
 	private String name;
+	private int age;
 	
 	boolean hasIt(List<SLS_Sample> l, String n) {
 		
 		boolean found = false;
 		for (SLS_Sample s : l) {
 			if (s.name.equals(n)) {
+				found = true;
+			}
+		}
+		
+		return found;
+	}
+	
+	boolean hasAge(List<SLS_Sample> l, int age) {
+		
+		boolean found = false;
+		for (SLS_Sample s : l) {
+			if (s.age == age) {
 				found = true;
 			}
 		}
