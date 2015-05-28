ec4f8fb931d9150b93bec68b7d305e913ce382ce
Dave Brosius <dbrosius@mebigfatguy.com>
2015-05-27 22:42:38 -0400

    revert BAS change, introduces embedded forloop fps

--- src/com/mebigfatguy/fbcontrib/detect/BloatedAssignmentScope.java
+++ src/com/mebigfatguy/fbcontrib/detect/BloatedAssignmentScope.java
@@ -298,6 +298,7 @@ public class BloatedAssignmentScope extends BytecodeScanningDetector {
                                 rootScopeBlock.addChild(sb);
                             } else {
                                 sb.pushUpLoadStores();
+                                sb.setStart(pc);
                             }
                         } else {
                             sb = new ScopeBlock(pc, target);
