a57cd86f963b1298071e66d40cb53803840807a5
Dave Brosius <dbrosius@mebigfatguy.com>
2015-08-11 02:33:48 -0400

    GitHub issue #55: don't report WEM in static initializers as there is minimal context

--- src/com/mebigfatguy/fbcontrib/detect/WeakExceptionMessaging.java
+++ src/com/mebigfatguy/fbcontrib/detect/WeakExceptionMessaging.java
@@ -42,152 +42,161 @@ import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for exceptions that are thrown with static strings as messages. Using static strings
- * doesn't differentiate one use of this method versus another, and so it may be difficult
- * to determine how this exception occurred without showing context.
+ * looks for exceptions that are thrown with static strings as messages. Using
+ * static strings doesn't differentiate one use of this method versus another,
+ * and so it may be difficult to determine how this exception occurred without
+ * showing context.
  */
 @CustomUserValue
 public class WeakExceptionMessaging extends BytecodeScanningDetector {
 
-	private static JavaClass exceptionClass;
-	private static final Set<String> ignorableExceptionTypes = new HashSet<String>();
+    private static JavaClass exceptionClass;
+    private static final Set<String> ignorableExceptionTypes = new HashSet<String>();
 
-	static {
-		try {
-			exceptionClass = Repository.lookupClass("java/lang/Exception");
-		} catch (ClassNotFoundException cnfe) {
-			exceptionClass = null;
-		}
+    static {
+        try {
+            exceptionClass = Repository.lookupClass("java/lang/Exception");
+        } catch (ClassNotFoundException cnfe) {
+            exceptionClass = null;
+        }
 
-		ignorableExceptionTypes.add("java.lang.UnsupportedOperationException");
-	}
+        ignorableExceptionTypes.add("java.lang.UnsupportedOperationException");
+    }
 
-	private final BugReporter bugReporter;
-	private OpcodeStack stack;
+    private final BugReporter bugReporter;
+    private OpcodeStack stack;
 
-	/**
+    /**
      * constructs a WEM detector given the reporter to report bugs on
-     * @param bugReporter the sync of bug reports
-	 */
-	public WeakExceptionMessaging(BugReporter bugReporter) {
-		this.bugReporter = bugReporter;
-	}
+     *
+     * @param bugReporter
+     *            the sync of bug reports
+     */
+    public WeakExceptionMessaging(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
 
-	/**
-	 * overrides the visitor to initialize and tear down the opcode stack
-	 *
-	 * @param classContext the context object of the currently parsed class
-	 */
-	@Override
-	public void visitClassContext(ClassContext classContext) {
-		try {
-			if (exceptionClass != null) {
-				stack = new OpcodeStack();
-				super.visitClassContext(classContext);
-			}
-		} finally {
-			stack = null;
-		}
-	}
+    /**
+     * overrides the visitor to initialize and tear down the opcode stack
+     *
+     * @param classContext
+     *            the context object of the currently parsed class
+     */
+    @Override
+    public void visitClassContext(ClassContext classContext) {
+        try {
+            if (exceptionClass != null) {
+                stack = new OpcodeStack();
+                super.visitClassContext(classContext);
+            }
+        } finally {
+            stack = null;
+        }
+    }
 
-	/**
-	 * looks for methods that contain a ATHROW opcodes
-	 *
-	 * @param method the context object of the current method
-	 * @return if the class uses throws
-	 */
-	public boolean prescreen(Method method) {
-		BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
-		return (bytecodeSet != null) && (bytecodeSet.get(Constants.ATHROW));
-	}
+    /**
+     * looks for methods that contain a ATHROW opcodes
+     *
+     * @param method
+     *            the context object of the current method
+     * @return if the class uses throws
+     */
+    public boolean prescreen(Method method) {
+        if (Values.STATIC_INITIALIZER.equals(method.getName())) {
+            return false;
+        }
 
-	/**
-	 * overrides the visitor to prescreen the method to look for throws calls
-	 * and only forward onto bytecode scanning if there
-	 *
-	 * @param obj the context object of the currently parsed code block
-	 */
-	@Override
-	public void visitCode(Code obj) {
-		Method method = getMethod();
-		if (!method.isSynthetic()) {
-			if (prescreen(method)) {
-				stack.resetForMethodEntry(this);
-				super.visitCode(obj);
-			}
-		}
-	}
+        BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
+        return (bytecodeSet != null) && (bytecodeSet.get(Constants.ATHROW));
+    }
 
-	/**
-	 * overrides the visitor to look for throws instructions using exceptions with
-	 * static messages
-	 *
-	 * @param seen the opcode of the currently visited instruction
-	 */
-	@Override
-	public void sawOpcode(int seen) {
-		boolean allConstantStrings = false;
-		boolean sawConstant = false;
-		try {
-	        stack.precomputation(this);
-	        
-			if (seen == ATHROW) {
-				if (stack.getStackDepth() > 0) {
-					OpcodeStack.Item item = stack.getStackItem(0);
-					if (item.getUserValue() != null) {
-						JavaClass exClass = item.getJavaClass();
-						if ((exClass == null) || !ignorableExceptionTypes.contains(exClass.getClassName())) {
-							bugReporter.reportBug(new BugInstance(this, BugType.WEM_WEAK_EXCEPTION_MESSAGING.name(), LOW_PRIORITY)
-									   .addClass(this)
-									   .addMethod(this)
-									   .addSourceLine(this));
-						}
-					}
-				}
-			} else if ((seen == LDC) || (seen == LDC_W)) {
-				if (getConstantRefOperand() instanceof ConstantString)
-					sawConstant = true;
-			} else if (seen == INVOKESPECIAL) {
-				if (Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
-					String clsName = getClassConstantOperand();
-					if (clsName.indexOf("Exception") >= 0) {
-						JavaClass exCls = Repository.lookupClass(clsName);
-						if (exCls.instanceOf(exceptionClass)) {
-							String sig = getSigConstantOperand();
-							Type[] argTypes = Type.getArgumentTypes(sig);
-							int stringParms = 0;
-							for (int t = 0; t < argTypes.length; t++) {
-								if ("Ljava/lang/String;".equals(argTypes[t].getSignature())) {
-									stringParms++;
-									int stackOffset = argTypes.length - t - 1;
-									if (stack.getStackDepth() > stackOffset) {
-										OpcodeStack.Item item = stack.getStackItem(stackOffset);
-										if (item.getUserValue() == null)
-											return;
-									}
-								}
-							}
-							if (clsName.equals("java/lang/Exception") && getSigConstantOperand().equals("(Ljava/lang/Throwable;)V")) {
-								bugReporter.reportBug(new BugInstance(this, BugType.WEM_OBSCURING_EXCEPTION.name(), LOW_PRIORITY)
-											.addClass(this)
-											.addMethod(this)
-											.addSourceLine(this));
-							}
-							allConstantStrings = stringParms > 0;
-						}
-					}
-				}
-			}
-		} catch (ClassNotFoundException cnfe) {
-			bugReporter.reportMissingClass(cnfe);
-		} finally {
-			TernaryPatcher.pre(stack, seen);
-			stack.sawOpcode(this, seen);
-			TernaryPatcher.post(stack, seen);
-			if ((sawConstant || allConstantStrings) && (stack.getStackDepth() > 0)) {
-				OpcodeStack.Item item = stack.getStackItem(0);
-				item.setUserValue(Boolean.TRUE);
-			}
-		}
-	}
+    /**
+     * overrides the visitor to prescreen the method to look for throws calls
+     * and only forward onto bytecode scanning if there
+     *
+     * @param obj
+     *            the context object of the currently parsed code block
+     */
+    @Override
+    public void visitCode(Code obj) {
+        Method method = getMethod();
+        if (!method.isSynthetic()) {
+            if (prescreen(method)) {
+                stack.resetForMethodEntry(this);
+                super.visitCode(obj);
+            }
+        }
+    }
+
+    /**
+     * overrides the visitor to look for throws instructions using exceptions
+     * with static messages
+     *
+     * @param seen
+     *            the opcode of the currently visited instruction
+     */
+    @Override
+    public void sawOpcode(int seen) {
+        boolean allConstantStrings = false;
+        boolean sawConstant = false;
+        try {
+            stack.precomputation(this);
+
+            if (seen == ATHROW) {
+                if (stack.getStackDepth() > 0) {
+                    OpcodeStack.Item item = stack.getStackItem(0);
+                    if (item.getUserValue() != null) {
+                        JavaClass exClass = item.getJavaClass();
+                        if ((exClass == null) || !ignorableExceptionTypes.contains(exClass.getClassName())) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.WEM_WEAK_EXCEPTION_MESSAGING.name(), LOW_PRIORITY).addClass(this)
+                                    .addMethod(this).addSourceLine(this));
+                        }
+                    }
+                }
+            } else if ((seen == LDC) || (seen == LDC_W)) {
+                if (getConstantRefOperand() instanceof ConstantString) {
+                    sawConstant = true;
+                }
+            } else if (seen == INVOKESPECIAL) {
+                if (Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
+                    String clsName = getClassConstantOperand();
+                    if (clsName.indexOf("Exception") >= 0) {
+                        JavaClass exCls = Repository.lookupClass(clsName);
+                        if (exCls.instanceOf(exceptionClass)) {
+                            String sig = getSigConstantOperand();
+                            Type[] argTypes = Type.getArgumentTypes(sig);
+                            int stringParms = 0;
+                            for (int t = 0; t < argTypes.length; t++) {
+                                if ("Ljava/lang/String;".equals(argTypes[t].getSignature())) {
+                                    stringParms++;
+                                    int stackOffset = argTypes.length - t - 1;
+                                    if (stack.getStackDepth() > stackOffset) {
+                                        OpcodeStack.Item item = stack.getStackItem(stackOffset);
+                                        if (item.getUserValue() == null) {
+                                            return;
+                                        }
+                                    }
+                                }
+                            }
+                            if (clsName.equals("java/lang/Exception") && getSigConstantOperand().equals("(Ljava/lang/Throwable;)V")) {
+                                bugReporter.reportBug(new BugInstance(this, BugType.WEM_OBSCURING_EXCEPTION.name(), LOW_PRIORITY).addClass(this).addMethod(this)
+                                        .addSourceLine(this));
+                            }
+                            allConstantStrings = stringParms > 0;
+                        }
+                    }
+                }
+            }
+        } catch (ClassNotFoundException cnfe) {
+            bugReporter.reportMissingClass(cnfe);
+        } finally {
+            TernaryPatcher.pre(stack, seen);
+            stack.sawOpcode(this, seen);
+            TernaryPatcher.post(stack, seen);
+            if ((sawConstant || allConstantStrings) && (stack.getStackDepth() > 0)) {
+                OpcodeStack.Item item = stack.getStackItem(0);
+                item.setUserValue(Boolean.TRUE);
+            }
+        }
+    }
 }
