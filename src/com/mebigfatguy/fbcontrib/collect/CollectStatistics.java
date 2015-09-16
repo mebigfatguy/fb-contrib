22071af7865e187787ce0c6cb82ca253e32dd781
Dave Brosius <dbrosius@mebigfatguy.com>
2015-09-15 20:28:00 -0400

    fix the damn backport bug again

--- src/com/mebigfatguy/fbcontrib/collect/CollectStatistics.java
+++ src/com/mebigfatguy/fbcontrib/collect/CollectStatistics.java
@@ -18,6 +18,7 @@
  */
 package com.mebigfatguy.fbcontrib.collect;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,7 +28,6 @@ import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
 
-import edu.emory.mathcs.backport.java.util.Collections;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.NonReportingDetector;
