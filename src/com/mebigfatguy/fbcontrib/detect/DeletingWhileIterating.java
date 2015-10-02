9adf6965c09332f39360df3ae9027e69b2944ed6
Dave Brosius <dbrosius@mebigfatguy.com>
2015-10-01 20:42:12 -0400

    backport bug again, you'd think i'd learn

--- src/com/mebigfatguy/fbcontrib/detect/DeletingWhileIterating.java
+++ src/com/mebigfatguy/fbcontrib/detect/DeletingWhileIterating.java
@@ -20,6 +20,7 @@ package com.mebigfatguy.fbcontrib.detect;
 
 import java.util.ArrayList;
 import java.util.BitSet;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -43,7 +44,6 @@ import com.mebigfatguy.fbcontrib.utils.TernaryPatcher;
 import com.mebigfatguy.fbcontrib.utils.ToString;
 import com.mebigfatguy.fbcontrib.utils.Values;
 
-import edu.emory.mathcs.backport.java.util.Collections;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
