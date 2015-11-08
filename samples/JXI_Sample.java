afd5690081ff4a2821b41aa468d504386af84a68
Dave Brosius <dbrosius@mebigfatguy.com>
2015-11-08 13:50:15 -0500

    add pattern for bad @Context parm type

--- samples/JXI_Sample.java
+++ samples/JXI_Sample.java
@@ -1,9 +1,13 @@
+import java.io.InputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,17 +28,24 @@ public class JXI_Sample {
     }
     
     @GET
+    @Path("/stuffNotRight/{good}")
+    public Response mismatchParm(@PathParam("bad") String bad) {
+        return Response.ok().build();
+    }
+    
+    @PUT
+    @Path("/stuffMe")
+    public Response stuffWithBadContext(@Context InputStream is) {
+        return Response.ok().build();
+    }
+    
+    @GET
     @Path("/stuffy")
     @Produces(MediaType.APPLICATION_JSON) 
     public Response fpFine(Stuff s) {
         return Response.ok().build();
     }
-    
-    @GET
-    @Path("/stuffNotRight/{good}")
-    public Response mismatchParm(@PathParam("bad") String bad) {
-        return Response.ok().build();
-    }
+
     
     @POST
     @Path("/stuffok/{blub}")
