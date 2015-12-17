0851e5732a5888dc00ea3b42f4a93258970d4046
Dave Brosius <dbrosius@mebigfatguy.com>
2015-12-16 19:45:05 -0500

    add new bug pattern for JPAI

--- samples/JPAI_Sample.java
+++ samples/JPAI_Sample.java
@@ -1,3 +1,9 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import org.springframework.transaction.annotation.Transactional;
 
 public class JPAI_Sample {
@@ -5,5 +11,42 @@ public class JPAI_Sample {
     @Transactional
     private void writeData() {
         
+    }
+    
+    @Entity
+    @Table(name = "MY_ENTITY")
+    public static class MyEntity {
+        
+        public Integer id;
+        
+        @Id
+        @SequenceGenerator(name = "MY_ENTITY_SEQ", sequenceName = "MY_ENTITY_SEQ", allocationSize = 100)
+        @GeneratedValue(generator = "MY_ENTITY_SEQ")
+        public Integer getId() {
+            return id;
+        }
+        
+        public void setId(Integer id) {
+            this.id = id;
+        }
+        
+        @Override
+        public int hashCode() {
+            return id.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof MyEntity)) {
+                return false;
+            }
+            
+            MyEntity that = (MyEntity) o;
+            if (id == null) {
+                return that.id == null;
+            }
+            
+            return id.equals(that.id);
+        }
     }
 }
