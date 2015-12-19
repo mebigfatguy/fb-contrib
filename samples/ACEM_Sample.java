157726a914c0066dcb9be05a6c0a4c8906df09f0
Dave Brosius <dbrosius@mebigfatguy.com>
2015-12-18 19:38:21 -0500

    add FP for ACEM

--- samples/ACEM_Sample.java
+++ samples/ACEM_Sample.java
@@ -1,9 +1,19 @@
 
-public abstract class ACEM_Sample {
+public abstract class ACEM_Sample implements Foo {
     public void test() {
     }
 
     public int test1() {
         throw new UnsupportedOperationException("Not implemented");
     }
+    
+    @Override
+    public void doFPFoo() {
+    }
+    
+    
+}
+
+interface Foo {
+    void doFPFoo();
 }
