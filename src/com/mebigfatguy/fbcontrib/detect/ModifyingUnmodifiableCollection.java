4f9572c755533e9d5e210ef78d2a94240adf1664
Dave Brosius <dbrosius@mebigfatguy.com>
2015-12-22 08:05:32 -0500

    cleanup ugly strings concats with QMethod

--- src/com/mebigfatguy/fbcontrib/detect/ModifyingUnmodifiableCollection.java
+++ src/com/mebigfatguy/fbcontrib/detect/ModifyingUnmodifiableCollection.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2015 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -29,6 +29,7 @@ import com.mebigfatguy.fbcontrib.collect.MethodInfo;
 import com.mebigfatguy.fbcontrib.collect.Statistics;
 import com.mebigfatguy.fbcontrib.utils.BugType;
 import com.mebigfatguy.fbcontrib.utils.CollectionUtils;
+import com.mebigfatguy.fbcontrib.utils.QMethod;
 import com.mebigfatguy.fbcontrib.utils.Values;
 
 import edu.umd.cs.findbugs.BugInstance;
@@ -39,25 +40,24 @@ import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for code that attempts to modify a collection that is or may be defined
- * as immutable. Doing so will cause exceptions at runtime.
+ * looks for code that attempts to modify a collection that is or may be defined as immutable. Doing so will cause exceptions at runtime.
  */
 @CustomUserValue
 public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
 
-    private static final Map<String, Integer> MODIFYING_METHODS;
+    private static final Map<QMethod, Integer> MODIFYING_METHODS;
 
     static {
-        Map<String, Integer> mm = new HashMap<String, Integer>();
-        mm.put("add(Ljava/lang/Object;)Z", Values.ONE);
-        mm.put("remove(Ljava/lang/Object;)Z", Values.ONE);
-        mm.put("addAll(Ljava/util/Collection;)Z", Values.ONE);
-        mm.put("retainAll(Ljava/util/Collection;)Z", Values.ONE);
-        mm.put("removeAll(Ljava/util/Collection;)Z", Values.ONE);
-        mm.put("put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", Values.TWO);
-        mm.put("remove(Ljava/lang/Object;)Ljava/lang/Object;", Values.ONE);
-        mm.put("putAll(Ljava/util/Map;)V;", Values.ONE);
-        MODIFYING_METHODS = Collections.<String, Integer>unmodifiableMap(mm);
+        Map<QMethod, Integer> mm = new HashMap<QMethod, Integer>();
+        mm.put(new QMethod("add", "(Ljava/lang/Object;)Z"), Values.ONE);
+        mm.put(new QMethod("remove", "(Ljava/lang/Object;)Z"), Values.ONE);
+        mm.put(new QMethod("addAll", "(Ljava/util/Collection;)Z"), Values.ONE);
+        mm.put(new QMethod("retainAll", "(Ljava/util/Collection;)Z"), Values.ONE);
+        mm.put(new QMethod("removeAll", "(Ljava/util/Collection;)Z"), Values.ONE);
+        mm.put(new QMethod("put", "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"), Values.TWO);
+        mm.put(new QMethod("remove", "(Ljava/lang/Object;)Ljava/lang/Object;"), Values.ONE);
+        mm.put(new QMethod("putAll", "(Ljava/util/Map;)V;"), Values.ONE);
+        MODIFYING_METHODS = Collections.<QMethod, Integer> unmodifiableMap(mm);
     }
 
     private final BugReporter bugReporter;
@@ -66,7 +66,7 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
 
     /**
      * constructs a MUC detector given the reporter to report bugs on
-     * 
+     *
      * @param reporter
      *            the sync of bug reports
      */
@@ -76,7 +76,7 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
 
     /**
      * overrides the visitor to setup and tear down the opcode stack
-     * 
+     *
      * @param context
      *            the context object of the currently parse java class
      */
@@ -91,9 +91,8 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
     }
 
     /**
-     * overrides the visitor to reset the opcode stack, and reset the reported
-     * immutability of the method
-     * 
+     * overrides the visitor to reset the opcode stack, and reset the reported immutability of the method
+     *
      * @param obj
      *            the context object of the currently parse code block
      */
@@ -105,9 +104,8 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
     }
 
     /**
-     * overrides the visitor to find method mutations on collections that have
-     * previously been determined to have been created as immutable collections
-     * 
+     * overrides the visitor to find method mutations on collections that have previously been determined to have been created as immutable collections
+     *
      * @param seen
      *            the currently parsed opcode
      */
@@ -123,37 +121,37 @@ public class ModifyingUnmodifiableCollection extends BytecodeScanningDetector {
             stack.precomputation(this);
 
             switch (seen) {
-            case INVOKESTATIC:
-            case INVOKEINTERFACE:
-            case INVOKESPECIAL:
-            case INVOKEVIRTUAL: {
-                String className = getClassConstantOperand();
-                String methodName = getNameConstantOperand();
-                String signature = getSigConstantOperand();
+                case INVOKESTATIC:
+                case INVOKEINTERFACE:
+                case INVOKESPECIAL:
+                case INVOKEVIRTUAL: {
+                    String className = getClassConstantOperand();
+                    String methodName = getNameConstantOperand();
+                    String signature = getSigConstantOperand();
 
-                MethodInfo mi = Statistics.getStatistics().getMethodStatistics(className, methodName, signature);
-                imType = mi.getImmutabilityType();
+                    MethodInfo mi = Statistics.getStatistics().getMethodStatistics(className, methodName, signature);
+                    imType = mi.getImmutabilityType();
 
-                if (seen == INVOKEINTERFACE) {
-                    Integer collectionOffset = MODIFYING_METHODS.get(methodName + signature);
-                    if ((collectionOffset != null) && CollectionUtils.isListSetMap(className)) {
-                        if (stack.getStackDepth() > collectionOffset.intValue()) {
-                            OpcodeStack.Item item = stack.getStackItem(collectionOffset.intValue());
-                            ImmutabilityType type = (ImmutabilityType) item.getUserValue();
+                    if (seen == INVOKEINTERFACE) {
+                        Integer collectionOffset = MODIFYING_METHODS.get(new QMethod(methodName, signature));
+                        if ((collectionOffset != null) && CollectionUtils.isListSetMap(className)) {
+                            if (stack.getStackDepth() > collectionOffset.intValue()) {
+                                OpcodeStack.Item item = stack.getStackItem(collectionOffset.intValue());
+                                ImmutabilityType type = (ImmutabilityType) item.getUserValue();
 
-                            if ((type == ImmutabilityType.IMMUTABLE)
-                                    || ((type == ImmutabilityType.POSSIBLY_IMMUTABLE) && (reportedType != ImmutabilityType.POSSIBLY_IMMUTABLE))) {
-                                bugReporter.reportBug(new BugInstance(this, BugType.MUC_MODIFYING_UNMODIFIABLE_COLLECTION.name(),
-                                        (type == ImmutabilityType.IMMUTABLE) ? HIGH_PRIORITY : NORMAL_PRIORITY).addClass(this).addMethod(this)
-                                                .addSourceLine(this));
-                                reportedType = type;
+                                if ((type == ImmutabilityType.IMMUTABLE)
+                                        || ((type == ImmutabilityType.POSSIBLY_IMMUTABLE) && (reportedType != ImmutabilityType.POSSIBLY_IMMUTABLE))) {
+                                    bugReporter.reportBug(new BugInstance(this, BugType.MUC_MODIFYING_UNMODIFIABLE_COLLECTION.name(),
+                                            (type == ImmutabilityType.IMMUTABLE) ? HIGH_PRIORITY : NORMAL_PRIORITY).addClass(this).addMethod(this)
+                                                    .addSourceLine(this));
+                                    reportedType = type;
+                                }
                             }
                         }
                     }
                 }
-            }
                 break;
-            default:
+                default:
                 break;
             }
         } catch (ClassNotFoundException cnfe) {
