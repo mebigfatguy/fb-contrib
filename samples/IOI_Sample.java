0d367bd83dc8015309d27c9a206ee6663844ef0a
Dave Brosius <dbrosius@mebigfatguy.com>
2016-01-05 21:50:57 -0500

    add new IOI sample to chew on

---
+++ samples/IOI_Sample.java
@@ -0,0 +1,21 @@
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+
+public class IOI_Sample {
+
+    public byte[] getData(File f) throws IOException {
+        try (InputStream is = new BufferedInputStream(new FileInputStream(f));
+             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            
+            IOUtils.copy(is, baos);
+            
+            return baos.toByteArray();
+        }
+    }
+}
