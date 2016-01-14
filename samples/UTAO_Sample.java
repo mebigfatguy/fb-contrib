43286c7a3a0bcd958c81575fbd64302e88df3c65
Dave Brosius <dbrosius@mebigfatguy.com>
2016-01-13 22:18:50 -0500

    Add FP case from GitHub issue #94

--- samples/UTAO_Sample.java
+++ samples/UTAO_Sample.java
@@ -1,5 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.testng.annotations.BeforeMethod;
 
 import junit.framework.TestCase;
 
@@ -9,8 +12,8 @@ public class UTAO_Sample extends TestCase {
     public void testExactDoubles(double d1, double d2) {
         Assert.assertEquals(d1, d2);
         Assert.assertEquals("Still bad", d1, d2);
-        Assert.assertEquals(0.1, d1, d2);	// Actually good
-        Assert.assertEquals("This one is ok", 0.1, d1, d2);	// Still good
+        Assert.assertEquals(0.1, d1, d2); // Actually good
+        Assert.assertEquals("This one is ok", 0.1, d1, d2); // Still good
     }
 
     public void testTrue(boolean b) {
@@ -20,11 +23,11 @@ public class UTAO_Sample extends TestCase {
     public void testFalse(boolean b) {
         Assert.assertEquals("Wow this is bad", false, b);
     }
-    
+
     public void testNull(String s) {
         Assert.assertEquals(null, s);
     }
-    
+
     public void testNotNull(String s) {
         Assert.assertNotEquals(null, s);
     }
@@ -46,23 +49,23 @@ public class UTAO_Sample extends TestCase {
         Assert.assertTrue(s.equals(s2));
         Assert.assertTrue(s.length() == s.length());
     }
-    
+
     public void testUseAssertNotEquals(String s, String s2) {
         Assert.assertFalse(s.equals(s2));
         Assert.assertFalse(s.length() == s.length());
         Assert.assertFalse("this is bad", s.equals(s2));
         Assert.assertFalse("this is reallly bad", s.length() == s.length());
     }
-    
+
     public void testUseAssertNotEqualsCrossed(String s, String s2) {
-    	Assert.assertTrue(!s.equals(s2));
+        Assert.assertTrue(!s.equals(s2));
         Assert.assertTrue(s.length() != s.length());
         Assert.assertTrue("this is bad", !s.equals(s2));
         Assert.assertTrue("this is reallly bad", s.length() != s.length());
     }
-    
+
     public void testUseAssertEqualsCrossed(String s, String s2) {
-    	Assert.assertFalse(!s.equals(s2));
+        Assert.assertFalse(!s.equals(s2));
         Assert.assertFalse(s.length() != s.length());
         Assert.assertFalse("this is bad", !s.equals(s2));
         Assert.assertFalse("this is reallly bad", s.length() != s.length());
@@ -87,16 +90,16 @@ class New {
     private void check(String a, String b) {
         Assert.assertEquals(a, b);
     }
-    
-    @Test(expected=RuntimeException.class)
+
+    @Test(expected = RuntimeException.class)
     public void fpNoAssertsWithJUnitExpects() {
         throw new RuntimeException();
     }
-    
+
     @Test
     @SuppressWarnings("deprecation")
     public void usingOldClasses(int x) {
-        junit.framework.Assert.assertEquals(0,  x);
+        junit.framework.Assert.assertEquals(0, x);
     }
 }
 
@@ -114,20 +117,20 @@ class TestNG {
     public void testFalse(boolean b) {
         org.testng.Assert.assertEquals(b, false, "Wow this is bad");
     }
-    
+
     @org.testng.annotations.Test
     public void testExactDoubles(double d1, double d2) {
-    	org.testng.Assert.assertEquals(d1, d2, "Don't ever do this!");
+        org.testng.Assert.assertEquals(d1, d2, "Don't ever do this!");
     }
-    
+
     @org.testng.annotations.Test
     public void testNull(String s) {
-    	org.testng.Assert.assertEquals(s, null);
+        org.testng.Assert.assertEquals(s, null);
     }
-    
+
     @org.testng.annotations.Test
     public void testNotNull(String s) {
-    	org.testng.Assert.assertNotEquals(s, null);
+        org.testng.Assert.assertNotEquals(s, null);
     }
 
     @org.testng.annotations.Test
@@ -151,33 +154,53 @@ class TestNG {
         org.testng.Assert.assertTrue(s.equals(s2));
         org.testng.Assert.assertTrue(s.length() == s.length());
     }
-    
+
     @org.testng.annotations.Test
     public void testUseAssertNotEquals(String s, String s2) {
-    	org.testng.Assert.assertFalse(s.equals(s2));
-    	org.testng.Assert.assertFalse(s.length() == s.length());
+        org.testng.Assert.assertFalse(s.equals(s2));
+        org.testng.Assert.assertFalse(s.length() == s.length());
     }
-    
+
     @org.testng.annotations.Test
     public void testUseAssertNotEqualsCrossed(String s, String s2) {
-    	org.testng.Assert.assertTrue(!s.equals(s2));
-    	org.testng.Assert.assertTrue(s.length() != s.length());
+        org.testng.Assert.assertTrue(!s.equals(s2));
+        org.testng.Assert.assertTrue(s.length() != s.length());
     }
-    
+
     @org.testng.annotations.Test
     public void testUseAssertEqualsCrossed(String s, String s2) {
-    	org.testng.Assert.assertFalse(!s.equals(s2));
-    	org.testng.Assert.assertFalse(s.length() != s.length());
+        org.testng.Assert.assertFalse(!s.equals(s2));
+        org.testng.Assert.assertFalse(s.length() != s.length());
     }
 
     @org.testng.annotations.Test
     public void test3ArgNP(float foo, int boo) {
-    	org.testng.Assert.assertEquals(foo, 1.0f, 0.1);
-    	org.testng.Assert.assertEquals(boo, 20, 0);
+        org.testng.Assert.assertEquals(foo, 1.0f, 0.1);
+        org.testng.Assert.assertEquals(boo, 20, 0);
     }
-    
-    @org.testng.annotations.Test(expectedExceptions=RuntimeException.class)
+
+    @org.testng.annotations.Test(expectedExceptions = RuntimeException.class)
     public void fpNoAssertsWithNGExpects() {
         throw new RuntimeException();
     }
+}
+
+class GitHubIssue94 {
+    private Object realObject;
+
+    @Mock
+    private Object mockObject;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        realObject = new Object();
+    }
+
+    @Test
+    public void fpShouldNotEqualMockObject() {
+        org.testng.Assert.assertNotEquals(realObject, mockObject);
+    }
+
 }
