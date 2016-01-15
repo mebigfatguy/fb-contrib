620d2c84431c6e4ecde5f6964c10a02b7dce4f47
thrawnca <shell_layer-github@yahoo.com.au>
2016-01-15 10:41:38 +1000

    ignore exception softening when there is no other way to do the job, #86

--- src/com/mebigfatguy/fbcontrib/detect/CollectionNamingConfusion.java
+++ src/com/mebigfatguy/fbcontrib/detect/CollectionNamingConfusion.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -99,13 +99,14 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
     /**
      * looks for a name that mentions a collection type but the wrong type for
      * the variable
-     * 
+     *
      * @param name
      *            the variable name
      * @param signature
      *            the variable signature
      * @return whether the name doesn't match the type
      */
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "EXS_EXCEPTION_SOFTENING_RETURN_FALSE", justification = "No other simple way to determine whether class exists")
     private boolean checkConfusedName(String name, String signature) {
         try {
             name = name.toLowerCase();
