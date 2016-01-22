3a87dfb4a736622750392365d8b9d8bbe23b680b
Dave Brosius <dbrosius@mebigfatguy.com>
2016-01-22 16:43:55 -0500

    javadoc

--- src/com/mebigfatguy/fbcontrib/detect/NonCollectionMethodUse.java
+++ src/com/mebigfatguy/fbcontrib/detect/NonCollectionMethodUse.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -29,29 +29,22 @@ import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 
 /**
- * looks for method calls to collection classes where the method is not defined
- * by the Collections interface, and an equivalent method exists in the
- * interface.
+ * looks for method calls to collection classes where the method is not defined by the Collections interface, and an equivalent method exists in the interface.
  */
 public class NonCollectionMethodUse extends BytecodeScanningDetector {
-    private static final Set<FQMethod> oldMethods = UnmodifiableSet.create(
-        new FQMethod("java/util/Hashtable", "contains", "(java/lang/Object)Z"),
-        new FQMethod("java/util/Hashtable", "elements", "()Ljava/util/Enumeration;"),
-        new FQMethod("java/util/Hashtable", "keys", "()Ljava/util/Enumeration;"),
-        new FQMethod("java/util/Vector", "addElement", "(Ljava/lang/Object;)V"),
-        new FQMethod("java/util/Vector", "elementAt", "(I)Ljava/lang/Object;"),
-        new FQMethod("java/util/Vector", "insertElementAt", "(Ljava/lang/Object;I)V"),
-        new FQMethod("java/util/Vector", "removeAllElements", "()V"),
-        new FQMethod("java/util/Vector", "removeElement", "(Ljava/lang/Object;)Z"),
-        new FQMethod("java/util/Vector", "removeElementAt", "(I)V"),
-        new FQMethod("java/util/Vector", "setElementAt", "(Ljava/lang/Object;I)V")
-    );
+    private static final Set<FQMethod> oldMethods = UnmodifiableSet.create(new FQMethod("java/util/Hashtable", "contains", "(java/lang/Object)Z"),
+            new FQMethod("java/util/Hashtable", "elements", "()Ljava/util/Enumeration;"),
+            new FQMethod("java/util/Hashtable", "keys", "()Ljava/util/Enumeration;"), new FQMethod("java/util/Vector", "addElement", "(Ljava/lang/Object;)V"),
+            new FQMethod("java/util/Vector", "elementAt", "(I)Ljava/lang/Object;"),
+            new FQMethod("java/util/Vector", "insertElementAt", "(Ljava/lang/Object;I)V"), new FQMethod("java/util/Vector", "removeAllElements", "()V"),
+            new FQMethod("java/util/Vector", "removeElement", "(Ljava/lang/Object;)Z"), new FQMethod("java/util/Vector", "removeElementAt", "(I)V"),
+            new FQMethod("java/util/Vector", "setElementAt", "(Ljava/lang/Object;I)V"));
 
     private BugReporter bugReporter;
 
     /**
      * constructs a NCMU detector given the reporter to report bugs on
-     * 
+     *
      * @param bugReporter
      *            the sync of bug reports
      */
@@ -59,6 +52,12 @@ public class NonCollectionMethodUse extends BytecodeScanningDetector {
         this.bugReporter = bugReporter;
     }
 
+    /**
+     * implements the visitor to look for method calls that are one of the old pre-collections1.2 set of methods
+     *
+     * @param seen
+     *            the currently parsed opcode
+     */
     @Override
     public void sawOpcode(int seen) {
         if (seen == INVOKEVIRTUAL) {
