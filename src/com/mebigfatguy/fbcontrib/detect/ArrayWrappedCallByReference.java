a1a40da51e47e76398ab27203a4fe1dcd7e313d6
Dave Brosius <dbrosius@mebigfatguy.com>
2016-01-23 15:51:02 -0500

    break apart big switch

--- src/com/mebigfatguy/fbcontrib/detect/ArrayWrappedCallByReference.java
+++ src/com/mebigfatguy/fbcontrib/detect/ArrayWrappedCallByReference.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -41,10 +41,8 @@ import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for methods that use an array of length one to pass a variable to
- * achieve call by pointer ala C++. It is better to define a proper return class
- * type that holds all the relevant information retrieved from the called
- * method.
+ * looks for methods that use an array of length one to pass a variable to achieve call by pointer ala C++. It is better to define a proper return class type
+ * that holds all the relevant information retrieved from the called method.
  */
 @CustomUserValue
 public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
@@ -70,7 +68,7 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
 
     /**
      * constructs a AWCBR detector given the reporter to report bugs on
-     * 
+     *
      * @param bugReporter
      *            the sync of bug reports
      */
@@ -98,7 +96,7 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
 
     /**
      * looks for methods that contain a NEWARRAY or ANEWARRAY opcodes
-     * 
+     *
      * @param method
      *            the context object of the current method
      * @return if the class uses synchronization
@@ -110,7 +108,7 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
 
     /**
      * implements the visitor to reset the stack of opcodes
-     * 
+     *
      * @param obj
      *            the context object for the currently parsed code block
      */
@@ -125,7 +123,7 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
 
     /**
      * implements the visitor to wrapped array parameter calls
-     * 
+     *
      * @param seen
      *            the currently visitor opcode
      */
@@ -136,61 +134,104 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
             stack.precomputation(this);
 
             switch (seen) {
-            case NEWARRAY:
-            case ANEWARRAY: {
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item itm = stack.getStackItem(0);
-                    Integer size = (Integer) itm.getConstant();
-                    if ((size != null) && (size.intValue() == 1)) {
-                        userValue = Values.NEGATIVE_ONE;
-                    }
-                }
-            }
-                break;
-
-            case IASTORE:
-            case LASTORE:
-            case FASTORE:
-            case DASTORE:
-            case AASTORE:
-            case BASTORE:
-            case CASTORE:
-            case SASTORE: {
-                if (stack.getStackDepth() >= 2) {
-                    OpcodeStack.Item itm = stack.getStackItem(2);
-                    int reg = itm.getRegisterNumber();
-                    if (reg != -1) {
-                        WrapperInfo wi = wrappers.get(Integer.valueOf(reg));
-                        if (wi != null) {
-                            OpcodeStack.Item elItm = stack.getStackItem(0);
-                            wi.wrappedReg = elItm.getRegisterNumber();
+                case NEWARRAY:
+                case ANEWARRAY: {
+                    if (stack.getStackDepth() > 0) {
+                        OpcodeStack.Item itm = stack.getStackItem(0);
+                        Integer size = (Integer) itm.getConstant();
+                        if ((size != null) && (size.intValue() == 1)) {
+                            userValue = Values.NEGATIVE_ONE;
                         }
-                    } else {
-                        OpcodeStack.Item elItm = stack.getStackItem(0);
-                        if (elItm.getRegisterNumber() != -1)
-                            userValue = Integer.valueOf(elItm.getRegisterNumber());
                     }
                 }
-            }
                 break;
 
-            case ASTORE:
-            case ASTORE_0:
-            case ASTORE_1:
-            case ASTORE_2:
-            case ASTORE_3: {
-                if (stack.getStackDepth() >= 1) {
-                    OpcodeStack.Item itm = stack.getStackItem(0);
-                    String sig = itm.getSignature();
-                    if ((sig.length() > 0) && (itm.getSignature().charAt(0) == '[')) {
-                        int reg = RegisterUtils.getAStoreReg(this, seen);
-                        Integer elReg = (Integer) itm.getUserValue();
-                        if (elReg != null)
-                            wrappers.put(Integer.valueOf(reg), new WrapperInfo(elReg.intValue()));
-                    } else {
+                case IASTORE:
+                case LASTORE:
+                case FASTORE:
+                case DASTORE:
+                case AASTORE:
+                case BASTORE:
+                case CASTORE:
+                case SASTORE: {
+                    userValue = processArrayElementStore();
+                }
+                break;
+
+                case ASTORE:
+                case ASTORE_0:
+                case ASTORE_1:
+                case ASTORE_2:
+                case ASTORE_3: {
+                    processLocalStore(seen);
+                }
+                break;
+
+                case INVOKEVIRTUAL:
+                case INVOKEINTERFACE:
+                case INVOKESPECIAL:
+                case INVOKESTATIC: {
+                    processMethodCall();
+                }
+                break;
+
+                case IALOAD:
+                case LALOAD:
+                case FALOAD:
+                case DALOAD:
+                case AALOAD:
+                case BALOAD:
+                case CALOAD:
+                case SALOAD: {
+                    if (stack.getStackDepth() >= 2) {
+                        OpcodeStack.Item arItm = stack.getStackItem(1);
+                        int arReg = arItm.getRegisterNumber();
+                        WrapperInfo wi = wrappers.get(Integer.valueOf(arReg));
+                        if ((wi != null) && wi.wasArg) {
+                            userValue = Integer.valueOf(wi.wrappedReg);
+                        }
+                    }
+                }
+                break;
+
+                case ALOAD:
+                case ALOAD_0:
+                case ALOAD_1:
+                case ALOAD_2:
+                case ALOAD_3: {
+                    int reg = RegisterUtils.getALoadReg(this, seen);
+                    WrapperInfo wi = wrappers.get(Integer.valueOf(reg));
+                    if (wi != null) {
+                        userValue = Integer.valueOf(wi.wrappedReg);
+                    }
+                }
+                break;
+
+                case ISTORE:
+                case ISTORE_0:
+                case ISTORE_1:
+                case ISTORE_2:
+                case ISTORE_3:
+                case LSTORE:
+                case LSTORE_0:
+                case LSTORE_1:
+                case LSTORE_2:
+                case LSTORE_3:
+                case DSTORE:
+                case DSTORE_0:
+                case DSTORE_1:
+                case DSTORE_2:
+                case DSTORE_3:
+                case FSTORE:
+                case FSTORE_0:
+                case FSTORE_1:
+                case FSTORE_2:
+                case FSTORE_3: {
+                    if (stack.getStackDepth() >= 1) {
+                        OpcodeStack.Item itm = stack.getStackItem(0);
                         Integer elReg = (Integer) itm.getUserValue();
                         if (elReg != null) {
-                            int reg = RegisterUtils.getAStoreReg(this, seen);
+                            int reg = RegisterUtils.getStoreReg(this, seen);
                             if (elReg.intValue() == reg) {
                                 bugReporter.reportBug(new BugInstance(this, BugType.AWCBR_ARRAY_WRAPPED_CALL_BY_REFERENCE.name(), NORMAL_PRIORITY)
                                         .addClass(this).addMethod(this).addSourceLine(this));
@@ -198,99 +239,9 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
                         }
                     }
                 }
-            }
                 break;
 
-            case INVOKEVIRTUAL:
-            case INVOKEINTERFACE:
-            case INVOKESPECIAL:
-            case INVOKESTATIC: {
-                if (!("invoke".equals(getNameConstantOperand()) && "java/lang/reflect/Method".equals(getClassConstantOperand()))) {
-                    String sig = getSigConstantOperand();
-                    Type[] args = Type.getArgumentTypes(sig);
-                    if (stack.getStackDepth() >= args.length) {
-                        for (int i = 0; i < args.length; i++) {
-                            Type t = args[i];
-                            String argSig = t.getSignature();
-                            if ((argSig.length() > 0) && (argSig.charAt(0) == '[')) {
-                                OpcodeStack.Item itm = stack.getStackItem(args.length - i - 1);
-                                int arrayReg = itm.getRegisterNumber();
-                                WrapperInfo wi = wrappers.get(Integer.valueOf(arrayReg));
-                                if (wi != null)
-                                    wi.wasArg = true;
-                            }
-                        }
-                    }
-                }
-            }
-                break;
-
-            case IALOAD:
-            case LALOAD:
-            case FALOAD:
-            case DALOAD:
-            case AALOAD:
-            case BALOAD:
-            case CALOAD:
-            case SALOAD: {
-                if (stack.getStackDepth() >= 2) {
-                    OpcodeStack.Item arItm = stack.getStackItem(1);
-                    int arReg = arItm.getRegisterNumber();
-                    WrapperInfo wi = wrappers.get(Integer.valueOf(arReg));
-                    if ((wi != null) && wi.wasArg) {
-                        userValue = Integer.valueOf(wi.wrappedReg);
-                    }
-                }
-            }
-                break;
-
-            case ALOAD:
-            case ALOAD_0:
-            case ALOAD_1:
-            case ALOAD_2:
-            case ALOAD_3: {
-                int reg = RegisterUtils.getALoadReg(this, seen);
-                WrapperInfo wi = wrappers.get(Integer.valueOf(reg));
-                if (wi != null)
-                    userValue = Integer.valueOf(wi.wrappedReg);
-            }
-                break;
-
-            case ISTORE:
-            case ISTORE_0:
-            case ISTORE_1:
-            case ISTORE_2:
-            case ISTORE_3:
-            case LSTORE:
-            case LSTORE_0:
-            case LSTORE_1:
-            case LSTORE_2:
-            case LSTORE_3:
-            case DSTORE:
-            case DSTORE_0:
-            case DSTORE_1:
-            case DSTORE_2:
-            case DSTORE_3:
-            case FSTORE:
-            case FSTORE_0:
-            case FSTORE_1:
-            case FSTORE_2:
-            case FSTORE_3: {
-                if (stack.getStackDepth() >= 1) {
-                    OpcodeStack.Item itm = stack.getStackItem(0);
-                    Integer elReg = (Integer) itm.getUserValue();
-                    if (elReg != null) {
-                        int reg = RegisterUtils.getStoreReg(this, seen);
-                        if (elReg.intValue() == reg) {
-                            bugReporter.reportBug(new BugInstance(this, BugType.AWCBR_ARRAY_WRAPPED_CALL_BY_REFERENCE.name(), NORMAL_PRIORITY).addClass(this)
-                                    .addMethod(this).addSourceLine(this));
-                        }
-                    }
-                }
-            }
-                break;
-
-            default:
+                default:
                 break;
             }
         } finally {
@@ -301,6 +252,88 @@ public class ArrayWrappedCallByReference extends BytecodeScanningDetector {
                 if (stack.getStackDepth() > 0) {
                     OpcodeStack.Item itm = stack.getStackItem(0);
                     itm.setUserValue(userValue);
+                }
+            }
+        }
+    }
+
+    /**
+     * looks for stores to registers, if that store is an array, builds a wrapper info for it and stores it in the wrappers collection. If it is a regular
+     * store, sees if this value, came from a wrapper array passed into a method, and if so reports it.
+     *
+     * @param seen
+     *            the currently parsed opcode
+     */
+    private void processLocalStore(int seen) {
+        if (stack.getStackDepth() >= 1) {
+            OpcodeStack.Item itm = stack.getStackItem(0);
+            String sig = itm.getSignature();
+            if ((sig.length() > 0) && (itm.getSignature().charAt(0) == '[')) {
+                int reg = RegisterUtils.getAStoreReg(this, seen);
+                Integer elReg = (Integer) itm.getUserValue();
+                if (elReg != null) {
+                    wrappers.put(Integer.valueOf(reg), new WrapperInfo(elReg.intValue()));
+                }
+            } else {
+                Integer elReg = (Integer) itm.getUserValue();
+                if (elReg != null) {
+                    int reg = RegisterUtils.getAStoreReg(this, seen);
+                    if (elReg.intValue() == reg) {
+                        bugReporter.reportBug(new BugInstance(this, BugType.AWCBR_ARRAY_WRAPPED_CALL_BY_REFERENCE.name(), NORMAL_PRIORITY).addClass(this)
+                                .addMethod(this).addSourceLine(this));
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * processes a store to an array element to see if this array is being used as a wrapper array, and if so records the register that is stored within it.
+     *
+     * @return the user value representing the stored register value
+     */
+    private Integer processArrayElementStore() {
+        if (stack.getStackDepth() >= 2) {
+            OpcodeStack.Item itm = stack.getStackItem(2);
+            int reg = itm.getRegisterNumber();
+            if (reg != -1) {
+                WrapperInfo wi = wrappers.get(Integer.valueOf(reg));
+                if (wi != null) {
+                    OpcodeStack.Item elItm = stack.getStackItem(0);
+                    wi.wrappedReg = elItm.getRegisterNumber();
+                }
+            } else {
+                OpcodeStack.Item elItm = stack.getStackItem(0);
+                if (elItm.getRegisterNumber() != -1) {
+                    return Integer.valueOf(elItm.getRegisterNumber());
+                }
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * processes a method call looking for parameters that are arrays. If this array was seen earlier as a simple wrapping array, then it marks it as being
+     * having been used as a parameter.
+     *
+     */
+    private void processMethodCall() {
+        if (!("invoke".equals(getNameConstantOperand()) && "java/lang/reflect/Method".equals(getClassConstantOperand()))) {
+            String sig = getSigConstantOperand();
+            Type[] args = Type.getArgumentTypes(sig);
+            if (stack.getStackDepth() >= args.length) {
+                for (int i = 0; i < args.length; i++) {
+                    Type t = args[i];
+                    String argSig = t.getSignature();
+                    if ((argSig.length() > 0) && (argSig.charAt(0) == '[')) {
+                        OpcodeStack.Item itm = stack.getStackItem(args.length - i - 1);
+                        int arrayReg = itm.getRegisterNumber();
+                        WrapperInfo wi = wrappers.get(Integer.valueOf(arrayReg));
+                        if (wi != null) {
+                            wi.wasArg = true;
+                        }
+                    }
                 }
             }
         }
