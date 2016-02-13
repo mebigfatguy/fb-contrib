e2f6287c14cc2a1e7c5d28bc60dc925a24e4d9ae
Dave Brosius <dbrosius@mebigfatguy.com>
2016-02-12 22:13:03 -0500

    javadoc

--- src/com/mebigfatguy/fbcontrib/detect/BackportReusePublicIdentifiers.java
+++ src/com/mebigfatguy/fbcontrib/detect/BackportReusePublicIdentifiers.java
@@ -2,17 +2,17 @@
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Bhaskar Maddala
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -31,20 +31,31 @@ import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 
 /**
- * Detects use of Backport concurrent classes. Updated/Efficient version of
- * these classes are available in versions of the JDK 5.0 and higher, and these
- * classes should only be used if you are targeting JDK 1.4 and lower.
- * 
+ * Detects use of Backport concurrent classes. Updated/Efficient version of these classes are available in versions of the JDK 5.0 and higher, and these classes
+ * should only be used if you are targeting JDK 1.4 and lower.
+ *
  * Finds usage of classes from backport utils package.
  */
 public class BackportReusePublicIdentifiers extends OpcodeStackDetector {
 
     private final BugReporter bugReporter;
 
+    /**
+     * constructs a BRPI detector given the reporter to report bugs on
+     *
+     * @param bugReporter
+     *            the sync of bug reports
+     */
     public BackportReusePublicIdentifiers(final BugReporter bugReporter) {
         this.bugReporter = bugReporter;
     }
 
+    /**
+     * overrides the visitor to make sure this is a 'modern' class better than 1.4
+     *
+     * @param classContext
+     *            the currently parsed class
+     */
     @Override
     public void visitClassContext(ClassContext classContext) {
         JavaClass cls = classContext.getJavaClass();
@@ -53,32 +64,41 @@ public class BackportReusePublicIdentifiers extends OpcodeStackDetector {
         }
     }
 
+    /**
+     * overrides the visitor to look for method calls to the emory backport concurrent library when the now built in versions are available
+     *
+     * @param seen
+     *            the currently parsed opcode
+     */
     @Override
     public void sawOpcode(int seen) {
         stack.precomputation(this);
 
         switch (seen) {
-        case INVOKESTATIC: {
-            String className = getClassConstantOperand();
-            if (className.startsWith("edu/emory/mathcs/backport/")) {
-                reportBug();
+            case INVOKESTATIC: {
+                String className = getClassConstantOperand();
+                if (className.startsWith("edu/emory/mathcs/backport/")) {
+                    reportBug();
+                }
             }
-        }
             break;
 
-        case INVOKESPECIAL: {
-            String className = getClassConstantOperand();
-            String methodName = getNameConstantOperand();
-            if (className.startsWith("edu/emory/mathcs/backport/") && Values.CONSTRUCTOR.equals(methodName)) {
-                reportBug();
+            case INVOKESPECIAL: {
+                String className = getClassConstantOperand();
+                String methodName = getNameConstantOperand();
+                if (className.startsWith("edu/emory/mathcs/backport/") && Values.CONSTRUCTOR.equals(methodName)) {
+                    reportBug();
+                }
             }
-        }
             break;
-        default:
+            default:
             break;
         }
     }
 
+    /**
+     * issues a new bug at this location
+     */
     private void reportBug() {
         bugReporter.reportBug(new BugInstance(this, BugType.BRPI_BACKPORT_REUSE_PUBLIC_IDENTIFIERS.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
                 .addSourceLine(this));
