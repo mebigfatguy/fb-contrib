6ddf98087443c204399de15285470dd1feb56ec1
Dave Brosius <dbrosius@mebigfatguy.com>
2016-02-13 10:25:45 -0500

    javadoc

--- src/com/mebigfatguy/fbcontrib/detect/CollectionNamingConfusion.java
+++ src/com/mebigfatguy/fbcontrib/detect/CollectionNamingConfusion.java
@@ -35,8 +35,7 @@ import edu.umd.cs.findbugs.ba.ClassContext;
 import edu.umd.cs.findbugs.visitclass.PreorderVisitor;
 
 /**
- * looks for fields and local variables that have Map, Set, List in their names
- * but the variable is a collection of a different basic type.
+ * looks for fields and local variables that have Map, Set, List in their names but the variable is a collection of a different basic type.
  */
 public class CollectionNamingConfusion extends PreorderVisitor implements Detector {
 
@@ -62,10 +61,22 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
     private BugReporter bugReporter;
     private ClassContext classContext;
 
+    /**
+     * constructs a CNC detector given the reporter to report bugs on
+     *
+     * @param bugReporter
+     *            the sync of bug reports
+     */
     public CollectionNamingConfusion(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
     }
 
+    /**
+     * overrides the visitor to make sure that the static initializer was able to load the map class
+     *
+     * @param classContext
+     *            the currently parsed class
+     */
     @Override
     public void visitClassContext(ClassContext classContext) {
         if (MAP_CLASS != null) {
@@ -74,6 +85,12 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
         }
     }
 
+    /**
+     * overrides the visitor to look for fields where the name has 'Map', 'Set', 'List' in it but the type of that field isn't that.
+     *
+     * @param obj
+     *            the currently parsed field
+     */
     @Override
     public void visitField(Field obj) {
         if (checkConfusedName(obj.getName(), obj.getSignature())) {
@@ -82,6 +99,13 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
         }
     }
 
+    /**
+     * overrides the visitor to look for local variables where the name has 'Map', 'Set', 'List' in it but the type of that field isn't that. note that this
+     * only is useful if compiled with debug labels.
+     *
+     * @param obj
+     *            the currently parsed method
+     */
     @Override
     public void visitMethod(Method obj) {
         LocalVariableTable lvt = obj.getLocalVariableTable();
@@ -97,8 +121,7 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
     }
 
     /**
-     * looks for a name that mentions a collection type but the wrong type for
-     * the variable
+     * looks for a name that mentions a collection type but the wrong type for the variable
      *
      * @param name
      *            the variable name
@@ -132,6 +155,9 @@ public class CollectionNamingConfusion extends PreorderVisitor implements Detect
         return false;
     }
 
+    /**
+     * implements the visitor by does nothing
+     */
     @Override
     public void report() {
         // not used, implements the interface
