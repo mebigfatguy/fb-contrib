99aa4dad0f1d97d90a2ffe45ffc8ae520524a770
thrawnca <shell_layer-github@yahoo.com.au>
2016-02-19 08:21:39 -0500

    GitHub Issue #104: remove redundant condition in UnboundMethodTemplateParameter

--- src/com/mebigfatguy/fbcontrib/detect/UnboundMethodTemplateParameter.java
+++ src/com/mebigfatguy/fbcontrib/detect/UnboundMethodTemplateParameter.java
@@ -77,12 +77,11 @@ public class UnboundMethodTemplateParameter extends PreorderVisitor implements D
                 TemplateSignature ts = parseSignatureAttribute((Signature) a);
                 if (ts != null) {
                     for (TemplateItem templateParm : ts.templateParameters) {
-                        if (!ts.signature.contains('T' + templateParm.templateType + ';') && !ts.signature.contains("[T" + templateParm.templateType + ';')) {
-                            if (!isTemplateParent(templateParm.templateType, ts.templateParameters)) {
-                                bugReporter.reportBug(new BugInstance(this, BugType.UMTP_UNBOUND_METHOD_TEMPLATE_PARAMETER.name(), NORMAL_PRIORITY)
-                                        .addClass(this).addMethod(this).addString("Template Parameter: " + templateParm.templateType));
-                                return;
-                            }
+                        if (!ts.signature.contains('T' + templateParm.templateType + ';')
+                                && !isTemplateParent(templateParm.templateType, ts.templateParameters)) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.UMTP_UNBOUND_METHOD_TEMPLATE_PARAMETER.name(), NORMAL_PRIORITY).addClass(this)
+                                    .addMethod(this).addString("Template Parameter: " + templateParm.templateType));
+                            return;
                         }
                     }
                 }
