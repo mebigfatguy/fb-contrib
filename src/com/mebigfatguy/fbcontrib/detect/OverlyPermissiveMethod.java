b98712061006e9fd6bc88b4cbd6cbf9602dadf8e
Dave Brosius <dbrosius@mebigfatguy.com>
2016-02-25 00:27:15 -0500

    GitHub Issue #107: Don't check for calling on this, if the method is static

--- src/com/mebigfatguy/fbcontrib/detect/OverlyPermissiveMethod.java
+++ src/com/mebigfatguy/fbcontrib/detect/OverlyPermissiveMethod.java
@@ -198,6 +198,10 @@ public class OverlyPermissiveMethod extends BytecodeScanningDetector {
      * @return when it is called on this or not
      */
     private boolean isCallingOnThis(String sig) {
+        if (getMethod().isStatic()) {
+            return false;
+        }
+        
         Type[] argTypes = Type.getArgumentTypes(sig);
         if (stack.getStackDepth() <= argTypes.length) {
             return false;
