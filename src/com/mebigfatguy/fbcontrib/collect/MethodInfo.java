a6da855af923ba59d3cdeea52fd640adccfc9f8f
Dave Brosius <dbrosius@mebigfatguy.com>
2016-05-01 20:08:59 -0400

    add hashCode/equals

--- src/com/mebigfatguy/fbcontrib/collect/MethodInfo.java
+++ src/com/mebigfatguy/fbcontrib/collect/MethodInfo.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -100,6 +100,23 @@ public class MethodInfo {
 
     public void setImmutabilityType(ImmutabilityType imType) {
         immutabilityOrdinal = (byte) imType.ordinal();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MethodInfo)) {
+            return false;
+        }
+
+        MethodInfo mi = (MethodInfo) o;
+
+        return (numMethodBytes == mi.numMethodBytes) && (numMethodCalls == mi.numMethodCalls) && (immutabilityOrdinal == mi.immutabilityOrdinal)
+                && (declaredAccess == mi.declaredAccess) && (isCalledType == mi.isCalledType);
+    }
+
+    @Override
+    public int hashCode() {
+        return numMethodBytes ^ numMethodCalls ^ immutabilityOrdinal ^ declaredAccess ^ isCalledType;
     }
 
     @Override
