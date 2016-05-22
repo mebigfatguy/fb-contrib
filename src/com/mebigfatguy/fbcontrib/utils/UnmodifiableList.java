ceb83eabd524671aae13c7e831ce0cfa95974f06
Dave Brosius <dbrosius@mebigfatguy.com>
2016-05-22 10:11:33 -0400

    add UnmodifiableList factory

---
+++ src/com/mebigfatguy/fbcontrib/utils/UnmodifiableList.java
@@ -0,0 +1,40 @@
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2016 Dave Brosius
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.mebigfatguy.fbcontrib.utils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * a factory for creating unmodifiable ArrayLists by presenting values to a var-args creator method.
+ */
+public class UnmodifiableList {
+
+    private UnmodifiableList() {
+    }
+
+    @SafeVarargs
+    public static <T> List<T> create(T... elements) {
+        List<T> l = new ArrayList<>(Arrays.<T> asList(elements));
+
+        return Collections.<T> unmodifiableList(l);
+    }
+}
