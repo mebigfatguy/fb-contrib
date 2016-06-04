9adf9e57fc57d76096039a3fa09b6842bca8234b
Dave Brosius <dbrosius@mebigfatguy.com>
2016-06-04 08:07:10 -0400

    simplfy

--- src/com/mebigfatguy/fbcontrib/detect/ArrayIndexOutOfBounds.java
+++ src/com/mebigfatguy/fbcontrib/detect/ArrayIndexOutOfBounds.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.Constant;
 import org.apache.bcel.classfile.ConstantInteger;
@@ -42,11 +41,9 @@ import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for usage of arrays with statically known indices where it can be
- * determined that the index is out of bounds based on how the array was
- * allocated. This delector is obviously limited to a small subset of out of
- * bounds exceptions that can be statically determined, and not the large family
- * of problems that can occur at runtime.
+ * looks for usage of arrays with statically known indices where it can be determined that the index is out of bounds based on how the array was allocated. This
+ * delector is obviously limited to a small subset of out of bounds exceptions that can be statically determined, and not the large family of problems that can
+ * occur at runtime.
  */
 @CustomUserValue
 public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
@@ -96,7 +93,7 @@ public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
         initializedRegs.clear();
         modifyRegs.clear();
         Type[] argTypes = m.getArgumentTypes();
-        int arg = ((m.getAccessFlags() & Constants.ACC_STATIC) != 0) ? 0 : 1;
+        int arg = m.isStatic() ? 0 : 1;
         for (Type argType : argTypes) {
             String argSig = argType.getSignature();
             initializedRegs.set(arg);
@@ -112,8 +109,7 @@ public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
     }
 
     /**
-     * overrides the visitor to look for stores to arrays that can be statically
-     * determined to be outside the bounds of the initialized array
+     * overrides the visitor to look for stores to arrays that can be statically determined to be outside the bounds of the initialized array
      *
      * @param seen
      *            the currently parsed opcode
@@ -126,146 +122,146 @@ public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
             stack.precomputation(this);
 
             switch (seen) {
-            case ICONST_0:
-            case ICONST_1:
-            case ICONST_2:
-            case ICONST_3:
-            case ICONST_4:
-            case ICONST_5:
-                size = Integer.valueOf(seen - ICONST_0);
-                sizeSet = true;
-                break;
-
-            case ILOAD:
-            case ILOAD_0:
-            case ILOAD_1:
-            case ILOAD_2:
-            case ILOAD_3: {
-                int reg = RegisterUtils.getLoadReg(this, seen);
-                if (modifyRegs.get(reg)) {
-                    modifyRegs.clear(reg);
+                case ICONST_0:
+                case ICONST_1:
+                case ICONST_2:
+                case ICONST_3:
+                case ICONST_4:
+                case ICONST_5:
+                    size = Integer.valueOf(seen - ICONST_0);
                     sizeSet = true;
-                }
-            }
                 break;
 
-            case BIPUSH:
-            case SIPUSH:
-                size = Integer.valueOf(getIntConstant());
-                sizeSet = true;
-                break;
-
-            case IINC:
-                modifyRegs.set(getRegisterOperand());
-                break;
-
-            case IADD:
-            case ISUB:
-            case IMUL:
-            case IDIV:
-            case F2I:
-            case D2I:
-            case L2I:
-                sizeSet = true;
-                break;
-
-            case ISTORE:
-            case ISTORE_0:
-            case ISTORE_1:
-            case ISTORE_2:
-            case ISTORE_3:
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    if (item.getUserValue() == null) {
-                        modifyRegs.set(getRegisterOperand());
+                case ILOAD:
+                case ILOAD_0:
+                case ILOAD_1:
+                case ILOAD_2:
+                case ILOAD_3: {
+                    int reg = RegisterUtils.getLoadReg(this, seen);
+                    if (modifyRegs.get(reg)) {
+                        modifyRegs.clear(reg);
+                        sizeSet = true;
                     }
                 }
                 break;
 
-            case LDC:
-                Constant c = getConstantRefOperand();
-                if (c instanceof ConstantInteger) {
-                    size = Integer.valueOf(((ConstantInteger) c).getBytes());
+                case BIPUSH:
+                case SIPUSH:
+                    size = Integer.valueOf(getIntConstant());
                     sizeSet = true;
-                }
                 break;
 
-            case NEWARRAY:
-            case ANEWARRAY:
-                if (stack.getStackDepth() >= 1) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    size = (Integer) item.getUserValue();
+                case IINC:
+                    modifyRegs.set(getRegisterOperand());
+                break;
+
+                case IADD:
+                case ISUB:
+                case IMUL:
+                case IDIV:
+                case F2I:
+                case D2I:
+                case L2I:
                     sizeSet = true;
-                }
                 break;
 
-            case IASTORE:
-            case LASTORE:
-            case FASTORE:
-            case DASTORE:
-            case AASTORE:
-            case BASTORE:
-            case CASTORE:
-            case SASTORE:
-                processArrayStore();
+                case ISTORE:
+                case ISTORE_0:
+                case ISTORE_1:
+                case ISTORE_2:
+                case ISTORE_3:
+                    if (stack.getStackDepth() > 0) {
+                        OpcodeStack.Item item = stack.getStackItem(0);
+                        if (item.getUserValue() == null) {
+                            modifyRegs.set(getRegisterOperand());
+                        }
+                    }
                 break;
 
-            case IALOAD:
-            case LALOAD:
-            case FALOAD:
-            case DALOAD:
-            case AALOAD:
-            case BALOAD:
-            case CALOAD:
-            case SALOAD:
-                processArrayLoad();
+                case LDC:
+                    Constant c = getConstantRefOperand();
+                    if (c instanceof ConstantInteger) {
+                        size = Integer.valueOf(((ConstantInteger) c).getBytes());
+                        sizeSet = true;
+                    }
                 break;
 
-            case ASTORE_0:
-            case ASTORE_1:
-            case ASTORE_2:
-            case ASTORE_3:
-            case ASTORE:
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item value = stack.getStackItem(0);
-                    if (!value.isNull()) {
+                case NEWARRAY:
+                case ANEWARRAY:
+                    if (stack.getStackDepth() >= 1) {
+                        OpcodeStack.Item item = stack.getStackItem(0);
+                        size = (Integer) item.getUserValue();
+                        sizeSet = true;
+                    }
+                break;
+
+                case IASTORE:
+                case LASTORE:
+                case FASTORE:
+                case DASTORE:
+                case AASTORE:
+                case BASTORE:
+                case CASTORE:
+                case SASTORE:
+                    processArrayStore();
+                break;
+
+                case IALOAD:
+                case LALOAD:
+                case FALOAD:
+                case DALOAD:
+                case AALOAD:
+                case BALOAD:
+                case CALOAD:
+                case SALOAD:
+                    processArrayLoad();
+                break;
+
+                case ASTORE_0:
+                case ASTORE_1:
+                case ASTORE_2:
+                case ASTORE_3:
+                case ASTORE:
+                    if (stack.getStackDepth() > 0) {
+                        OpcodeStack.Item value = stack.getStackItem(0);
+                        if (!value.isNull()) {
+                            initializedRegs.set(getRegisterOperand());
+                        }
+                    } else {
                         initializedRegs.set(getRegisterOperand());
                     }
-                } else {
-                    initializedRegs.set(getRegisterOperand());
-                }
                 break;
 
-            case IFEQ:
-            case IFNE:
-            case IFLT:
-            case IFGE:
-            case IFGT:
-            case IFLE:
-            case IF_ICMPEQ:
-            case IF_ICMPNE:
-            case IF_ICMPLT:
-            case IF_ICMPGE:
-            case IF_ICMPGT:
-            case IF_ICMPLE:
-            case IF_ACMPEQ:
-            case IF_ACMPNE:
-            case GOTO:
-            case GOTO_W:
-                int branchTarget = getBranchTarget();
-                Iterator<Map.Entry<Integer, Integer>> it = nullStoreToLocation.entrySet().iterator();
-                while (it.hasNext()) {
-                    Map.Entry<Integer, Integer> entry = it.next();
-                    int pc = entry.getValue().intValue();
-                    if (branchTarget < pc && initializedRegs.get(entry.getKey().intValue())) {
-                        it.remove();
+                case IFEQ:
+                case IFNE:
+                case IFLT:
+                case IFGE:
+                case IFGT:
+                case IFLE:
+                case IF_ICMPEQ:
+                case IF_ICMPNE:
+                case IF_ICMPLT:
+                case IF_ICMPGE:
+                case IF_ICMPGT:
+                case IF_ICMPLE:
+                case IF_ACMPEQ:
+                case IF_ACMPNE:
+                case GOTO:
+                case GOTO_W:
+                    int branchTarget = getBranchTarget();
+                    Iterator<Map.Entry<Integer, Integer>> it = nullStoreToLocation.entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry<Integer, Integer> entry = it.next();
+                        int pc = entry.getValue().intValue();
+                        if ((branchTarget < pc) && initializedRegs.get(entry.getKey().intValue())) {
+                            it.remove();
+                        }
                     }
-                }
             }
 
         } finally {
             stack.sawOpcode(this, seen);
-            if (sizeSet && stack.getStackDepth() >= 1) {
+            if (sizeSet && (stack.getStackDepth() >= 1)) {
                 OpcodeStack.Item item = stack.getStackItem(0);
                 item.setUserValue(size);
             }
@@ -279,9 +275,9 @@ public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
             if (index != null) {
                 OpcodeStack.Item arrayItem = stack.getStackItem(1);
                 Integer sz = (Integer) arrayItem.getUserValue();
-                if (sz != null && index.intValue() >= sz.intValue()) {
-                    bugReporter.reportBug(new BugInstance(this, BugType.AIOB_ARRAY_INDEX_OUT_OF_BOUNDS.name(), HIGH_PRIORITY).addClass(this)
-                            .addMethod(this).addSourceLine(this));
+                if ((sz != null) && (index.intValue() >= sz.intValue())) {
+                    bugReporter.reportBug(new BugInstance(this, BugType.AIOB_ARRAY_INDEX_OUT_OF_BOUNDS.name(), HIGH_PRIORITY).addClass(this).addMethod(this)
+                            .addSourceLine(this));
                 }
             }
         }
@@ -294,13 +290,13 @@ public class ArrayIndexOutOfBounds extends BytecodeScanningDetector {
             if (index != null) {
                 OpcodeStack.Item arrayItem = stack.getStackItem(2);
                 Integer sz = (Integer) arrayItem.getUserValue();
-                if (sz != null && index.intValue() >= sz.intValue()) {
-                    bugReporter.reportBug(new BugInstance(this, BugType.AIOB_ARRAY_INDEX_OUT_OF_BOUNDS.name(), HIGH_PRIORITY).addClass(this)
-                            .addMethod(this).addSourceLine(this));
+                if ((sz != null) && (index.intValue() >= sz.intValue())) {
+                    bugReporter.reportBug(new BugInstance(this, BugType.AIOB_ARRAY_INDEX_OUT_OF_BOUNDS.name(), HIGH_PRIORITY).addClass(this).addMethod(this)
+                            .addSourceLine(this));
                 }
 
                 int reg = arrayItem.getRegisterNumber();
-                if (reg >= 0 && !initializedRegs.get(reg)) {
+                if ((reg >= 0) && !initializedRegs.get(reg)) {
                     nullStoreToLocation.put(Integer.valueOf(reg), Integer.valueOf(getPC()));
                 }
             }
