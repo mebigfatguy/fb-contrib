047c465a1e8b39e2e309d6641e9da8eea9363b02
Dave Brosius <dbrosius@mebigfatguy.com>
2016-06-18 00:39:41 -0400

    suppress warnings

--- src/com/mebigfatguy/fbcontrib/utils/CollectionUtils.java
+++ src/com/mebigfatguy/fbcontrib/utils/CollectionUtils.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -22,8 +22,7 @@ import org.apache.bcel.Repository;
 import org.apache.bcel.classfile.JavaClass;
 
 /**
- * a collection of static methods for determining if a class belongs to one or
- * more collection types.
+ * a collection of static methods for determining if a class belongs to one or more collection types.
  */
 public class CollectionUtils {
 
@@ -37,6 +36,7 @@ public class CollectionUtils {
             SET_CLASS = Repository.lookupClass("java/util/Set");
             MAP_CLASS = Repository.lookupClass("java/util/Map");
         } catch (ClassNotFoundException cnfe) {
+            // no bugReporter at present, so nothing to do
         }
     }
 
@@ -48,11 +48,11 @@ public class CollectionUtils {
 
     /**
      * determines if the current class name is derived from List, Set or Map
-     * 
+     *
      * @param clsName
      *            the class to determine it's parentage
      * @return if the class is a List, Set or Map
-     * 
+     *
      * @throws ClassNotFoundException
      *             if the cls parameter can't be found
      */
