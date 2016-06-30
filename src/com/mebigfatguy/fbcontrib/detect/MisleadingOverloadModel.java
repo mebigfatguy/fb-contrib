28b9d0d75a71b11230e5700ae45f85c61c3325df
Dave Brosius <dbrosius@mebigfatguy.com>
2016-06-29 23:10:11 -0400

    warnings

--- src/com/mebigfatguy/fbcontrib/detect/MisleadingOverloadModel.java
+++ src/com/mebigfatguy/fbcontrib/detect/MisleadingOverloadModel.java
@@ -1,17 +1,17 @@
 /*
  * fb-contrib - Auxiliary detectors for Java programs
  * Copyright (C) 2005-2016 Dave Brosius
- * 
+ *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -34,9 +34,8 @@ import edu.umd.cs.findbugs.ba.XFactory;
 import edu.umd.cs.findbugs.visitclass.PreorderVisitor;
 
 /**
- * looks for classes that define both static and instance methods with the same
- * name. This 'overloading' is confusing as one method is instance based the
- * other class based, and points to a confusion in implementation.
+ * looks for classes that define both static and instance methods with the same name. This 'overloading' is confusing as one method is instance based the other
+ * class based, and points to a confusion in implementation.
  */
 public class MisleadingOverloadModel extends PreorderVisitor implements Detector {
     enum MethodFoundType {
@@ -47,7 +46,7 @@ public class MisleadingOverloadModel extends PreorderVisitor implements Detector
 
     /**
      * constructs a MOM detector given the reporter to report bugs on
-     * 
+     *
      * @param bugReporter
      *            the sync of bug reports
      */
@@ -60,23 +59,25 @@ public class MisleadingOverloadModel extends PreorderVisitor implements Detector
         JavaClass cls = classContext.getJavaClass();
         String clsName = cls.getClassName();
         Method[] methods = cls.getMethods();
-        Map<String, MethodFoundType> declMethods = new HashMap<String, MethodFoundType>(methods.length);
+        Map<String, MethodFoundType> declMethods = new HashMap<>(methods.length);
         for (Method m : methods) {
             String methodName = m.getName();
             boolean report;
             MethodFoundType newType;
             if (m.isStatic()) {
                 report = declMethods.get(methodName) == MethodFoundType.Instance;
-                if (report)
+                if (report) {
                     newType = MethodFoundType.Both;
-                else
+                } else {
                     newType = MethodFoundType.Static;
+                }
             } else {
                 report = declMethods.get(m.getName()) == MethodFoundType.Static;
-                if (report)
+                if (report) {
                     newType = MethodFoundType.Both;
-                else
+                } else {
                     newType = MethodFoundType.Instance;
+                }
             }
 
             declMethods.put(methodName, newType);
@@ -90,5 +91,6 @@ public class MisleadingOverloadModel extends PreorderVisitor implements Detector
     /** implements the visitor to do nothing */
     @Override
     public void report() {
+        // required by the interface, but not needed
     }
 }
