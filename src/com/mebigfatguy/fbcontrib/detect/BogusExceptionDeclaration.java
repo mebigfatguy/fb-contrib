7584219f91fc6da0239c106deef28ef6f147dfde
Dave Brosius <dbrosius@mebigfatguy.com>
2016-08-03 21:30:17 -0400

    no longer need to check each op, whether to continue

--- src/com/mebigfatguy/fbcontrib/detect/BogusExceptionDeclaration.java
+++ src/com/mebigfatguy/fbcontrib/detect/BogusExceptionDeclaration.java
@@ -218,10 +218,6 @@ public class BogusExceptionDeclaration extends BytecodeScanningDetector {
      */
     @Override
     public void sawOpcode(int seen) {
-        if (declaredCheckedExceptions.isEmpty()) {
-            return;
-        }
-
         try {
 
             stack.precomputation(this);
