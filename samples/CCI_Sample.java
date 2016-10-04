6ded2b0eab08d0050338253908619c2cf5a357ab
Dave Brosius <dbrosius@mebigfatguy.com>
2016-10-03 21:43:19 -0400

    add somebasic logic to CCI (not working, yet)

---
+++ samples/CCI_Sample.java
@@ -0,0 +1,21 @@
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CCI_Sample {
+
+    private Map<String, Set<String>> map = new ConcurrentHashMap<>();
+
+    public void update(String key, String value) {
+
+        Set<String> values = map.get(key);
+
+        if (values == null) {
+            values = Collections.<String> newSetFromMap(new ConcurrentHashMap<String, Boolean>());
+            map.put(key, values);
+        }
+
+        values.add(value);
+    }
+}
--- src/com/mebigfatguy/fbcontrib/detect/ConcurrentCollectionIssues.java
+++ src/com/mebigfatguy/fbcontrib/detect/ConcurrentCollectionIssues.java
@@ -19,8 +19,14 @@
  */
 package com.mebigfatguy.fbcontrib.detect;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.bcel.classfile.Code;
 
+import com.mebigfatguy.fbcontrib.utils.BugType;
+
+import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
 import edu.umd.cs.findbugs.OpcodeStack;
@@ -32,6 +38,11 @@ public class ConcurrentCollectionIssues extends BytecodeScanningDetector {
 
     private final BugReporter bugReporter;
     private OpcodeStack stack;
+    private Map<String, CCIUserValue> fieldUserValues;
+
+    private enum CCIUserValue {
+        CONCURRENT_HASHMAP, CONCURRENT_HASHMAP_VALUE;
+    };
 
     /**
      * constructs a CCI detector given the reporter to report bugs on
@@ -53,8 +64,10 @@ public class ConcurrentCollectionIssues extends BytecodeScanningDetector {
     public void visitClassContext(ClassContext classContext) {
         try {
             stack = new OpcodeStack();
+            fieldUserValues = new HashMap<>();
             classContext.getJavaClass().accept(this);
         } finally {
+            fieldUserValues = null;
             stack = null;
         }
     }
@@ -76,11 +89,60 @@ public class ConcurrentCollectionIssues extends BytecodeScanningDetector {
      */
     @Override
     public void sawOpcode(int seen) {
+        CCIUserValue userValue = null;
         try {
             stack.precomputation(this);
 
+            switch (seen) {
+                case INVOKESPECIAL:
+                    if ("java/util/concurrent/ConcurrentHashMap".equals(getClassConstantOperand()) && "<init>".equals(getNameConstantOperand())) {
+                        userValue = CCIUserValue.CONCURRENT_HASHMAP;
+                    }
+                break;
+
+                case INVOKEINTERFACE:
+                case INVOKEVIRTUAL:
+                    if ("get".equals(getNameConstantOperand())) {
+                        if (stack.getStackDepth() >= 2) {
+                            OpcodeStack.Item itm = stack.getStackItem(1);
+                            if (itm.getUserValue() == CCIUserValue.CONCURRENT_HASHMAP) {
+                                userValue = CCIUserValue.CONCURRENT_HASHMAP_VALUE;
+                            }
+                        }
+                    } else if ("put".equals(getNameConstantOperand())) {
+                        if (stack.getStackDepth() >= 3) {
+                            OpcodeStack.Item mapItem = stack.getStackItem(2);
+                            OpcodeStack.Item valueItem = stack.getStackItem(0);
+
+                            if (mapItem.getUserValue() == CCIUserValue.CONCURRENT_HASHMAP) {
+                                bugReporter.reportBug(new BugInstance(this, BugType.CCI_CONCURRENT_COLLECTION_ISSUES_USE_PUT_IS_RACY.name(), NORMAL_PRIORITY)
+                                        .addClass(this).addMethod(this).addSourceLine(this));
+                            }
+                        }
+                    }
+                break;
+
+                case PUTFIELD:
+                case PUTSTATIC:
+                    if (stack.getStackDepth() >= 1) {
+                        OpcodeStack.Item itm = stack.getStackItem(0);
+                        CCIUserValue uv = (CCIUserValue) itm.getUserValue();
+                        fieldUserValues.put(getNameConstantOperand(), uv);
+                    }
+                break;
+
+                case GETFIELD:
+                case GETSTATIC:
+                    userValue = fieldUserValues.get(getNameConstantOperand());
+                break;
+            }
+
         } finally {
             stack.sawOpcode(this, seen);
+            if ((userValue != null) && (stack.getStackDepth() > 0)) {
+                OpcodeStack.Item itm = stack.getStackItem(0);
+                itm.setUserValue(userValue);
+            }
         }
     }
 }
