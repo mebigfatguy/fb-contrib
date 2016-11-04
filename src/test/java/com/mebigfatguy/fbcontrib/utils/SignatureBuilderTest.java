f344860e9f49c7b8384a886b8fb335a1c58b3cfa
Dave Brosius <dbrosius@mebigfatguy.com>
2016-11-03 20:46:26 -0400

    add license headers

--- src/test/java/com/mebigfatguy/fbcontrib/utils/SignatureBuilderTest.java
+++ src/test/java/com/mebigfatguy/fbcontrib/utils/SignatureBuilderTest.java
@@ -1,8 +1,26 @@
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2016 ThrawnCA
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.mebigfatguy.fbcontrib.utils;
 
-import org.testng.annotations.*;
+import static org.testng.Assert.assertEquals;
 
-import static org.testng.Assert.*;
+import org.testng.annotations.Test;
 
 public class SignatureBuilderTest {
 
@@ -13,32 +31,38 @@ public class SignatureBuilderTest {
 
     @Test
     public void shouldConvertClassnamesToSignatures() {
-        assertEquals(new SignatureBuilder().withMethodName("classNames").withParamTypes("java.lang.String").withReturnType("java/util/Collection").build(), "classNames(Ljava/lang/String;)Ljava/util/Collection;");
+        assertEquals(new SignatureBuilder().withMethodName("classNames").withParamTypes("java.lang.String").withReturnType("java/util/Collection").build(),
+                "classNames(Ljava/lang/String;)Ljava/util/Collection;");
     }
 
     @Test
     public void shouldPreserveSignatures() {
-        assertEquals(new SignatureBuilder().withMethodName("signatures").withParamTypes("Ljava/lang/Object;").withReturnType("Ljava/util/Date;").build(), "signatures(Ljava/lang/Object;)Ljava/util/Date;");
+        assertEquals(new SignatureBuilder().withMethodName("signatures").withParamTypes("Ljava/lang/Object;").withReturnType("Ljava/util/Date;").build(),
+                "signatures(Ljava/lang/Object;)Ljava/util/Date;");
     }
 
     @Test
     public void shouldOverrideExistingParamTypes() {
-        assertEquals(new SignatureBuilder().withMethodName("overrideParams").withParamTypes("java/lang/Object").withParamTypes("java/lang/String", "java/lang/Boolean").build(), "overrideParams(Ljava/lang/String;Ljava/lang/Boolean;)V");
+        assertEquals(new SignatureBuilder().withMethodName("overrideParams").withParamTypes("java/lang/Object")
+                .withParamTypes("java/lang/String", "java/lang/Boolean").build(), "overrideParams(Ljava/lang/String;Ljava/lang/Boolean;)V");
     }
 
     @Test
     public void shouldAcceptPrimitiveTypes() {
-        assertEquals(new SignatureBuilder().withMethodName("primitives").withParamTypes("B", "S", "C", "I", "J", "java.lang.Object", "Z", "F").withReturnType("D").build(), "primitives(BSCIJLjava/lang/Object;ZF)D");
+        assertEquals(new SignatureBuilder().withMethodName("primitives").withParamTypes("B", "S", "C", "I", "J", "java.lang.Object", "Z", "F")
+                .withReturnType("D").build(), "primitives(BSCIJLjava/lang/Object;ZF)D");
     }
 
     @Test
     public void shouldAcceptClasses() {
-        assertEquals(new SignatureBuilder().withMethodName("classes").withParamTypes(String.class, Object.class).withReturnType(Integer.class).build(), "classes(Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/Integer;");
+        assertEquals(new SignatureBuilder().withMethodName("classes").withParamTypes(String.class, Object.class).withReturnType(Integer.class).build(),
+                "classes(Ljava/lang/String;Ljava/lang/Object;)Ljava/lang/Integer;");
     }
 
     @Test
     public void shouldBlankReturnTypeWhenNeeded() {
-        assertEquals(new SignatureBuilder().withMethodName("noReturn").withParamTypes("java.lang.Object").withoutReturnType().build(), "noReturn(Ljava/lang/Object;)");
+        assertEquals(new SignatureBuilder().withMethodName("noReturn").withParamTypes("java.lang.Object").withoutReturnType().build(),
+                "noReturn(Ljava/lang/Object;)");
     }
 
 }
--- src/test/java/com/mebigfatguy/fbcontrib/utils/SignatureUtilsTest.java
+++ src/test/java/com/mebigfatguy/fbcontrib/utils/SignatureUtilsTest.java
@@ -1,91 +1,61 @@
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2016 ThrawnCA
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 package com.mebigfatguy.fbcontrib.utils;
+
+import static org.testng.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.testng.annotations.*;
-
-import static org.testng.Assert.*;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 public class SignatureUtilsTest {
 
     @DataProvider(name = "namesToSignatures")
     public Object[][] namesToSignatures() {
         String sigString = "Ljava/lang/String;";
-        return new Object[][] {
-            {"java.lang.String", sigString},
-            {"java/lang/String", sigString},
-            {sigString, sigString},
-            {"B", "B"},
-            {"S", "S"},
-            {"C", "C"},
-            {"I", "I"},
-            {"J", "J"},
-            {"F", "F"},
-            {"D", "D"},
-            {"Z", "Z"},
-            {"", ""},
-            {null, null},
-        };
+        return new Object[][] { { "java.lang.String", sigString }, { "java/lang/String", sigString }, { sigString, sigString }, { "B", "B" }, { "S", "S" },
+                { "C", "C" }, { "I", "I" }, { "J", "J" }, { "F", "F" }, { "D", "D" }, { "Z", "Z" }, { "", "" }, { null, null }, };
     }
 
     @DataProvider(name = "signaturesToSlashedNames")
     public Object[][] signaturesToSlashedNames() {
         String slashedString = "java/lang/String";
-        return new Object[][] {
-            {"Ljava/lang/String;", slashedString},
-            {slashedString, slashedString},
-            {"B", "B"},
-            {"S", "S"},
-            {"C", "C"},
-            {"I", "I"},
-            {"J", "J"},
-            {"F", "F"},
-            {"D", "D"},
-            {"Z", "Z"},
-            {"", ""},
-            {null, null},
-        };
+        return new Object[][] { { "Ljava/lang/String;", slashedString }, { slashedString, slashedString }, { "B", "B" }, { "S", "S" }, { "C", "C" },
+                { "I", "I" }, { "J", "J" }, { "F", "F" }, { "D", "D" }, { "Z", "Z" }, { "", "" }, { null, null }, };
     }
 
     @DataProvider(name = "signaturesToDottedNames")
     public Object[][] signaturesToDottedNames() {
         String dottedString = "java.lang.String";
-        return new Object[][] {
-            {"Ljava/lang/String;", dottedString},
-            {"java/lang/String", dottedString},
-            {dottedString, dottedString},
-            {"B", "B"},
-            {"S", "S"},
-            {"C", "C"},
-            {"I", "I"},
-            {"J", "J"},
-            {"F", "F"},
-            {"D", "D"},
-            {"Z", "Z"},
-            {"", ""},
-        };
+        return new Object[][] { { "Ljava/lang/String;", dottedString }, { "java/lang/String", dottedString }, { dottedString, dottedString }, { "B", "B" },
+                { "S", "S" }, { "C", "C" }, { "I", "I" }, { "J", "J" }, { "F", "F" }, { "D", "D" }, { "Z", "Z" }, { "", "" }, };
     }
 
     @DataProvider(name = "namesToArrays")
     public Object[][] namesToArrays() {
         String sigStringArray = "[Ljava/lang/String;";
-        return new Object[][] {
-            {"java.lang.String", sigStringArray},
-            {"java/lang/String", sigStringArray},
-            {sigStringArray, "[[Ljava/lang/String;"},
-            {"B", "[B"},
-            {"S", "[S"},
-            {"C", "[C"},
-            {"I", "[I"},
-            {"J", "[J"},
-            {"F", "[F"},
-            {"D", "[D"},
-            {"Z", "[Z"},
-            {"", ""},
-            {null, null},
-        };
+        return new Object[][] { { "java.lang.String", sigStringArray }, { "java/lang/String", sigStringArray }, { sigStringArray, "[[Ljava/lang/String;" },
+                { "B", "[B" }, { "S", "[S" }, { "C", "[C" }, { "I", "[I" }, { "J", "[J" }, { "F", "[F" }, { "D", "[D" }, { "Z", "[Z" }, { "", "" },
+                { null, null }, };
     }
 
     @Test
