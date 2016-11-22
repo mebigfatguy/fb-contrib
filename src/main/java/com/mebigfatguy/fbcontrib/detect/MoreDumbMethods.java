61f3d2d9c4e387b473483b556ce2c6076e48a628
Dave Brosius <dbrosius@mebigfatguy.com>
2016-11-21 19:19:06 -0500

    make constants static

--- src/main/java/com/mebigfatguy/fbcontrib/detect/MoreDumbMethods.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/MoreDumbMethods.java
@@ -40,23 +40,25 @@ import edu.umd.cs.findbugs.ba.ClassContext;
  * looks for method calls that are unsafe or might indicate bugs.
  */
 public class MoreDumbMethods extends BytecodeScanningDetector {
-    private final static Map<FQMethod, ReportInfo> dumbMethods = new HashMap<FQMethod, ReportInfo>();
+    private final static Map<FQMethod, ReportInfo> dumbMethods = new HashMap<>();
 
     private static final Set<ReportInfo> assertableReports = UnmodifiableSet.create(new ReportInfo("MDM_LOCK_ISLOCKED", LOW_PRIORITY));
 
-    private final BugReporter bugReporter;
-
-    private boolean sawAssertionDisabled;
-    private int assertionEnd;
+    private static final String byteArrayToVoid = new SignatureBuilder().withParamTypes(SignatureBuilder.SIG_BYTE_ARRAY).toString();
+    private static final String intToByteArray = new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT).withReturnType(SignatureBuilder.SIG_BYTE_ARRAY)
+            .toString();
 
     static {
         dumbMethods.put(new FQMethod("java/lang/Runtime", "exit", SignatureBuilder.SIG_INT_TO_VOID), new ReportInfo("MDM_RUNTIME_EXIT_OR_HALT", LOW_PRIORITY));
         dumbMethods.put(new FQMethod("java/lang/Runtime", "halt", SignatureBuilder.SIG_INT_TO_VOID), new ReportInfo("MDM_RUNTIME_EXIT_OR_HALT", HIGH_PRIORITY));
 
-        dumbMethods.put(new FQMethod("java/lang/Runtime", "runFinalization", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_RUNFINALIZATION", NORMAL_PRIORITY));
-        dumbMethods.put(new FQMethod("java/lang/System", "runFinalization", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_RUNFINALIZATION", NORMAL_PRIORITY));
+        dumbMethods.put(new FQMethod("java/lang/Runtime", "runFinalization", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_RUNFINALIZATION", NORMAL_PRIORITY));
+        dumbMethods.put(new FQMethod("java/lang/System", "runFinalization", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_RUNFINALIZATION", NORMAL_PRIORITY));
 
-        dumbMethods.put(new FQMethod("java/math/BigDecimal", "equals", SignatureBuilder.SIG_OBJECT_TO_BOOLEAN), new ReportInfo("MDM_BIGDECIMAL_EQUALS", NORMAL_PRIORITY));
+        dumbMethods.put(new FQMethod("java/math/BigDecimal", "equals", SignatureBuilder.SIG_OBJECT_TO_BOOLEAN),
+                new ReportInfo("MDM_BIGDECIMAL_EQUALS", NORMAL_PRIORITY));
 
         //
         // Network checks
@@ -64,57 +66,84 @@ public class MoreDumbMethods extends BytecodeScanningDetector {
         dumbMethods.put(new FQMethod("java/net/InetAddress", "getLocalHost", new SignatureBuilder().withReturnType("java/net/InetAddress").toString()),
                 new ReportInfo("MDM_INETADDRESS_GETLOCALHOST", NORMAL_PRIORITY));
 
-        dumbMethods.put(new FQMethod("java/net/ServerSocket", Values.CONSTRUCTOR, SignatureBuilder.SIG_INT_TO_VOID), new ReportInfo("MDM_PROMISCUOUS_SERVERSOCKET", NORMAL_PRIORITY));
-        dumbMethods.put(new FQMethod("java/net/ServerSocket", Values.CONSTRUCTOR, new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT, Values.SIG_PRIMITIVE_INT).toString()),
+        dumbMethods.put(new FQMethod("java/net/ServerSocket", Values.CONSTRUCTOR, SignatureBuilder.SIG_INT_TO_VOID),
                 new ReportInfo("MDM_PROMISCUOUS_SERVERSOCKET", NORMAL_PRIORITY));
-        dumbMethods.put(new FQMethod("javax/net/ServerSocketFactory", "createServerSocket", new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT).withReturnType("java/net/ServerSocket").toString()),
+        dumbMethods.put(
+                new FQMethod("java/net/ServerSocket", Values.CONSTRUCTOR,
+                        new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT, Values.SIG_PRIMITIVE_INT).toString()),
+                new ReportInfo("MDM_PROMISCUOUS_SERVERSOCKET", NORMAL_PRIORITY));
+        dumbMethods.put(
+                new FQMethod("javax/net/ServerSocketFactory", "createServerSocket",
+                        new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT).withReturnType("java/net/ServerSocket").toString()),
                 new ReportInfo("MDM_PROMISCUOUS_SERVERSOCKET", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("javax/net/ServerSocketFactory", "createServerSocket", new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT, Values.SIG_PRIMITIVE_INT).withReturnType("java/net/ServerSocket").toString()),
+        dumbMethods.put(
+                new FQMethod("javax/net/ServerSocketFactory", "createServerSocket", new SignatureBuilder()
+                        .withParamTypes(Values.SIG_PRIMITIVE_INT, Values.SIG_PRIMITIVE_INT).withReturnType("java/net/ServerSocket").toString()),
                 new ReportInfo("MDM_PROMISCUOUS_SERVERSOCKET", LOW_PRIORITY));
 
         //
         // Random Number Generator checks
         //
-        dumbMethods.put(new FQMethod("java/util/Random", Values.CONSTRUCTOR, SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_RANDOM_SEED", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/Random", Values.CONSTRUCTOR, SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_RANDOM_SEED", LOW_PRIORITY));
 
         //
         // Thread checks
         //
-        dumbMethods.put(new FQMethod("java/lang/Thread", "getPriority", SignatureBuilder.SIG_VOID_TO_INT), new ReportInfo("MDM_THREAD_PRIORITIES", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/lang/Thread", "setPriority", SignatureBuilder.SIG_INT_TO_VOID), new ReportInfo("MDM_THREAD_PRIORITIES", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/lang/Thread", "getPriority", SignatureBuilder.SIG_VOID_TO_INT),
+                new ReportInfo("MDM_THREAD_PRIORITIES", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/lang/Thread", "setPriority", SignatureBuilder.SIG_INT_TO_VOID),
+                new ReportInfo("MDM_THREAD_PRIORITIES", LOW_PRIORITY));
 
         dumbMethods.put(new FQMethod("java/lang/Thread", "sleep", SignatureBuilder.SIG_LONG_TO_VOID), new ReportInfo("MDM_THREAD_YIELD", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/lang/Thread", "sleep", SignatureBuilder.SIG_LONG_AND_INT_TO_VOID),
-                new ReportInfo("MDM_THREAD_YIELD", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/lang/Thread", "sleep", SignatureBuilder.SIG_LONG_AND_INT_TO_VOID), new ReportInfo("MDM_THREAD_YIELD", LOW_PRIORITY));
         dumbMethods.put(new FQMethod("java/lang/Thread", "yield", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_THREAD_YIELD", NORMAL_PRIORITY));
 
         dumbMethods.put(new FQMethod("java/lang/Thread", "join", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod(Values.SLASHED_JAVA_LANG_OBJECT, "wait", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Condition", "await", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "lock", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "lockInterruptibly", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "lock", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod(Values.SLASHED_JAVA_LANG_OBJECT, "wait", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Condition", "await", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "lock", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "lockInterruptibly", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "lock", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
         dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "lockInterruptibly", SignatureBuilder.SIG_VOID_TO_VOID),
                 new ReportInfo("MDM_WAIT_WITHOUT_TIMEOUT", LOW_PRIORITY));
 
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Condition", "signal", SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_SIGNAL_NOT_SIGNALALL", NORMAL_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Condition", "signal", SignatureBuilder.SIG_VOID_TO_VOID),
+                new ReportInfo("MDM_SIGNAL_NOT_SIGNALALL", NORMAL_PRIORITY));
 
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "tryLock", SignatureBuilder.SIG_VOID_TO_BOOLEAN), new ReportInfo("MDM_THREAD_FAIRNESS", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "tryLock", SignatureBuilder.SIG_VOID_TO_BOOLEAN), new ReportInfo("MDM_THREAD_FAIRNESS", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/Lock", "tryLock", SignatureBuilder.SIG_VOID_TO_BOOLEAN),
+                new ReportInfo("MDM_THREAD_FAIRNESS", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "tryLock", SignatureBuilder.SIG_VOID_TO_BOOLEAN),
+                new ReportInfo("MDM_THREAD_FAIRNESS", LOW_PRIORITY));
 
         dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "isHeldByCurrentThread", SignatureBuilder.SIG_VOID_TO_BOOLEAN),
                 new ReportInfo("MDM_LOCK_ISLOCKED", LOW_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "isLocked", SignatureBuilder.SIG_VOID_TO_BOOLEAN), new ReportInfo("MDM_LOCK_ISLOCKED", LOW_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/concurrent/locks/ReentrantLock", "isLocked", SignatureBuilder.SIG_VOID_TO_BOOLEAN),
+                new ReportInfo("MDM_LOCK_ISLOCKED", LOW_PRIORITY));
 
         //
         // String checks
         //
-        dumbMethods.put(new FQMethod(Values.SLASHED_JAVA_LANG_STRING, Values.CONSTRUCTOR, new SignatureBuilder().withParamTypes(SignatureBuilder.SIG_BYTE_ARRAY).toString()),
+        dumbMethods.put(
+                new FQMethod(Values.SLASHED_JAVA_LANG_STRING, Values.CONSTRUCTOR,
+                        new SignatureBuilder().withParamTypes(SignatureBuilder.SIG_BYTE_ARRAY).toString()),
                 new ReportInfo("MDM_STRING_BYTES_ENCODING", NORMAL_PRIORITY));
-        dumbMethods.put(new FQMethod(Values.SLASHED_JAVA_LANG_STRING, "getBytes", new SignatureBuilder().withReturnType(SignatureBuilder.SIG_BYTE_ARRAY).toString()),
+        dumbMethods.put(
+                new FQMethod(Values.SLASHED_JAVA_LANG_STRING, "getBytes", new SignatureBuilder().withReturnType(SignatureBuilder.SIG_BYTE_ARRAY).toString()),
                 new ReportInfo("MDM_STRING_BYTES_ENCODING", NORMAL_PRIORITY));
-        dumbMethods.put(new FQMethod("java/util/Locale", "setDefault", new SignatureBuilder().withParamTypes("java/util/Locale").toString()), new ReportInfo("MDM_SETDEFAULTLOCALE", NORMAL_PRIORITY));
+        dumbMethods.put(new FQMethod("java/util/Locale", "setDefault", new SignatureBuilder().withParamTypes("java/util/Locale").toString()),
+                new ReportInfo("MDM_SETDEFAULTLOCALE", NORMAL_PRIORITY));
     }
+
+    private final BugReporter bugReporter;
+
+    private boolean sawAssertionDisabled;
+    private int assertionEnd;
 
     /**
      * constructs an MDM detector given the reporter to report bugs on
@@ -128,10 +157,9 @@ public class MoreDumbMethods extends BytecodeScanningDetector {
 
     @Override
     public void visitClassContext(ClassContext classContext) {
-        String byteArrayToVoid = new SignatureBuilder().withParamTypes(SignatureBuilder.SIG_BYTE_ARRAY).toString();
-        String intToByteArray = new SignatureBuilder().withParamTypes(Values.SIG_PRIMITIVE_INT).withReturnType(SignatureBuilder.SIG_BYTE_ARRAY).toString();
         if (classContext.getJavaClass().getMajor() <= MAJOR_1_5) {
-            dumbMethods.put(new FQMethod("java/security/SecureRandom", Values.CONSTRUCTOR, SignatureBuilder.SIG_VOID_TO_VOID), new ReportInfo("MDM_SECURERANDOM", LOW_PRIORITY));
+            dumbMethods.put(new FQMethod("java/security/SecureRandom", Values.CONSTRUCTOR, SignatureBuilder.SIG_VOID_TO_VOID),
+                    new ReportInfo("MDM_SECURERANDOM", LOW_PRIORITY));
             dumbMethods.put(new FQMethod("java/security/SecureRandom", Values.CONSTRUCTOR, byteArrayToVoid), new ReportInfo("MDM_SECURERANDOM", LOW_PRIORITY));
             dumbMethods.put(new FQMethod("java/security/SecureRandom", "getSeed", intToByteArray), new ReportInfo("MDM_SECURERANDOM", LOW_PRIORITY));
         } else {
