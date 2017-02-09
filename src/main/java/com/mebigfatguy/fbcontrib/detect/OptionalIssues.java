7ddf7e52e51293b94f998e5746f6a1a627444099
Dave Brosius <dbrosius@mebigfatguy.com>
2017-02-08 20:43:24 -0500

    stub in empty detector for Optional use as per GitHub issue #179

---
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/OptionalIssues.java
@@ -0,0 +1,66 @@
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2017 Dave Brosius
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.mebigfatguy.fbcontrib.detect;
+
+import org.apache.bcel.Constants;
+import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.JavaClass;
+
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.OpcodeStack;
+import edu.umd.cs.findbugs.ba.ClassContext;
+
+public class OptionalIssues extends BytecodeScanningDetector {
+
+    private BugReporter bugReporter;
+    private OpcodeStack stack;
+
+    public OptionalIssues(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
+
+    @Override
+    public void visitClassContext(ClassContext classContext) {
+        JavaClass cls = classContext.getJavaClass();
+
+        if (cls.getMajor() >= Constants.MAJOR_1_8) {
+            try {
+                stack = new OpcodeStack();
+                super.visitClassContext(classContext);
+            } finally {
+                stack = null;
+            }
+        }
+    }
+
+    @Override
+    public void visitCode(Code obj) {
+        stack.resetForMethodEntry(this);
+        super.visitCode(obj);
+    }
+
+    @Override
+    public void sawOpcode(int seen) {
+        try {
+        } finally {
+            stack.sawOpcode(this, seen);
+        }
+    }
+}
