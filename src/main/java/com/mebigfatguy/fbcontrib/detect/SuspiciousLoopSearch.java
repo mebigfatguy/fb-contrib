e5000d9aaf9fbf67e4f823ea194a4a76ad66b520
Dave Brosius <dbrosius@mebigfatguy.com>
2017-03-01 18:53:18 -0500

    handle breaks out of if blocks

--- src/main/java/com/mebigfatguy/fbcontrib/detect/SuspiciousLoopSearch.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/SuspiciousLoopSearch.java
@@ -45,235 +45,240 @@ import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for loops where an equality check is made and a variable is set because of it. It would seem once the item is found, the loop can be terminated,
- * however the code continues on, looking for more matches. It is possible the code is looking for the last match, but if this we case, a reverse iterator might
- * be more effective.
+ * looks for loops where an equality check is made and a variable is set because
+ * of it. It would seem once the item is found, the loop can be terminated,
+ * however the code continues on, looking for more matches. It is possible the
+ * code is looking for the last match, but if this we case, a reverse iterator
+ * might be more effective.
  */
 public class SuspiciousLoopSearch extends BytecodeScanningDetector {
 
-    enum State {
-        SAW_NOTHING, SAW_EQUALS, SAW_IFEQ
-    };
+	enum State {
+		SAW_NOTHING, SAW_EQUALS, SAW_IFEQ
+	};
 
-    private BugReporter bugReporter;
-    private OpcodeStack stack;
-    private State state;
-    private List<IfBlock> ifBlocks;
-    private int equalsPos;
-    private Map<Integer, Integer> loadedRegs;
+	private BugReporter bugReporter;
+	private OpcodeStack stack;
+	private State state;
+	private List<IfBlock> ifBlocks;
+	private int equalsPos;
+	private Map<Integer, Integer> loadedRegs;
 
-    /**
-     * constructs an SLS detector given the reporter to report bugs on
-     *
-     * @param bugReporter
-     *            the sync of bug reports
-     */
-    public SuspiciousLoopSearch(BugReporter bugReporter) {
-        this.bugReporter = bugReporter;
-    }
+	/**
+	 * constructs an SLS detector given the reporter to report bugs on
+	 *
+	 * @param bugReporter
+	 *            the sync of bug reports
+	 */
+	public SuspiciousLoopSearch(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
 
-    /**
-     * overrides the visitor to initialize and tear down the opcode stack
-     *
-     * @param classContext
-     *            the context object of the currently parsed class
-     */
-    @Override
-    public void visitClassContext(ClassContext classContext) {
-        try {
-            ifBlocks = new ArrayList<>();
-            loadedRegs = new HashMap<>();
-            stack = new OpcodeStack();
-            super.visitClassContext(classContext);
-        } finally {
-            ifBlocks = null;
-            loadedRegs = null;
-            stack = null;
-        }
-    }
+	/**
+	 * overrides the visitor to initialize and tear down the opcode stack
+	 *
+	 * @param classContext
+	 *            the context object of the currently parsed class
+	 */
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		try {
+			ifBlocks = new ArrayList<>();
+			loadedRegs = new HashMap<>();
+			stack = new OpcodeStack();
+			super.visitClassContext(classContext);
+		} finally {
+			ifBlocks = null;
+			loadedRegs = null;
+			stack = null;
+		}
+	}
 
-    /**
-     * overrides the visitor to reset the stack
-     *
-     * @param obj
-     *            the context object of the currently parsed code block
-     */
-    @Override
-    public void visitCode(Code obj) {
-        if (prescreen(getMethod())) {
-            ifBlocks.clear();
-            loadedRegs.clear();
-            stack.resetForMethodEntry(this);
-            state = State.SAW_NOTHING;
-            super.visitCode(obj);
-        }
-    }
+	/**
+	 * overrides the visitor to reset the stack
+	 *
+	 * @param obj
+	 *            the context object of the currently parsed code block
+	 */
+	@Override
+	public void visitCode(Code obj) {
+		if (prescreen(getMethod())) {
+			ifBlocks.clear();
+			loadedRegs.clear();
+			stack.resetForMethodEntry(this);
+			state = State.SAW_NOTHING;
+			super.visitCode(obj);
+		}
+	}
 
-    /**
-     * implements the visitor to find continuations after finding a search result in a loop.
-     *
-     * @param seen
-     *            the currently visitor opcode
-     */
-    @Override
-    public void sawOpcode(int seen) {
-        try {
-            switch (state) {
-                case SAW_NOTHING:
-                    sawOpcodeAfterNothing(seen);
-                break;
+	/**
+	 * implements the visitor to find continuations after finding a search
+	 * result in a loop.
+	 *
+	 * @param seen
+	 *            the currently visitor opcode
+	 */
+	@Override
+	public void sawOpcode(int seen) {
+		try {
+			switch (state) {
+			case SAW_NOTHING:
+				sawOpcodeAfterNothing(seen);
+				break;
 
-                case SAW_EQUALS:
-                    sawOpcodeAfterEquals(seen);
-                break;
+			case SAW_EQUALS:
+				sawOpcodeAfterEquals(seen);
+				break;
 
-                case SAW_IFEQ:
-                    sawOpcodeAfterBranch(seen);
-                break;
-            }
+			case SAW_IFEQ:
+				sawOpcodeAfterBranch(seen);
+				break;
+			}
 
-            processStore(seen);
-            processLoop(seen);
-        } finally {
-            stack.sawOpcode(this, seen);
-        }
+			processLoad(seen);
+			processLoop(seen);
+		} finally {
+			stack.sawOpcode(this, seen);
+		}
 
-    }
+	}
 
-    private void sawOpcodeAfterNothing(int seen) {
-        if ((seen == INVOKEVIRTUAL) && "equals".equals(getNameConstantOperand()) && SignatureBuilder.SIG_OBJECT_TO_BOOLEAN.equals(getSigConstantOperand())) {
-            state = State.SAW_EQUALS;
-            equalsPos = getPC();
-        } else if (seen == IF_ICMPNE) {
-            if (getBranchOffset() > 0) {
-                state = State.SAW_IFEQ;
-                int target = getBranchTarget();
-                ifBlocks.add(new IfBlock(equalsPos, target));
-            } else {
-                state = State.SAW_NOTHING;
-            }
-        }
-    }
+	private void sawOpcodeAfterNothing(int seen) {
+		if ((seen == INVOKEVIRTUAL) && "equals".equals(getNameConstantOperand())
+				&& SignatureBuilder.SIG_OBJECT_TO_BOOLEAN.equals(getSigConstantOperand())) {
+			state = State.SAW_EQUALS;
+			equalsPos = getPC();
+		} else if (seen == IF_ICMPNE) {
+			if (getBranchOffset() > 0) {
+				state = State.SAW_IFEQ;
+				int target = getBranchTarget();
+				ifBlocks.add(new IfBlock(equalsPos, target));
+			} else {
+				state = State.SAW_NOTHING;
+			}
+		}
+	}
 
-    private void sawOpcodeAfterEquals(int seen) {
-        if (seen == IFEQ) {
-            if (getBranchOffset() > 0) {
-                state = State.SAW_IFEQ;
-                int target = getBranchTarget();
-                ifBlocks.add(new IfBlock(equalsPos, target));
-            } else {
-                state = State.SAW_NOTHING;
-            }
-        } else {
-            state = State.SAW_NOTHING;
-        }
-    }
+	private void sawOpcodeAfterEquals(int seen) {
+		if (seen == IFEQ) {
+			if (getBranchOffset() > 0) {
+				state = State.SAW_IFEQ;
+				int target = getBranchTarget();
+				ifBlocks.add(new IfBlock(equalsPos, target));
+			} else {
+				state = State.SAW_NOTHING;
+			}
+		} else {
+			state = State.SAW_NOTHING;
+		}
+	}
 
-    private void sawOpcodeAfterBranch(int seen) {
-        if (OpcodeUtils.isBranch(seen) || OpcodeUtils.isReturn(seen)) {
-            state = State.SAW_NOTHING;
-        } else if (!ifBlocks.isEmpty()) {
-            IfBlock block = ifBlocks.get(ifBlocks.size() - 1);
-            if (OpcodeUtils.isStore(seen)) {
-                int reg = RegisterUtils.getStoreReg(this, seen);
-                if (!loadedRegs.containsKey(reg)) {
-                    LocalVariableTable lvt = getMethod().getLocalVariableTable();
-                    String sig = "";
-                    if (lvt != null) {
-                        LocalVariable lv = lvt.getLocalVariable(reg, getPC());
-                        if (lv != null) {
-                            sig = lv.getSignature();
-                        }
-                    }
-                    // ignore boolean flag stores, as this is a
-                    // relatively normal occurrence
-                    if (!Values.SIG_PRIMITIVE_BOOLEAN.equals(sig)) {
-                        block.storeRegs.put(Integer.valueOf(RegisterUtils.getStoreReg(this, seen)), Integer.valueOf(getPC()));
-                    }
-                }
-            }
-        }
-    }
+	private void sawOpcodeAfterBranch(int seen) {
+		if (!ifBlocks.isEmpty() && OpcodeUtils.isStore(seen)) {
+			IfBlock block = ifBlocks.get(ifBlocks.size() - 1);
+			int reg = RegisterUtils.getStoreReg(this, seen);
+			if (!loadedRegs.containsKey(reg)) {
+				LocalVariableTable lvt = getMethod().getLocalVariableTable();
+				String sig = "";
+				if (lvt != null) {
+					LocalVariable lv = lvt.getLocalVariable(reg, getPC());
+					if (lv != null) {
+						sig = lv.getSignature();
+					}
+				}
+				// ignore boolean flag stores, as this is a
+				// relatively normal occurrence
+				if (!Values.SIG_PRIMITIVE_BOOLEAN.equals(sig)) {
+					block.storeRegs.put(Integer.valueOf(RegisterUtils.getStoreReg(this, seen)),
+							Integer.valueOf(getPC()));
+				}
+			}
+		}
+	}
 
-    private void processStore(int seen) {
-        if (OpcodeUtils.isLoad(seen)) {
-            int reg = RegisterUtils.getLoadReg(this, seen);
-            for (IfBlock block : ifBlocks) {
-                block.storeRegs.remove(Integer.valueOf(reg));
-            }
-            loadedRegs.put(reg, getPC());
-        }
-    }
+	private void processLoad(int seen) {
+		if (OpcodeUtils.isLoad(seen)) {
+			int reg = RegisterUtils.getLoadReg(this, seen);
+			loadedRegs.put(reg, getPC());
+		}
+	}
 
-    private void processLoop(int seen) {
-        if (isBranch(seen) && (getBranchOffset() < 0)) {
-            List<IfBlock> blocksInLoop = new ArrayList<>(4);
+	private void processLoop(int seen) {
+		if (isBranch(seen) && (getBranchOffset() < 0)) {
+			List<IfBlock> blocksInLoop = new ArrayList<>(4);
 
-            Iterator<IfBlock> it = ifBlocks.iterator();
-            int target = getBranchTarget();
-            while (it.hasNext()) {
-                IfBlock block = it.next();
-                if (target <= block.start) {
-                    if (block.storeRegs.size() == 1) {
-                        blocksInLoop.add(block);
-                    }
-                    it.remove();
-                    state = State.SAW_NOTHING;
-                } else {
-                    break;
-                }
-            }
+			Iterator<IfBlock> it = ifBlocks.iterator();
+			int target = getBranchTarget();
+			while (it.hasNext()) {
+				IfBlock block = it.next();
+				if (target <= block.start) {
+					if (block.storeRegs.size() == 1) {
+						blocksInLoop.add(block);
+					}
+					it.remove();
+				} else {
+					break;
+				}
+			}
 
-            if (blocksInLoop.size() == 1) {
-                bugReporter.reportBug(new BugInstance(this, BugType.SLS_SUSPICIOUS_LOOP_SEARCH.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                        .addSourceLine(this, blocksInLoop.get(0).storeRegs.values().iterator().next().intValue()));
-            }
+			if (blocksInLoop.size() == 1) {
+				IfBlock block = blocksInLoop.get(0);
+				Integer pc = loadedRegs.get(block.storeRegs.entrySet().iterator().next().getKey());
+				if ((pc == null) || pc.intValue() < target) {
 
-            Iterator<Map.Entry<Integer, Integer>> loadedIt = loadedRegs.entrySet().iterator();
-            while (loadedIt.hasNext()) {
-                if (loadedIt.next().getValue() >= target) {
-                    loadedIt.remove();
-                }
-            }
-            loadedRegs.clear();
-        } else if ((seen == GOTO) || (seen == GOTO_W)) {
-            if (!ifBlocks.isEmpty()) {
-                IfBlock block = ifBlocks.get(ifBlocks.size() - 1);
-                if (block.end >= getPC()) {
-                    for (Map.Entry<Integer, Integer> storeEntry : block.storeRegs.entrySet()) {
-                        loadedRegs.put(storeEntry.getKey(), storeEntry.getValue());
-                    }
-                }
-            }
-        }
-    }
+					bugReporter
+							.reportBug(new BugInstance(this, BugType.SLS_SUSPICIOUS_LOOP_SEARCH.name(), NORMAL_PRIORITY)
+									.addClass(this).addMethod(this).addSourceLine(this,
+											blocksInLoop.get(0).storeRegs.values().iterator().next().intValue()));
+				}
+			}
 
-    /**
-     * looks for methods that contain a GOTO opcodes
-     *
-     * @param method
-     *            the context object of the current method
-     * @return if the class uses synchronization
-     */
-    private boolean prescreen(Method method) {
-        BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
-        return (bytecodeSet != null) && bytecodeSet.get(Constants.GOTO);
-    }
+			Iterator<Map.Entry<Integer, Integer>> loadedIt = loadedRegs.entrySet().iterator();
+			while (loadedIt.hasNext()) {
+				if (loadedIt.next().getValue() >= target) {
+					loadedIt.remove();
+				}
+			}
+			loadedRegs.clear();
+			state = State.SAW_NOTHING;
+		} else if ((seen == GOTO) || (seen == GOTO_W)) {
+			if (!ifBlocks.isEmpty()) {
+				IfBlock block = ifBlocks.get(ifBlocks.size() - 1);
+				if (block.end >= getPC()) {
+					for (Map.Entry<Integer, Integer> storeEntry : block.storeRegs.entrySet()) {
+						loadedRegs.put(storeEntry.getKey(), storeEntry.getValue());
+					}
+				}
+			}
+		}
+	}
 
-    static class IfBlock {
-        int start;
-        int end;
-        private Map<Integer, Integer> storeRegs;
+	/**
+	 * looks for methods that contain a GOTO opcodes
+	 *
+	 * @param method
+	 *            the context object of the current method
+	 * @return if the class uses synchronization
+	 */
+	private boolean prescreen(Method method) {
+		BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
+		return (bytecodeSet != null) && bytecodeSet.get(Constants.GOTO);
+	}
 
-        public IfBlock(int start, int end) {
-            this.start = start;
-            this.end = end;
-            storeRegs = new HashMap<>(4);
-        }
+	static class IfBlock {
+		int start;
+		int end;
+		private Map<Integer, Integer> storeRegs;
 
-        @Override
-        public String toString() {
-            return ToString.build(this);
-        }
-    }
+		public IfBlock(int start, int end) {
+			this.start = start;
+			this.end = end;
+			storeRegs = new HashMap<>(4);
+		}
+
+		@Override
+		public String toString() {
+			return ToString.build(this);
+		}
+	}
 }
