ae8816cb242c903aca048e7900d07df97e449f9a
Dave Brosius <dbrosius@mebigfatguy.com>
2017-03-11 18:02:46 -0500

    use BitSet rather than Set<Integer> for low valued ints

--- src/main/java/com/mebigfatguy/fbcontrib/detect/UnnecessaryNewNullCheck.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/UnnecessaryNewNullCheck.java
@@ -19,8 +19,6 @@
 package com.mebigfatguy.fbcontrib.detect;
 
 import java.util.BitSet;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.apache.bcel.Constants;
 import org.apache.bcel.classfile.Code;
@@ -40,17 +38,15 @@ import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for construction of new objects, and then the immediate testing whether
- * the object is null or not. As the new operator will always succeed, or
- * through an exception, this test is unnecessary and represents a
- * misunderstanding as to how the jvm works.
+ * looks for construction of new objects, and then the immediate testing whether the object is null or not. As the new operator will always succeed, or through
+ * an exception, this test is unnecessary and represents a misunderstanding as to how the jvm works.
  */
 @CustomUserValue
 public class UnnecessaryNewNullCheck extends BytecodeScanningDetector {
     private final BugReporter bugReporter;
     private OpcodeStack stack;
     private BitSet allocationRegs;
-    private Set<Integer> transitionPoints;
+    private BitSet transitionPoints;
 
     public UnnecessaryNewNullCheck(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
@@ -61,7 +57,7 @@ public class UnnecessaryNewNullCheck extends BytecodeScanningDetector {
         try {
             stack = new OpcodeStack();
             allocationRegs = new BitSet();
-            transitionPoints = new HashSet<Integer>();
+            transitionPoints = new BitSet();
             super.visitClassContext(classContext);
         } finally {
             stack = null;
@@ -80,7 +76,7 @@ public class UnnecessaryNewNullCheck extends BytecodeScanningDetector {
             CodeException[] ce = obj.getExceptionTable();
             if (ce != null) {
                 for (CodeException element : ce) {
-                    transitionPoints.add(Integer.valueOf(element.getEndPC()));
+                    transitionPoints.set(element.getEndPC());
                 }
             }
             super.visitCode(obj);
@@ -93,7 +89,7 @@ public class UnnecessaryNewNullCheck extends BytecodeScanningDetector {
         try {
             stack.precomputation(this);
 
-            if (transitionPoints.contains(Integer.valueOf(getPC()))) {
+            if (transitionPoints.get(getPC())) {
                 allocationRegs.clear();
                 int depth = stack.getStackDepth();
                 for (int i = 0; i < depth; i++) {
@@ -103,88 +99,88 @@ public class UnnecessaryNewNullCheck extends BytecodeScanningDetector {
             }
 
             switch (seen) {
-            case NEW:
-            case ANEWARRAY:
-            case MULTIANEWARRAY:
-                sawAlloc = true;
-                break;
-
-            case INVOKESPECIAL:
-                if (Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
+                case NEW:
+                case ANEWARRAY:
+                case MULTIANEWARRAY:
                     sawAlloc = true;
-                }
                 break;
 
-            case ASTORE:
-            case ASTORE_0:
-            case ASTORE_1:
-            case ASTORE_2:
-            case ASTORE_3:
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    int reg = RegisterUtils.getAStoreReg(this, seen);
-                    if (item.getUserValue() == null) {
-                        allocationRegs.clear(reg);
-                    } else {
-                        allocationRegs.set(reg);
+                case INVOKESPECIAL:
+                    if (Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
+                        sawAlloc = true;
                     }
-                }
                 break;
 
-            case ALOAD:
-            case ALOAD_0:
-            case ALOAD_1:
-            case ALOAD_2:
-            case ALOAD_3:
-                int reg = RegisterUtils.getALoadReg(this, seen);
-                sawAlloc = (allocationRegs.get(reg));
-                break;
-
-            case IFNONNULL:
-            case IFNULL:
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    if ((item.getUserValue() != null) && AttributesUtils.isValidLineNumber(getCode(), getPC())) {
-                        bugReporter.reportBug(new BugInstance(this, BugType.UNNC_UNNECESSARY_NEW_NULL_CHECK.name(), NORMAL_PRIORITY).addClass(this)
-                                .addMethod(this).addSourceLine(this));
+                case ASTORE:
+                case ASTORE_0:
+                case ASTORE_1:
+                case ASTORE_2:
+                case ASTORE_3:
+                    if (stack.getStackDepth() > 0) {
+                        OpcodeStack.Item item = stack.getStackItem(0);
+                        int reg = RegisterUtils.getAStoreReg(this, seen);
+                        if (item.getUserValue() == null) {
+                            allocationRegs.clear(reg);
+                        } else {
+                            allocationRegs.set(reg);
+                        }
                     }
-                }
-                transitionPoints.add(Integer.valueOf(getBranchTarget()));
-                allocationRegs.clear();
                 break;
 
-            case IFEQ:
-            case IFNE:
-            case IFLT:
-            case IFGE:
-            case IFGT:
-            case IFLE:
-            case IF_ICMPEQ:
-            case IF_ICMPNE:
-            case IF_ICMPLT:
-            case IF_ICMPGE:
-            case IF_ICMPGT:
-            case IF_ICMPLE:
-            case IF_ACMPEQ:
-            case IF_ACMPNE:
-            case GOTO:
-            case GOTO_W:
-                transitionPoints.add(Integer.valueOf(getBranchTarget()));
-                allocationRegs.clear();
+                case ALOAD:
+                case ALOAD_0:
+                case ALOAD_1:
+                case ALOAD_2:
+                case ALOAD_3:
+                    int reg = RegisterUtils.getALoadReg(this, seen);
+                    sawAlloc = (allocationRegs.get(reg));
                 break;
 
-            case TABLESWITCH:
-            case LOOKUPSWITCH:
-            case IRETURN:
-            case LRETURN:
-            case FRETURN:
-            case DRETURN:
-            case ARETURN:
-            case RETURN:
-            case ATHROW:
-                allocationRegs.clear();
+                case IFNONNULL:
+                case IFNULL:
+                    if (stack.getStackDepth() > 0) {
+                        OpcodeStack.Item item = stack.getStackItem(0);
+                        if ((item.getUserValue() != null) && AttributesUtils.isValidLineNumber(getCode(), getPC())) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.UNNC_UNNECESSARY_NEW_NULL_CHECK.name(), NORMAL_PRIORITY).addClass(this)
+                                    .addMethod(this).addSourceLine(this));
+                        }
+                    }
+                    transitionPoints.set(getBranchTarget());
+                    allocationRegs.clear();
                 break;
-            default:
+
+                case IFEQ:
+                case IFNE:
+                case IFLT:
+                case IFGE:
+                case IFGT:
+                case IFLE:
+                case IF_ICMPEQ:
+                case IF_ICMPNE:
+                case IF_ICMPLT:
+                case IF_ICMPGE:
+                case IF_ICMPGT:
+                case IF_ICMPLE:
+                case IF_ACMPEQ:
+                case IF_ACMPNE:
+                case GOTO:
+                case GOTO_W:
+                    transitionPoints.set(getBranchTarget());
+                    allocationRegs.clear();
+                break;
+
+                case TABLESWITCH:
+                case LOOKUPSWITCH:
+                case IRETURN:
+                case LRETURN:
+                case FRETURN:
+                case DRETURN:
+                case ARETURN:
+                case RETURN:
+                case ATHROW:
+                    allocationRegs.clear();
+                break;
+                default:
                 break;
             }
         } finally {
