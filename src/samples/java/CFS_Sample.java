b0f82620f210c084ad18aaf1d1e5195b01061a49
Dave Brosius <dbrosius@mebigfatguy.com>
2017-03-25 18:55:02 -0400

    add fp as shown by issue #184

--- src/samples/java/CFS_Sample.java
+++ src/samples/java/CFS_Sample.java
@@ -13,4 +13,33 @@ public class CFS_Sample {
         d.setHours(0);
         return d;
     }
+
+    public View fpCreateScrollView(View container, View content) {
+
+        if (!content.shouldAdd()) {
+            return container;
+        }
+
+        View scroller = View.createScroller(container.getChild(), content);
+        container.addView(scroller);
+        return scroller;
+    }
+
+    static class View {
+
+        public static View createScroller(Object child, View content) {
+            return null;
+        }
+
+        public void addView(View scroller) {
+        }
+
+        public boolean shouldAdd() {
+            return false;
+        }
+
+        public Object getChild() {
+            return null;
+        }
+    }
 }
