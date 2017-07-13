38057d9d3abb280c07ca7ff6841020bebe77de0e
Dave Brosius <dbrosius@mebigfatguy.com>
2017-07-13 19:31:32 -0400

    add test for array form of setAccessible

--- src/samples/java/ex/RFI_Sample.java
+++ src/samples/java/ex/RFI_Sample.java
@@ -1,5 +1,6 @@
 package ex;
 
+import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 
 public class RFI_Sample {
@@ -13,6 +14,21 @@ public class RFI_Sample {
 
         byte[] data = (byte[]) f.get(s);
         data[1] = 'a';
+
+        System.out.println(s);
+    }
+
+    public void accessMultipleFields() throws Exception {
+        String s = "Hello";
+
+        Field f = String.class.getDeclaredField("value");
+        Field g = String.class.getDeclaredField("hash");
+        AccessibleObject.setAccessible(new Field[] { f, g }, true);
+
+        byte[] data = (byte[]) f.get(s);
+        data[1] = 'a';
+
+        g.set(s, 0);
 
         System.out.println(s);
     }
