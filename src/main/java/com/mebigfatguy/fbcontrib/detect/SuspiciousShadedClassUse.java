9c6d3df65670d95c2f2c07fb638a90984be30e13
Dave Brosius <dbrosius@mebigfatguy.com>
2017-09-06 16:50:52 -0400

    add .net. as suspicious non-root

--- src/main/java/com/mebigfatguy/fbcontrib/detect/SuspiciousShadedClassUse.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/SuspiciousShadedClassUse.java
@@ -39,7 +39,8 @@ public class SuspiciousShadedClassUse extends BytecodeScanningDetector {
     // @formatter:off
         "/org/",
         "/com/",
-        "/edu/"
+        "/edu/",
+        "/net/"
     // @formatter:on
     );
 
