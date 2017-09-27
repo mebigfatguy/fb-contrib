8d9c0ca58313b30db3c25197784f5c507893b441
Dave Brosius <dbrosius@mebigfatguy.com>
2017-09-27 17:22:08 -0400

    remove warnings around use of the old Constants interface

--- src/main/java/com/mebigfatguy/fbcontrib/detect/UseSplit.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/UseSplit.java
@@ -21,6 +21,7 @@ package com.mebigfatguy.fbcontrib.detect;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.bcel.Const;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.JavaClass;
 
@@ -44,205 +45,206 @@ import edu.umd.cs.findbugs.ba.ClassContext;
  */
 @CustomUserValue
 public class UseSplit extends BytecodeScanningDetector {
-    enum State {
-        SEEN_NOTHING, SEEN_STRINGTOKENIZER, SEEN_COUNTTOKENS, SEEN_NEWARRAY, SEEN_HASMORE, SEEN_NEXT, SEEN_ARRAYSTORE
-    }
+	enum State {
+		SEEN_NOTHING, SEEN_STRINGTOKENIZER, SEEN_COUNTTOKENS, SEEN_NEWARRAY, SEEN_HASMORE, SEEN_NEXT, SEEN_ARRAYSTORE
+	}
 
-    private BugReporter bugReporter;
-    private OpcodeStack stack;
-    private Map<Integer, State> regValueType;
-    private State state;
-    private int loopStart, loopEnd;
+	private BugReporter bugReporter;
+	private OpcodeStack stack;
+	private Map<Integer, State> regValueType;
+	private State state;
+	private int loopStart, loopEnd;
 
-    /**
-     * constructs a USS detector given the reporter to report bugs on
-     *
-     * @param bugReporter
-     *            the sync of bug reports
-     */
-    public UseSplit(BugReporter bugReporter) {
-        this.bugReporter = bugReporter;
-    }
+	/**
+	 * constructs a USS detector given the reporter to report bugs on
+	 *
+	 * @param bugReporter
+	 *            the sync of bug reports
+	 */
+	public UseSplit(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
 
-    /**
-     * implements the visitor to make sure the class is at least java 1.4 and to
-     * reset the opcode stack
-     */
-    @Override
-    public void visitClassContext(ClassContext classContext) {
-        try {
-            JavaClass cls = classContext.getJavaClass();
-            if (cls.getMajor() >= MAJOR_1_4) {
-                stack = new OpcodeStack();
-                regValueType = new HashMap<Integer, State>();
-                super.visitClassContext(classContext);
-            }
-        } finally {
-            stack = null;
-            regValueType = null;
-        }
-    }
+	/**
+	 * implements the visitor to make sure the class is at least java 1.4 and to
+	 * reset the opcode stack
+	 */
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		try {
+			JavaClass cls = classContext.getJavaClass();
+			if (cls.getMajor() >= Const.MAJOR_1_4) {
+				stack = new OpcodeStack();
+				regValueType = new HashMap<Integer, State>();
+				super.visitClassContext(classContext);
+			}
+		} finally {
+			stack = null;
+			regValueType = null;
+		}
+	}
 
-    /**
-     * implements the visitor to reset the stack
-     *
-     * @param obj
-     *            the context object of the currently parsed code block
-     */
-    @Override
-    public void visitCode(Code obj) {
-        stack.resetForMethodEntry(this);
-        regValueType.clear();
-        state = State.SEEN_NOTHING;
-        loopStart = -1;
-        loopEnd = -1;
-        super.visitCode(obj);
-    }
+	/**
+	 * implements the visitor to reset the stack
+	 *
+	 * @param obj
+	 *            the context object of the currently parsed code block
+	 */
+	@Override
+	public void visitCode(Code obj) {
+		stack.resetForMethodEntry(this);
+		regValueType.clear();
+		state = State.SEEN_NOTHING;
+		loopStart = -1;
+		loopEnd = -1;
+		super.visitCode(obj);
+	}
 
-    /**
-     * implements the visitor to look for code that uses StringTokenizer when a
-     * simple String.split could be used.
-     *
-     * @param seen the currently parsed opcode
-     */
-    @Override
-    public void sawOpcode(int seen) {
-        try {
-            stack.precomputation(this);
+	/**
+	 * implements the visitor to look for code that uses StringTokenizer when a
+	 * simple String.split could be used.
+	 *
+	 * @param seen
+	 *            the currently parsed opcode
+	 */
+	@Override
+	public void sawOpcode(int seen) {
+		try {
+			stack.precomputation(this);
 
-            int pc = getPC();
-            if ((loopEnd != -1) && (pc > loopEnd)) {
-                loopStart = -1;
-                loopEnd = -1;
-                regValueType.clear();
-            }
+			int pc = getPC();
+			if ((loopEnd != -1) && (pc > loopEnd)) {
+				loopStart = -1;
+				loopEnd = -1;
+				regValueType.clear();
+			}
 
-            if ((seen == ALOAD) || ((seen >= ALOAD_0) && (seen <= ALOAD_3))) {
-                int reg = RegisterUtils.getALoadReg(this, seen);
-                State type = regValueType.get(Integer.valueOf(reg));
-                if (type == null) {
-                    state = State.SEEN_NOTHING;
-                } else {
-                    state = type;
-                }
-                return;
-            }
-            if ((seen == ASTORE) || ((seen >= ASTORE_0) && (seen <= ASTORE_3))) {
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    int reg = RegisterUtils.getAStoreReg(this, seen);
-                    regValueType.put(Integer.valueOf(reg), (State) item.getUserValue());
-                }
-                state = State.SEEN_NOTHING;
-                return;
-            }
-            if ((seen == ILOAD) || ((seen >= ILOAD_0) && (seen <= ILOAD_3))) {
-                int reg = RegisterUtils.getLoadReg(this, seen);
-                State type = regValueType.get(Integer.valueOf(reg));
-                if (type == null) {
-                    state = State.SEEN_NOTHING;
-                } else {
-                    state = type;
-                }
-                return;
-            }
-            if ((seen == ISTORE) || ((seen >= ISTORE_0) && (seen <= ISTORE_3))) {
-                if (stack.getStackDepth() > 0) {
-                    OpcodeStack.Item item = stack.getStackItem(0);
-                    int reg = RegisterUtils.getStoreReg(this, seen);
-                    regValueType.put(Integer.valueOf(reg), (State) item.getUserValue());
-                }
-                state = State.SEEN_NOTHING;
-                return;
-            }
+			if ((seen == Const.ALOAD) || ((seen >= Const.ALOAD_0) && (seen <= Const.ALOAD_3))) {
+				int reg = RegisterUtils.getALoadReg(this, seen);
+				State type = regValueType.get(Integer.valueOf(reg));
+				if (type == null) {
+					state = State.SEEN_NOTHING;
+				} else {
+					state = type;
+				}
+				return;
+			}
+			if ((seen == Const.ASTORE) || ((seen >= Const.ASTORE_0) && (seen <= Const.ASTORE_3))) {
+				if (stack.getStackDepth() > 0) {
+					OpcodeStack.Item item = stack.getStackItem(0);
+					int reg = RegisterUtils.getAStoreReg(this, seen);
+					regValueType.put(Integer.valueOf(reg), (State) item.getUserValue());
+				}
+				state = State.SEEN_NOTHING;
+				return;
+			}
+			if ((seen == Const.ILOAD) || ((seen >= Const.ILOAD_0) && (seen <= Const.ILOAD_3))) {
+				int reg = RegisterUtils.getLoadReg(this, seen);
+				State type = regValueType.get(Integer.valueOf(reg));
+				if (type == null) {
+					state = State.SEEN_NOTHING;
+				} else {
+					state = type;
+				}
+				return;
+			}
+			if ((seen == Const.ISTORE) || ((seen >= Const.ISTORE_0) && (seen <= Const.ISTORE_3))) {
+				if (stack.getStackDepth() > 0) {
+					OpcodeStack.Item item = stack.getStackItem(0);
+					int reg = RegisterUtils.getStoreReg(this, seen);
+					regValueType.put(Integer.valueOf(reg), (State) item.getUserValue());
+				}
+				state = State.SEEN_NOTHING;
+				return;
+			}
 
-            switch (state) {
-            case SEEN_NOTHING:
-                if ((seen == INVOKESPECIAL)
-                        && "java/util/StringTokenizer".equals(getClassConstantOperand())
-                        && Values.CONSTRUCTOR.equals(getNameConstantOperand())
-                        && SignatureBuilder.SIG_TWO_STRINGS_TO_VOID.equals(getSigConstantOperand())) {
-                    state = State.SEEN_STRINGTOKENIZER;
-                }
-                break;
+			switch (state) {
+			case SEEN_NOTHING:
+				if ((seen == Const.INVOKESPECIAL) && "java/util/StringTokenizer".equals(getClassConstantOperand())
+						&& Values.CONSTRUCTOR.equals(getNameConstantOperand())
+						&& SignatureBuilder.SIG_TWO_STRINGS_TO_VOID.equals(getSigConstantOperand())) {
+					state = State.SEEN_STRINGTOKENIZER;
+				}
+				break;
 
-            case SEEN_STRINGTOKENIZER:
-                if (seen == INVOKEVIRTUAL) {
-                    String methodName = getNameConstantOperand();
-                    String signature = getSigConstantOperand();
-                    if ("countTokens".equals(methodName) && SignatureBuilder.SIG_VOID_TO_INT.equals(signature))
-                        state = State.SEEN_COUNTTOKENS;
-                    else if ("hasMoreTokens".equals(methodName) || "hasMoreElements".equals(methodName))
-                        state = State.SEEN_HASMORE;
-                    else if ("nextToken".equals(methodName) || "nextElement".equals(methodName)) {
-                        if ((pc < loopStart) || (pc > loopEnd))
-                            regValueType.clear();
-                        else
-                            state = State.SEEN_NEXT;
-                    }
-                }
-                break;
+			case SEEN_STRINGTOKENIZER:
+				if (seen == Const.INVOKEVIRTUAL) {
+					String methodName = getNameConstantOperand();
+					String signature = getSigConstantOperand();
+					if ("countTokens".equals(methodName) && SignatureBuilder.SIG_VOID_TO_INT.equals(signature))
+						state = State.SEEN_COUNTTOKENS;
+					else if ("hasMoreTokens".equals(methodName) || "hasMoreElements".equals(methodName))
+						state = State.SEEN_HASMORE;
+					else if ("nextToken".equals(methodName) || "nextElement".equals(methodName)) {
+						if ((pc < loopStart) || (pc > loopEnd))
+							regValueType.clear();
+						else
+							state = State.SEEN_NEXT;
+					}
+				}
+				break;
 
-            case SEEN_COUNTTOKENS:
-                if (seen == ANEWARRAY)
-                    state = State.SEEN_NEWARRAY;
-                else if (seen == IF_ICMPGE) {
-                    int target = getBranchTarget() - 3;// sizeof goto
-                    byte[] code = getCode().getCode();
-                    if ((code[target] & 0x000000FF) == GOTO) {
-                        int offset = (code[target + 1] << 1) + code[target + 2];
-                        int gotoTarget = target + offset + 3;
-                        if (gotoTarget < getPC()) {
-                            loopStart = gotoTarget;
-                            loopEnd = target;
-                        }
-                    }
-                }
-                break;
+			case SEEN_COUNTTOKENS:
+				if (seen == Const.ANEWARRAY)
+					state = State.SEEN_NEWARRAY;
+				else if (seen == Const.IF_ICMPGE) {
+					int target = getBranchTarget() - 3;// sizeof goto
+					byte[] code = getCode().getCode();
+					if ((code[target] & 0x000000FF) == Const.GOTO) {
+						int offset = (code[target + 1] << 1) + code[target + 2];
+						int gotoTarget = target + offset + 3;
+						if (gotoTarget < getPC()) {
+							loopStart = gotoTarget;
+							loopEnd = target;
+						}
+					}
+				}
+				break;
 
-            case SEEN_HASMORE:
-                if (seen == IFEQ) {
-                    int target = getBranchTarget() - 3;// sizeof goto
-                    byte[] code = getCode().getCode();
-                    if ((code[target] & 0x000000FF) == GOTO) {
-                        int offset = (code[target + 1] << 1) + code[target + 2];
-                        int gotoTarget = target + offset + 3;
-                        if (gotoTarget < getPC()) {
-                            loopStart = gotoTarget;
-                            loopEnd = target;
-                        }
-                    }
-                }
-                state = State.SEEN_NOTHING;
-                break;
+			case SEEN_HASMORE:
+				if (seen == Const.IFEQ) {
+					int target = getBranchTarget() - 3;// sizeof goto
+					byte[] code = getCode().getCode();
+					if ((code[target] & 0x000000FF) == Const.GOTO) {
+						int offset = (code[target + 1] << 1) + code[target + 2];
+						int gotoTarget = target + offset + 3;
+						if (gotoTarget < getPC()) {
+							loopStart = gotoTarget;
+							loopEnd = target;
+						}
+					}
+				}
+				state = State.SEEN_NOTHING;
+				break;
 
-            case SEEN_NEXT:
-                if ((seen == AASTORE) && (pc > loopStart) && (pc < loopEnd) && (stack.getStackDepth() > 2)) {
-                    OpcodeStack.Item arrayItem = stack.getStackItem(2);
-                    State arrayType = (State) arrayItem.getUserValue();
-                    OpcodeStack.Item elemItem = stack.getStackItem(0);
-                    State elemType = (State) elemItem.getUserValue();
-                    if ((arrayType == State.SEEN_NEWARRAY) && (elemType == State.SEEN_NEXT)) {
-                        bugReporter.reportBug(new BugInstance(this, BugType.USS_USE_STRING_SPLIT.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                                .addSourceLine(this));
-                    }
-                }
-                state = State.SEEN_NOTHING;
-                break;
+			case SEEN_NEXT:
+				if ((seen == Const.AASTORE) && (pc > loopStart) && (pc < loopEnd) && (stack.getStackDepth() > 2)) {
+					OpcodeStack.Item arrayItem = stack.getStackItem(2);
+					State arrayType = (State) arrayItem.getUserValue();
+					OpcodeStack.Item elemItem = stack.getStackItem(0);
+					State elemType = (State) elemItem.getUserValue();
+					if ((arrayType == State.SEEN_NEWARRAY) && (elemType == State.SEEN_NEXT)) {
+						bugReporter
+								.reportBug(new BugInstance(this, BugType.USS_USE_STRING_SPLIT.name(), NORMAL_PRIORITY)
+										.addClass(this).addMethod(this).addSourceLine(this));
+					}
+				}
+				state = State.SEEN_NOTHING;
+				break;
 
-            case SEEN_ARRAYSTORE:
-            case SEEN_NEWARRAY:
-                break;
-            }
-        } finally {
-            TernaryPatcher.pre(stack, seen);
-            stack.sawOpcode(this, seen);
-            TernaryPatcher.post(stack, seen);
-            if ((state != State.SEEN_NOTHING) && (stack.getStackDepth() > 0)) {
-                OpcodeStack.Item item = stack.getStackItem(0);
-                item.setUserValue(state);
-            }
-        }
-    }
+			case SEEN_ARRAYSTORE:
+			case SEEN_NEWARRAY:
+				break;
+			}
+		} finally {
+			TernaryPatcher.pre(stack, seen);
+			stack.sawOpcode(this, seen);
+			TernaryPatcher.post(stack, seen);
+			if ((state != State.SEEN_NOTHING) && (stack.getStackDepth() > 0)) {
+				OpcodeStack.Item item = stack.getStackItem(0);
+				item.setUserValue(state);
+			}
+		}
+	}
 
 }
