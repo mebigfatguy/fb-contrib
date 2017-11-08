855042bff7154a30b57f4eefbfe23f19683af76b
Dave Brosius <dbrosius@mebigfatguy.com>
2017-11-08 18:35:04 -0500

    add example FP for COM as reported by #241

--- src/samples/java/ex/COM_Sample.java
+++ src/samples/java/ex/COM_Sample.java
@@ -5,118 +5,135 @@ import java.util.Set;
 
 public class COM_Sample {
 
-	public void test1() {
-	}
+    public void test1() {
+    }
 
-	public String test2(int i) {
-		return String.valueOf(i);
-	}
+    public String test2(int i) {
+        return String.valueOf(i);
+    }
 
-	public Set<String> test3(String a, String b, String c) {
-		Set<String> ss = new HashSet<>();
-		ss.add(a);
-		ss.add(b);
-		ss.add(c);
-		return ss;
-	}
+    public Set<String> test3(String a, String b, String c) {
+        Set<String> ss = new HashSet<>();
+        ss.add(a);
+        ss.add(b);
+        ss.add(c);
+        return ss;
+    }
 
-	public boolean test4(int w, double x, String y, boolean z) {
-		return z;
-	}
+    public boolean test4(int w, double x, String y, boolean z) {
+        return z;
+    }
 
-	protected void fpEnhanceAccess() {
-	}
+    protected void fpEnhanceAccess() {
+    }
 
-	public static class Derived extends COM_Sample {
-		@Override
-		public void test1() {
-		}
+    public static class Derived extends COM_Sample {
+        @Override
+        public void test1() {
+        }
 
-		@Override
-		public Set<String> test3(String a, String b, String c) {
-			Set<String> ss = new HashSet<>();
-			ss.add(a);
-			ss.add(b);
-			ss.add(c);
-			return ss;
-		}
+        @Override
+        public Set<String> test3(String a, String b, String c) {
+            Set<String> ss = new HashSet<>();
+            ss.add(a);
+            ss.add(b);
+            ss.add(c);
+            return ss;
+        }
 
-		@Override
-		public String test2(int i) {
-			return String.valueOf(i);
-		}
+        @Override
+        public String test2(int i) {
+            return String.valueOf(i);
+        }
 
-		@Override
-		public boolean test4(int w, double x, String y, boolean z) {
-			return super.test4(w, x, y, z);
-		}
+        @Override
+        public boolean test4(int w, double x, String y, boolean z) {
+            return super.test4(w, x, y, z);
+        }
 
-		@Override
-		public void fpEnhanceAccess() {
-		}
-	}
+        @Override
+        public void fpEnhanceAccess() {
+        }
+    }
 }
 
 interface Inf {
-	void m1();
+    void m1();
 
-	void m2();
+    void m2();
 }
 
 abstract class c1 implements Inf {
-	@Override
-	public void m1() {
-	}
+    @Override
+    public void m1() {
+    }
 }
 
 abstract class c2 extends c1 {
-	@Override
-	public void m2() {
-	}
+    @Override
+    public void m2() {
+    }
 }
 
 abstract class s1 {
-	public static final String FOO = "foo";
+    public static final String FOO = "foo";
 
-	public String getFoo() {
-		return FOO;
-	}
+    public String getFoo() {
+        return FOO;
+    }
 }
 
 abstract class s2 extends s1 {
-	public static final String FOO = "foo2";
+    public static final String FOO = "foo2";
 
-	@Override
-	public String getFoo() {
-		return FOO;
-	}
+    @Override
+    public String getFoo() {
+        return FOO;
+    }
 }
 
 class NonSync {
-	public int comExtendedSync() {
-		return 42;
-	}
+    public int comExtendedSync() {
+        return 42;
+    }
 }
 
 class Sync extends NonSync {
-	@Override
-	public synchronized int comExtendedSync() {
-		return super.comExtendedSync();
-	}
+    @Override
+    public synchronized int comExtendedSync() {
+        return super.comExtendedSync();
+    }
 }
 
 class ParentGH232 {
 
-	protected long getN() {
-		return 2;
-	}
+    protected long getN() {
+        return 2;
+    }
 
 }
 
 class ChildGH232 extends ParentGH232 {
 
-	@Override
-	protected long getN() {
-		return 4;
-	}
+    @Override
+    protected long getN() {
+        return 4;
+    }
+}
+
+class GH241 {
+    private static final String[] STRINGS = new String[] { "" };
+
+    protected String[] getStrings() {
+        return STRINGS;
+    }
+}
+
+class Child241 extends GH241 {
+    private static final String[] STRINGS = new String[] { "Bar's string" };
+
+    @Override
+    protected String[] getStrings() {
+        return STRINGS;
+    }
 }
