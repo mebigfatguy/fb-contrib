2dc8fce68ddf1726b7a4b047fdc0c7a6da281f78
Dave Brosius <dbrosius@mebigfatguy.com>
2017-11-10 17:12:35 -0500

    cleanup warnings around old use of Constants interface

--- src/main/java/com/mebigfatguy/fbcontrib/detect/TristateBooleanPattern.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/TristateBooleanPattern.java
@@ -18,6 +18,7 @@
  */
 package com.mebigfatguy.fbcontrib.detect;
 
+import org.apache.bcel.Const;
 import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.Method;
 import org.apache.bcel.generic.Type;
@@ -32,80 +33,83 @@ import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for methods that are defined to return Boolean, but return null. This thus allows three return values, Boolean.FALSE, Boolean.TRUE and null. If three
- * values intended, it would be more clear to just create an enumeration with three values and return that type.
+ * looks for methods that are defined to return Boolean, but return null. This
+ * thus allows three return values, Boolean.FALSE, Boolean.TRUE and null. If
+ * three values intended, it would be more clear to just create an enumeration
+ * with three values and return that type.
  */
 public class TristateBooleanPattern extends BytecodeScanningDetector {
-    private BugReporter bugReporter;
-    private OpcodeStack stack;
+	private BugReporter bugReporter;
+	private OpcodeStack stack;
 
-    /**
-     * constructs a TBP detector given the reporter to report bugs on
-     *
-     * @param bugReporter
-     *            the sync of bug reports
-     */
-    public TristateBooleanPattern(BugReporter bugReporter) {
-        this.bugReporter = bugReporter;
-    }
+	/**
+	 * constructs a TBP detector given the reporter to report bugs on
+	 *
+	 * @param bugReporter
+	 *            the sync of bug reports
+	 */
+	public TristateBooleanPattern(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
 
-    /**
-     * implements the visitor to allocate the opcode stack
-     *
-     * @param classContext
-     *            the context object of the currently parsed class
-     */
-    @Override
-    public void visitClassContext(ClassContext classContext) {
-        try {
-            stack = new OpcodeStack();
-            super.visitClassContext(classContext);
-        } finally {
-            stack = null;
-        }
-    }
+	/**
+	 * implements the visitor to allocate the opcode stack
+	 *
+	 * @param classContext
+	 *            the context object of the currently parsed class
+	 */
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		try {
+			stack = new OpcodeStack();
+			super.visitClassContext(classContext);
+		} finally {
+			stack = null;
+		}
+	}
 
-    /**
-     * implements the visitor to filter out methods that don't return Boolean,
-     *
-     *
-     * @param obj
-     *            the context object for the currently parsed code block
-     */
-    @Override
-    public void visitCode(Code obj) {
-        try {
-            Method m = getMethod();
-            Type retType = m.getReturnType();
-            if ("Ljava/lang/Boolean;".equals(retType.getSignature())) {
-                stack.resetForMethodEntry(this);
-                super.visitCode(obj);
-            }
-        } catch (StopOpcodeParsingException e) {
-            // if the method was already reported
-        }
-    }
+	/**
+	 * implements the visitor to filter out methods that don't return Boolean,
+	 *
+	 *
+	 * @param obj
+	 *            the context object for the currently parsed code block
+	 */
+	@Override
+	public void visitCode(Code obj) {
+		try {
+			Method m = getMethod();
+			Type retType = m.getReturnType();
+			if ("Ljava/lang/Boolean;".equals(retType.getSignature())) {
+				stack.resetForMethodEntry(this);
+				super.visitCode(obj);
+			}
+		} catch (StopOpcodeParsingException e) {
+			// if the method was already reported
+		}
+	}
 
-    /**
-     * implements the visitor to look for null returns
-     */
-    @Override
-    public void sawOpcode(int seen) {
-        try {
+	/**
+	 * implements the visitor to look for null returns
+	 */
+	@Override
+	public void sawOpcode(int seen) {
+		try {
 
-            stack.precomputation(this);
+			stack.precomputation(this);
 
-            if ((seen == ARETURN) && (stack.getStackDepth() > 0)) {
-                OpcodeStack.Item item = stack.getStackItem(0);
-                if (item.isNull()) {
-                    bugReporter.reportBug(new BugInstance(this, BugType.TBP_TRISTATE_BOOLEAN_PATTERN.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                            .addSourceLine(this));
-                    throw new StopOpcodeParsingException();
-                }
-            }
-        } finally {
-            stack.sawOpcode(this, seen);
-        }
-    }
+			if ((seen == Const.ARETURN) && (stack.getStackDepth() > 0)) {
+				OpcodeStack.Item item = stack.getStackItem(0);
+				if (item.isNull()) {
+					bugReporter.reportBug(
+							new BugInstance(this, BugType.TBP_TRISTATE_BOOLEAN_PATTERN.name(), NORMAL_PRIORITY)
+									.addClass(this).addMethod(this).addSourceLine(this));
+					throw new StopOpcodeParsingException();
+				}
+			}
+		} finally {
+			stack.sawOpcode(this, seen);
+		}
+	}
 
 }
