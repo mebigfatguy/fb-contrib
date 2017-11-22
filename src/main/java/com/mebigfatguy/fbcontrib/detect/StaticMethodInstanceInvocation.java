82096cb4a612c3400acf5b313d3a750450d2272b
Dave Brosius <dbrosius@mebigfatguy.com>
2017-11-22 18:17:38 -0500

    cleanup warnings around old use of Constants interface

--- src/main/java/com/mebigfatguy/fbcontrib/detect/StaticMethodInstanceInvocation.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/StaticMethodInstanceInvocation.java
@@ -42,167 +42,174 @@ import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
 /**
- * looks for methods that make static method calls using an instance reference. For documentation purposes, it is better to call the method using the class
+ * looks for methods that make static method calls using an instance reference.
+ * For documentation purposes, it is better to call the method using the class
  * name. This may represent a change in definition that should be noticed.
  */
 public class StaticMethodInstanceInvocation extends BytecodeScanningDetector {
-    private final BugReporter bugReporter;
-    private OpcodeStack stack;
-    private List<PopInfo> popStack;
+	private final BugReporter bugReporter;
+	private OpcodeStack stack;
+	private List<PopInfo> popStack;
 
-    /**
-     * constructs a SMII detector given the reporter to report bugs on
-     *
-     * @param bugReporter
-     *            the sync of bug reports
-     */
-    public StaticMethodInstanceInvocation(BugReporter bugReporter) {
-        this.bugReporter = bugReporter;
-    }
+	/**
+	 * constructs a SMII detector given the reporter to report bugs on
+	 *
+	 * @param bugReporter
+	 *            the sync of bug reports
+	 */
+	public StaticMethodInstanceInvocation(BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
 
-    @Override
-    public void visitClassContext(ClassContext classContext) {
-        try {
-            stack = new OpcodeStack();
-            popStack = new ArrayList<>();
-            super.visitClassContext(classContext);
-        } finally {
-            stack = null;
-            popStack = null;
-        }
-    }
+	@Override
+	public void visitClassContext(ClassContext classContext) {
+		try {
+			stack = new OpcodeStack();
+			popStack = new ArrayList<>();
+			super.visitClassContext(classContext);
+		} finally {
+			stack = null;
+			popStack = null;
+		}
+	}
 
-    /**
-     * looks for methods that contain a INVOKESTATIC opcodes
-     *
-     * @param method
-     *            the context object of the current method
-     * @return if the class uses synchronization
-     */
-    private boolean prescreen(Method method) {
-        BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
-        return (bytecodeSet != null) && (bytecodeSet.get(Const.INVOKESTATIC));
-    }
+	/**
+	 * looks for methods that contain a INVOKESTATIC opcodes
+	 *
+	 * @param method
+	 *            the context object of the current method
+	 * @return if the class uses synchronization
+	 */
+	private boolean prescreen(Method method) {
+		BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
+		return (bytecodeSet != null) && (bytecodeSet.get(Const.INVOKESTATIC));
+	}
 
-    /**
-     * implement the visitor to reset the stack
-     *
-     * @param obj
-     *            the context object of the currently parsed method
-     */
-    @Override
-    public void visitCode(Code obj) {
-        Method m = getMethod();
-        if (prescreen(m)) {
-            stack.resetForMethodEntry(this);
-            popStack.clear();
-            super.visitCode(obj);
-        }
-    }
+	/**
+	 * implement the visitor to reset the stack
+	 *
+	 * @param obj
+	 *            the context object of the currently parsed method
+	 */
+	@Override
+	public void visitCode(Code obj) {
+		Method m = getMethod();
+		if (prescreen(m)) {
+			stack.resetForMethodEntry(this);
+			popStack.clear();
+			super.visitCode(obj);
+		}
+	}
 
-    /**
-     * implements the visitor to look for static method calls from instance variables
-     *
-     * @param seen
-     *            the opcode of the currently visited instruction
-     */
-    @Override
-    public void sawOpcode(int seen) {
-        try {
-            stack.precomputation(this);
+	/**
+	 * implements the visitor to look for static method calls from instance
+	 * variables
+	 *
+	 * @param seen
+	 *            the opcode of the currently visited instruction
+	 */
+	@Override
+	public void sawOpcode(int seen) {
+		try {
+			stack.precomputation(this);
 
-            int sDepth = stack.getStackDepth();
-            Iterator<PopInfo> it = popStack.iterator();
+			int sDepth = stack.getStackDepth();
+			Iterator<PopInfo> it = popStack.iterator();
 
-            while (it.hasNext()) {
-                if (sDepth < it.next().popDepth) {
-                    it.remove();
-                }
-            }
+			while (it.hasNext()) {
+				if (sDepth < it.next().popDepth) {
+					it.remove();
+				}
+			}
 
-            if ((seen == INVOKESTATIC) && !popStack.isEmpty()) {
-                String method = getNameConstantOperand();
-                if (method.indexOf('$') < 0) {
-                    PopInfo pInfo = popStack.get(0);
-                    int numArguments = SignatureUtils.getNumParameters(getSigConstantOperand());
-                    if (((numArguments > 0) || (pInfo.popPC == (getPC() - 1))) && (numArguments == (stack.getStackDepth() - pInfo.popDepth))
-                            && classDefinesStaticMethod(SignatureUtils.stripSignature(pInfo.popSignature))) {
-                        int lineNumber = -1;
-                        if (lineNumberTable != null) {
-                            lineNumber = lineNumberTable.getSourceLine(getPC());
-                        }
-                        if (pInfo.popLineNum == lineNumber) {
-                            bugReporter.reportBug(new BugInstance(this, BugType.SMII_STATIC_METHOD_INSTANCE_INVOCATION.name(), NORMAL_PRIORITY).addClass(this)
-                                    .addMethod(this).addSourceLine(this));
-                        }
-                        popStack.clear();
-                    }
-                }
-            }
+			if ((seen == Const.INVOKESTATIC) && !popStack.isEmpty()) {
+				String method = getNameConstantOperand();
+				if (method.indexOf('$') < 0) {
+					PopInfo pInfo = popStack.get(0);
+					int numArguments = SignatureUtils.getNumParameters(getSigConstantOperand());
+					if (((numArguments > 0) || (pInfo.popPC == (getPC() - 1)))
+							&& (numArguments == (stack.getStackDepth() - pInfo.popDepth))
+							&& classDefinesStaticMethod(SignatureUtils.stripSignature(pInfo.popSignature))) {
+						int lineNumber = -1;
+						if (lineNumberTable != null) {
+							lineNumber = lineNumberTable.getSourceLine(getPC());
+						}
+						if (pInfo.popLineNum == lineNumber) {
+							bugReporter.reportBug(
+									new BugInstance(this, BugType.SMII_STATIC_METHOD_INSTANCE_INVOCATION.name(),
+											NORMAL_PRIORITY).addClass(this).addMethod(this).addSourceLine(this));
+						}
+						popStack.clear();
+					}
+				}
+			}
 
-            if (OpcodeUtils.isAStore(seen) || (seen == PUTFIELD) || (seen == ATHROW) || (seen == GOTO) || (seen == GOTO_W)
-                    || ((seen >= IFEQ) && (seen <= IF_ACMPNE))) {
-                popStack.clear();
-            } else if ((seen == INVOKESPECIAL) || (seen == INVOKEINTERFACE) || (seen == INVOKEVIRTUAL) || (seen == INVOKESTATIC)) {
-                if (Values.SIG_VOID.equals(SignatureUtils.getReturnSignature(getSigConstantOperand()))) {
-                    popStack.clear();
-                }
-            }
+			if (OpcodeUtils.isAStore(seen) || (seen == Const.PUTFIELD) || (seen == Const.ATHROW) || (seen == Const.GOTO)
+					|| (seen == Const.GOTO_W) || ((seen >= Const.IFEQ) && (seen <= Const.IF_ACMPNE))) {
+				popStack.clear();
+			} else if ((seen == Const.INVOKESPECIAL) || (seen == Const.INVOKEINTERFACE) || (seen == Const.INVOKEVIRTUAL)
+					|| (seen == Const.INVOKESTATIC)) {
+				if (Values.SIG_VOID.equals(SignatureUtils.getReturnSignature(getSigConstantOperand()))) {
+					popStack.clear();
+				}
+			}
 
-            if ((seen == POP) && (stack.getStackDepth() > 0)) {
-                OpcodeStack.Item itm = stack.getStackItem(0);
-                String popSig = itm.getSignature();
-                if (popSig.startsWith(Values.SIG_QUALIFIED_CLASS_PREFIX)) {
-                    int lineNumber = -1;
-                    if (lineNumberTable != null) {
-                        lineNumber = lineNumberTable.getSourceLine(getPC());
-                    }
-                    popStack.add(new PopInfo(getPC(), lineNumber, popSig, sDepth - 1));
-                }
-            }
-        } catch (ClassNotFoundException cnfe) {
-            bugReporter.reportMissingClass(cnfe);
-            popStack.clear();
-        } finally {
-            stack.sawOpcode(this, seen);
-        }
-    }
+			if ((seen == Const.POP) && (stack.getStackDepth() > 0)) {
+				OpcodeStack.Item itm = stack.getStackItem(0);
+				String popSig = itm.getSignature();
+				if (popSig.startsWith(Values.SIG_QUALIFIED_CLASS_PREFIX)) {
+					int lineNumber = -1;
+					if (lineNumberTable != null) {
+						lineNumber = lineNumberTable.getSourceLine(getPC());
+					}
+					popStack.add(new PopInfo(getPC(), lineNumber, popSig, sDepth - 1));
+				}
+			}
+		} catch (ClassNotFoundException cnfe) {
+			bugReporter.reportMissingClass(cnfe);
+			popStack.clear();
+		} finally {
+			stack.sawOpcode(this, seen);
+		}
+	}
 
-    boolean classDefinesStaticMethod(String popSignature) throws ClassNotFoundException {
-        if (Values.DOTTED_JAVA_LANG_OBJECT.equals(popSignature) || Values.DOTTED_JAVA_LANG_CLASS.equals(popSignature)) {
-            return false;
-        }
+	boolean classDefinesStaticMethod(String popSignature) throws ClassNotFoundException {
+		if (Values.DOTTED_JAVA_LANG_OBJECT.equals(popSignature) || Values.DOTTED_JAVA_LANG_CLASS.equals(popSignature)) {
+			return false;
+		}
 
-        JavaClass cls = Repository.lookupClass(popSignature);
-        Method[] methods = cls.getMethods();
-        for (Method m : methods) {
-            if (m.isStatic() && m.getName().equals(getNameConstantOperand()) && m.getSignature().equals(getSigConstantOperand())) {
-                return true;
-            }
-        }
+		JavaClass cls = Repository.lookupClass(popSignature);
+		Method[] methods = cls.getMethods();
+		for (Method m : methods) {
+			if (m.isStatic() && m.getName().equals(getNameConstantOperand())
+					&& m.getSignature().equals(getSigConstantOperand())) {
+				return true;
+			}
+		}
 
-        return classDefinesStaticMethod(cls.getSuperclassName());
-    }
+		return classDefinesStaticMethod(cls.getSuperclassName());
+	}
 
-    /**
-     * holds information about a POP instruction, what was popped, where it occurred, etc.
-     */
-    static class PopInfo {
-        int popPC;
-        int popLineNum;
-        String popSignature;
-        int popDepth;
+	/**
+	 * holds information about a POP instruction, what was popped, where it
+	 * occurred, etc.
+	 */
+	static class PopInfo {
+		int popPC;
+		int popLineNum;
+		String popSignature;
+		int popDepth;
 
-        PopInfo(int pc, int lineNum, String signature, int depth) {
-            popPC = pc;
-            popLineNum = lineNum;
-            popSignature = signature;
-            popDepth = depth;
-        }
+		PopInfo(int pc, int lineNum, String signature, int depth) {
+			popPC = pc;
+			popLineNum = lineNum;
+			popSignature = signature;
+			popDepth = depth;
+		}
 
-        @Override
-        public String toString() {
-            return ToString.build(this);
-        }
-    }
+		@Override
+		public String toString() {
+			return ToString.build(this);
+		}
+	}
 }
