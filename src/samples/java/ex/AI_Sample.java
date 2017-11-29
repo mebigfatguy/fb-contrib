30ca3331bbf1a4a0f68e56580c92d3c89c974f09
Dave Brosius <dbrosius@mebigfatguy.com>
2017-11-28 23:02:33 -0500

    add FP for null checked return - #242

--- src/samples/java/ex/AI_Sample.java
+++ src/samples/java/ex/AI_Sample.java
@@ -1,5 +1,8 @@
 package ex;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import javax.annotation.Nullable;
 
 public class AI_Sample {
@@ -11,5 +14,14 @@ public class AI_Sample {
 
     public String getFoo2() {
         return getFoo();
+    }
+
+    public InputStream fpNullChecked() throws IOException {
+        InputStream is = AI_Sample.class.getResourceAsStream("/foo");
+        if (is == null) {
+            throw new IOException();
+        }
+
+        return is;
     }
 }
