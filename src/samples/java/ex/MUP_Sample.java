5fb743efb9fde606fc3ccb70938b3bac63aa1745
Dave Brosius <dbrosius@mebigfatguy.com>
2017-12-14 21:07:32 -0500

    add more MUP tests

--- src/samples/java/ex/MUP_Sample.java
+++ src/samples/java/ex/MUP_Sample.java
@@ -1,15 +1,40 @@
 package ex;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MUP_Sample {
 
-    public String testGetAfterContainsKey() {
-        Map<String, String> m = new HashMap<>();
+    Map<String, String> fieldMap = new HashMap<>();
 
-        if (m.containsKey("Foo")) {
-            String v = m.get("Foo");
+    public String testGetAfterContainsKeyLocal() {
+        Map<String, String> localMap = new HashMap<>();
+
+        if (localMap.containsKey("Foo")) {
+            String v = localMap.get("Foo");
+            return v + "Bar";
+        } else {
+            return "Bar";
+        }
+    }
+
+    public String testGetAfterContainsKeyWithReg() {
+        Map<Date, String> localMap = new HashMap<>();
+        Date d = new Date();
+
+        if (localMap.containsKey(d)) {
+            String v = localMap.get(d);
+            return v + "Bar";
+        } else {
+            return "Bar";
+        }
+    }
+
+    public String testGetAfterContainsKeyField() {
+
+        if (fieldMap.containsKey("Foo")) {
+            String v = fieldMap.get("Foo");
             return v + "Bar";
         } else {
             return "Bar";
