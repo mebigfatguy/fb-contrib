9aa1820dc8f50f79554b08cadf399e2c990c5943
Dave Brosius <dbrosius@mebigfatguy.com>
2017-12-21 19:09:49 -0500

    remove redundant interface

--- src/main/java/com/mebigfatguy/fbcontrib/detect/WiringIssues.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/WiringIssues.java
@@ -37,7 +37,6 @@ import com.mebigfatguy.fbcontrib.utils.Values;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
-import edu.umd.cs.findbugs.Detector;
 import edu.umd.cs.findbugs.FieldAnnotation;
 import edu.umd.cs.findbugs.OpcodeStack;
 import edu.umd.cs.findbugs.ba.ClassContext;
@@ -45,206 +44,197 @@ import edu.umd.cs.findbugs.ba.ClassContext;
 /**
  * looks for various issues around @Autowired/@Inject fields in DI classes
  * <ul>
- * <li>Injecting the same bean twice into the same class hierarchy, even with
- * different field names</li>
+ * <li>Injecting the same bean twice into the same class hierarchy, even with different field names</li>
  * </ul>
  */
-public class WiringIssues extends BytecodeScanningDetector implements Detector {
+public class WiringIssues extends BytecodeScanningDetector {
 
-	private static final String SPRING_AUTOWIRED = "Lorg/springframework/beans/factory/annotation/Autowired;";
-	private static final String SPRING_QUALIFIER = "Lorg/springframework/beans/factory/annotation/Qualifier;";
+    private static final String SPRING_AUTOWIRED = "Lorg/springframework/beans/factory/annotation/Autowired;";
+    private static final String SPRING_QUALIFIER = "Lorg/springframework/beans/factory/annotation/Qualifier;";
 
-	private BugReporter bugReporter;
-	private OpcodeStack stack;
+    private BugReporter bugReporter;
+    private OpcodeStack stack;
 
-	/**
-	 * constructs a WI detector given the reporter to report bugs on
-	 *
-	 * @param bugReporter
-	 *            the sync of bug reports
-	 */
-	public WiringIssues(BugReporter bugReporter) {
-		this.bugReporter = bugReporter;
-	}
+    /**
+     * constructs a WI detector given the reporter to report bugs on
+     *
+     * @param bugReporter
+     *            the sync of bug reports
+     */
+    public WiringIssues(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
 
-	@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "SF_SWITCH_NO_DEFAULT", justification = "Only a few cases need special handling")
-	@Override
-	public void visitClassContext(ClassContext classContext) {
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "SF_SWITCH_NO_DEFAULT", justification = "Only a few cases need special handling")
+    @Override
+    public void visitClassContext(ClassContext classContext) {
 
-		try {
-			JavaClass cls = classContext.getJavaClass();
+        try {
+            JavaClass cls = classContext.getJavaClass();
 
-			Field[] fields = cls.getFields();
+            Field[] fields = cls.getFields();
 
-			if (fields.length > 0) {
+            if (fields.length > 0) {
 
-				Map<WiringType, FieldAnnotation> wiredFields = new HashMap<>();
-				boolean loadedParents = false;
+                Map<WiringType, FieldAnnotation> wiredFields = new HashMap<>();
+                boolean loadedParents = false;
 
-				for (Field field : fields) {
-					boolean hasAutowired = false;
-					String qualifier = "";
-					for (AnnotationEntry entry : field.getAnnotationEntries()) {
-						switch (entry.getAnnotationType()) {
-						case SPRING_AUTOWIRED:
-							if (!loadedParents) {
-								loadParentAutowireds(cls.getSuperClass(), wiredFields);
-								loadedParents = true;
-							}
-							hasAutowired = true;
-							break;
+                for (Field field : fields) {
+                    boolean hasAutowired = false;
+                    String qualifier = "";
+                    for (AnnotationEntry entry : field.getAnnotationEntries()) {
+                        switch (entry.getAnnotationType()) {
+                            case SPRING_AUTOWIRED:
+                                if (!loadedParents) {
+                                    loadParentAutowireds(cls.getSuperClass(), wiredFields);
+                                    loadedParents = true;
+                                }
+                                hasAutowired = true;
+                            break;
 
-						case SPRING_QUALIFIER:
-							qualifier = entry.getElementValuePairs()[0].getValue().stringifyValue();
-							break;
-						}
-					}
+                            case SPRING_QUALIFIER:
+                                qualifier = entry.getElementValuePairs()[0].getValue().stringifyValue();
+                            break;
+                        }
+                    }
 
-					if (hasAutowired) {
-						WiringType wt = new WiringType(field.getSignature(), field.getGenericSignature(), qualifier);
-						FieldAnnotation existingAnnotation = wiredFields.get(wt);
-						if (existingAnnotation == null) {
-							wiredFields.put(wt, FieldAnnotation.fromBCELField(cls.getClassName(), field));
-						} else {
-							bugReporter.reportBug(
-									new BugInstance(this, BugType.WI_DUPLICATE_WIRED_TYPES.name(), NORMAL_PRIORITY)
-											.addClass(cls).addField(FieldAnnotation.fromBCELField(cls, field))
-											.addField(existingAnnotation));
-							wiredFields.remove(wt);
-						}
-					}
-				}
-			}
+                    if (hasAutowired) {
+                        WiringType wt = new WiringType(field.getSignature(), field.getGenericSignature(), qualifier);
+                        FieldAnnotation existingAnnotation = wiredFields.get(wt);
+                        if (existingAnnotation == null) {
+                            wiredFields.put(wt, FieldAnnotation.fromBCELField(cls.getClassName(), field));
+                        } else {
+                            bugReporter.reportBug(new BugInstance(this, BugType.WI_DUPLICATE_WIRED_TYPES.name(), NORMAL_PRIORITY).addClass(cls)
+                                    .addField(FieldAnnotation.fromBCELField(cls, field)).addField(existingAnnotation));
+                            wiredFields.remove(wt);
+                        }
+                    }
+                }
+            }
 
-			stack = new OpcodeStack();
-			super.visitClassContext(classContext);
-		} catch (ClassNotFoundException e) {
-			bugReporter.reportMissingClass(e);
-		} finally {
-			stack = null;
-		}
-	}
+            stack = new OpcodeStack();
+            super.visitClassContext(classContext);
+        } catch (ClassNotFoundException e) {
+            bugReporter.reportMissingClass(e);
+        } finally {
+            stack = null;
+        }
+    }
 
-	@Override
-	public void visitCode(Code obj) {
-		Method m = getMethod();
-		for (AnnotationEntry annotation : m.getAnnotationEntries()) {
-			String type = annotation.getAnnotationType();
-			if (type.startsWith("Lorg/junit/") || type.startsWith("Lorg/testng/")) {
-				return;
-			}
-		}
-		stack.resetForMethodEntry(this);
-		super.visitCode(obj);
-	}
+    @Override
+    public void visitCode(Code obj) {
+        Method m = getMethod();
+        for (AnnotationEntry annotation : m.getAnnotationEntries()) {
+            String type = annotation.getAnnotationType();
+            if (type.startsWith("Lorg/junit/") || type.startsWith("Lorg/testng/")) {
+                return;
+            }
+        }
+        stack.resetForMethodEntry(this);
+        super.visitCode(obj);
+    }
 
-	@Override
-	public void sawOpcode(int seen) {
-		try {
-			if ((seen == INVOKESPECIAL) && Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
-				String clsName = getClassConstantOperand();
-				if (Statistics.getStatistics().isAutowiredBean(clsName.replace('/', '.'))) {
-					String signature = getSigConstantOperand();
-					int numParms = SignatureUtils.getNumParameters(signature);
-					if (stack.getStackDepth() > numParms) {
-						OpcodeStack.Item itm = stack.getStackItem(numParms);
-						if (itm.getRegisterNumber() != 0) {
-							bugReporter.reportBug(
-									new BugInstance(this, BugType.WI_MANUALLY_ALLOCATING_AN_AUTOWIRED_BEAN.name(),
-											NORMAL_PRIORITY).addClass(this).addMethod(this).addSourceLine(this));
-						}
-					}
-				}
-			}
-		} finally {
-			stack.sawOpcode(this, seen);
-		}
-	}
+    @Override
+    public void sawOpcode(int seen) {
+        try {
+            if ((seen == INVOKESPECIAL) && Values.CONSTRUCTOR.equals(getNameConstantOperand())) {
+                String clsName = getClassConstantOperand();
+                if (Statistics.getStatistics().isAutowiredBean(clsName.replace('/', '.'))) {
+                    String signature = getSigConstantOperand();
+                    int numParms = SignatureUtils.getNumParameters(signature);
+                    if (stack.getStackDepth() > numParms) {
+                        OpcodeStack.Item itm = stack.getStackItem(numParms);
+                        if (itm.getRegisterNumber() != 0) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.WI_MANUALLY_ALLOCATING_AN_AUTOWIRED_BEAN.name(), NORMAL_PRIORITY).addClass(this)
+                                    .addMethod(this).addSourceLine(this));
+                        }
+                    }
+                }
+            }
+        } finally {
+            stack.sawOpcode(this, seen);
+        }
+    }
 
-	/**
-	 * loads all the types that are injected by @Autowired annotations in super
-	 * classes
-	 *
-	 * @param cls
-	 *            the class who's parents you want to load
-	 * @param wiredFields
-	 *            the collected map of autowired types
-	 * @throws ClassNotFoundException
-	 *             if a parent class can't be loaded
-	 */
-	@edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "SF_SWITCH_NO_DEFAULT", justification = "Only a few cases need special handling")
-	private void loadParentAutowireds(JavaClass cls, Map<WiringType, FieldAnnotation> wiredFields)
-			throws ClassNotFoundException {
+    /**
+     * loads all the types that are injected by @Autowired annotations in super classes
+     *
+     * @param cls
+     *            the class who's parents you want to load
+     * @param wiredFields
+     *            the collected map of autowired types
+     * @throws ClassNotFoundException
+     *             if a parent class can't be loaded
+     */
+    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(value = "SF_SWITCH_NO_DEFAULT", justification = "Only a few cases need special handling")
+    private void loadParentAutowireds(JavaClass cls, Map<WiringType, FieldAnnotation> wiredFields) throws ClassNotFoundException {
 
-		if (Values.DOTTED_JAVA_LANG_OBJECT.equals(cls.getClassName())) {
-			return;
-		}
+        if (Values.DOTTED_JAVA_LANG_OBJECT.equals(cls.getClassName())) {
+            return;
+        }
 
-		loadParentAutowireds(cls.getSuperClass(), wiredFields);
+        loadParentAutowireds(cls.getSuperClass(), wiredFields);
 
-		Field[] fields = cls.getFields();
+        Field[] fields = cls.getFields();
 
-		if (fields.length > 0) {
+        if (fields.length > 0) {
 
-			for (Field field : fields) {
-				boolean hasAutowired = false;
-				String qualifier = "";
-				for (AnnotationEntry entry : field.getAnnotationEntries()) {
-					switch (entry.getAnnotationType()) {
-					case SPRING_AUTOWIRED:
-						hasAutowired = true;
-						break;
+            for (Field field : fields) {
+                boolean hasAutowired = false;
+                String qualifier = "";
+                for (AnnotationEntry entry : field.getAnnotationEntries()) {
+                    switch (entry.getAnnotationType()) {
+                        case SPRING_AUTOWIRED:
+                            hasAutowired = true;
+                        break;
 
-					case SPRING_QUALIFIER:
-						qualifier = entry.getElementValuePairs()[0].getValue().stringifyValue();
-						break;
-					}
-				}
+                        case SPRING_QUALIFIER:
+                            qualifier = entry.getElementValuePairs()[0].getValue().stringifyValue();
+                        break;
+                    }
+                }
 
-				if (hasAutowired) {
-					WiringType wt = new WiringType(field.getSignature(), field.getGenericSignature(), qualifier);
-					wiredFields.put(wt, FieldAnnotation.fromBCELField(cls.getClassName(), field));
-				}
-			}
-		}
-	}
+                if (hasAutowired) {
+                    WiringType wt = new WiringType(field.getSignature(), field.getGenericSignature(), qualifier);
+                    wiredFields.put(wt, FieldAnnotation.fromBCELField(cls.getClassName(), field));
+                }
+            }
+        }
+    }
 
-	/**
-	 * represents the type of object that is to be wired in, including an optional
-	 * qualifier name
-	 */
-	static class WiringType {
-		String signature;
-		String genericSignature;
-		String qualifier;
+    /**
+     * represents the type of object that is to be wired in, including an optional qualifier name
+     */
+    static class WiringType {
+        String signature;
+        String genericSignature;
+        String qualifier;
 
-		public WiringType(String fieldSignature, String genSignature, String qualifierName) {
-			signature = fieldSignature;
-			genericSignature = genSignature;
-			qualifier = qualifierName;
-		}
+        public WiringType(String fieldSignature, String genSignature, String qualifierName) {
+            signature = fieldSignature;
+            genericSignature = genSignature;
+            qualifier = qualifierName;
+        }
 
-		@Override
-		public boolean equals(Object o) {
-			if (!(o instanceof WiringType)) {
-				return false;
-			}
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof WiringType)) {
+                return false;
+            }
 
-			WiringType that = (WiringType) o;
-			return signature.equals(that.signature) && Objects.equals(genericSignature, that.genericSignature)
-					&& qualifier.equals(that.qualifier);
-		}
+            WiringType that = (WiringType) o;
+            return signature.equals(that.signature) && Objects.equals(genericSignature, that.genericSignature) && qualifier.equals(that.qualifier);
+        }
 
-		@Override
-		public int hashCode() {
-			return signature.hashCode() ^ qualifier.hashCode()
-					^ ((genericSignature == null) ? 0 : genericSignature.hashCode());
-		}
+        @Override
+        public int hashCode() {
+            return signature.hashCode() ^ qualifier.hashCode() ^ ((genericSignature == null) ? 0 : genericSignature.hashCode());
+        }
 
-		@Override
-		public String toString() {
-			return ToString.build(this);
-		}
+        @Override
+        public String toString() {
+            return ToString.build(this);
+        }
 
-	}
+    }
 }
