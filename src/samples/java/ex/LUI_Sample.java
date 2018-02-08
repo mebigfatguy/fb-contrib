0cdbe6a3e387c8dc0a34eb1ff0152b20d96b9360
Dave Brosius <dbrosius@mebigfatguy.com>
2018-02-07 20:48:34 -0500

    add LUI detector

---
+++ src/samples/java/ex/LUI_Sample.java
@@ -0,0 +1,15 @@
+package ex;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LUI_Sample {
+
+    public List<String> testUseSingleton() {
+        return Arrays.asList("foo");
+    }
+
+    public List<String> fpWithArray() {
+        return Arrays.asList("foo", "bar");
+    }
+}
