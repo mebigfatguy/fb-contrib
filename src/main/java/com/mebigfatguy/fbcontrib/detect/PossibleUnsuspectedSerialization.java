48bc3c15cc9d8068a68235d853ec48f94013dc4c
Dave Brosius <dbrosius@mebigfatguy.com>
2018-02-28 21:33:17 -0500

    remove deprecations around bcel's Constants interface

--- src/main/java/com/mebigfatguy/fbcontrib/detect/PossibleUnsuspectedSerialization.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/PossibleUnsuspectedSerialization.java
@@ -18,6 +18,7 @@
  */
 package com.mebigfatguy.fbcontrib.detect;
 
+import org.apache.bcel.Const;
 import org.apache.bcel.classfile.Field;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
@@ -90,7 +91,7 @@ public class PossibleUnsuspectedSerialization extends BytecodeScanningDetector {
         try {
             stack.precomputation(this);
 
-            if (seen == INVOKEVIRTUAL) {
+            if (seen == Const.INVOKEVIRTUAL) {
                 String clsName = getClassConstantOperand();
                 if ("java/io/ObjectOutputStream".equals(clsName)) {
                     String name = getNameConstantOperand();
