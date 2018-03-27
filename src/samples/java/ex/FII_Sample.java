d0ede55b449400d121f2313b2f58173333bbd322
Dave Brosius <dbrosius@mebigfatguy.com>
2018-03-27 16:07:13 -0400

    add more FII sample methods

--- src/samples/java/ex/FII_Sample.java
+++ src/samples/java/ex/FII_Sample.java
@@ -10,11 +10,29 @@ public class FII_Sample {
         return baubles.stream().filter(b -> b.isFree()).collect(Collectors.toList());
     }
 
+    public List<String> getNames(List<Bauble> baubles) {
+
+        return baubles.stream().map(b -> b.getName()).collect(Collectors.toList());
+    }
+
+    public List<Bauble> getfpFreeBees(List<Bauble> baubles) {
+
+        return baubles.stream().filter(Bauble::isFree).collect(Collectors.toList());
+    }
+
+    public List<String> fpGetNames(List<Bauble> baubles) {
+
+        return baubles.stream().map(Bauble::getName).collect(Collectors.toList());
+    }
+
     public static class Bauble {
+
+        public String getName() {
+            return "golden orb";
+        }
 
         public boolean isFree() {
             return true;
         }
-
     }
 }
