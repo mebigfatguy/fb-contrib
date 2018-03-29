1220444b43360056cbe3481562f556cbfbb45733
Dave Brosius <dbrosius@mebigfatguy.com>
2018-03-28 21:55:01 -0400

    more infrastructure parsing method handles

--- src/main/java/com/mebigfatguy/fbcontrib/detect/FunctionalInterfaceIssues.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/FunctionalInterfaceIssues.java
@@ -25,10 +25,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.bcel.Constants;
+import org.apache.bcel.classfile.Attribute;
 import org.apache.bcel.classfile.Code;
+import org.apache.bcel.classfile.Constant;
 import org.apache.bcel.classfile.ConstantInvokeDynamic;
+import org.apache.bcel.classfile.ConstantMethodHandle;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.classfile.Method;
+import org.apache.bcel.classfile.Unknown;
+
+import com.mebigfatguy.fbcontrib.utils.CodeByteUtils;
 
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
@@ -44,6 +50,7 @@ public class FunctionalInterfaceIssues extends BytecodeScanningDetector {
 
     private BugReporter bugReporter;
     private OpcodeStack stack;
+    private Attribute bootstrapAtt;
     private Map<Method, List<FIInfo>> functionalInterfaceInfo;
 
     public FunctionalInterfaceIssues(BugReporter bugReporter) {
@@ -55,12 +62,17 @@ public class FunctionalInterfaceIssues extends BytecodeScanningDetector {
         try {
             JavaClass cls = classContext.getJavaClass();
             if (cls.getMajor() >= Constants.MAJOR_1_8) {
-                stack = new OpcodeStack();
-                functionalInterfaceInfo = new HashMap<>();
-                super.visitClassContext(classContext);
+                bootstrapAtt = getBootstrapAttribute(cls);
+                if (bootstrapAtt != null) {
+                    stack = new OpcodeStack();
+                    functionalInterfaceInfo = new HashMap<>();
+                    super.visitClassContext(classContext);
+                }
+
             }
         } finally {
             functionalInterfaceInfo = null;
+            bootstrapAtt = null;
             stack = null;
         }
     }
@@ -87,6 +99,8 @@ public class FunctionalInterfaceIssues extends BytecodeScanningDetector {
                     }
 
                     ConstantInvokeDynamic cid = (ConstantInvokeDynamic) getConstantRefOperand();
+
+                    ConstantMethodHandle cmh = getMethodHandle(cid.getBootstrapMethodAttrIndex());
                     FIInfo fii = new FIInfo(getPC(), cid.getBootstrapMethodAttrIndex());
                     fiis.add(fii);
                 break;
@@ -113,9 +127,35 @@ public class FunctionalInterfaceIssues extends BytecodeScanningDetector {
      *            the context object of the current method
      * @return if the class uses synchronization
      */
-    public boolean prescreen(Method method) {
+    private boolean prescreen(Method method) {
         BitSet bytecodeSet = getClassContext().getBytecodeSet(method);
         return (bytecodeSet != null) && (bytecodeSet.get(Constants.INVOKEDYNAMIC));
+    }
+
+    private Attribute getBootstrapAttribute(JavaClass cls) {
+        for (Attribute att : cls.getAttributes()) {
+            if ("BootstrapMethods".equals(att.getName())) {
+                return att;
+            }
+        }
+
+        return null;
+    }
+
+    private ConstantMethodHandle getMethodHandle(int bootstrapIndex) {
+        int offset = bootstrapIndex * 6;
+        byte[] attBytes = ((Unknown) bootstrapAtt).getBytes();
+        int methodRefIndex = CodeByteUtils.getshort(attBytes, offset += 2);
+        int numArgs = CodeByteUtils.getshort(attBytes, offset += 2);
+        for (int i = 0; i < numArgs; i++) {
+            int arg = CodeByteUtils.getshort(attBytes, offset += 2);
+            Constant c = getConstantPool().getConstant(arg);
+            if (c instanceof ConstantMethodHandle) {
+                return (ConstantMethodHandle) c;
+            }
+        }
+
+        return null;
     }
 
     class FIInfo {
