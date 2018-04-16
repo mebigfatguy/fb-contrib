ee269548caec3c72b7b340b2712fc03faa6750c6
Dave Brosius <dbrosius@mebigfatguy.com>
2018-04-15 20:43:46 -0400

    fix missing replacement from getXMethod() to getMethod()

--- src/main/java/com/mebigfatguy/fbcontrib/detect/FunctionalInterfaceIssues.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/FunctionalInterfaceIssues.java
@@ -179,7 +179,7 @@ public class FunctionalInterfaceIssues extends BytecodeScanningDetector {
                                 functionalInterfaceInfo.put(anonName, fiis);
                             }
 
-                            FIInfo fii = new FIInfo(getXMethod(), SourceLineAnnotation.fromVisitedInstruction(this));
+                            FIInfo fii = new FIInfo(getMethod(), SourceLineAnnotation.fromVisitedInstruction(this));
                             fiis.add(fii);
                         }
                     break;
