b1cc2febcbc56e5e923b9e0cf0a4d084e8941fc8
Dave Brosius <dbrosius@mebigfatguy.com>
2018-05-04 17:58:00 -0400

    move inner class out to top level

---
+++ src/samples/java/ex/ce/Envy.java
@@ -0,0 +1,21 @@
+package ex.ce;
+
+public class Envy {
+    public void a() {
+    }
+
+    public void b() {
+    }
+
+    public void c() {
+    }
+
+    public void d() {
+    }
+
+    public void e() {
+    }
+
+    public void f() {
+    }
+}
