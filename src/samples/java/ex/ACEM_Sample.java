87ce8a2949c47fde6ce866e4d2195e0512d7685a
Dave Brosius <dbrosius@mebigfatguy.com>
2018-05-26 11:07:54 -0400

    add ACEM fp for empty lambda methods

--- src/samples/java/ex/ACEM_Sample.java
+++ src/samples/java/ex/ACEM_Sample.java
@@ -1,18 +1,21 @@
 package ex;
 
 public abstract class ACEM_Sample implements Foo {
+
+    public static final Runnable fpNOP283 = () -> {
+    };
+
     public void test() {
     }
 
     public int test1() {
         throw new UnsupportedOperationException("Not implemented");
     }
-    
+
     @Override
     public void doFPFoo() {
     }
-    
-    
+
 }
 
 interface Foo {
