faf093ab1c7552e735856b288065a7c42c071b66
Dave Brosius <dbrosius@mebigfatguy.com>
2018-11-30 23:51:24 -0500

    add deflator/inflator examples for IOI for #318

--- src/samples/java/ex/IOI_Sample.java
+++ src/samples/java/ex/IOI_Sample.java
@@ -9,47 +9,63 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 import org.apache.commons.io.IOUtils;
 
 public class IOI_Sample {
 
-    public byte[] getIOIData(File f) throws IOException {
-        try (InputStream is = new BufferedInputStream(new FileInputStream(f)); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+	public byte[] getIOIData(File f) throws IOException {
+		try (InputStream is = new BufferedInputStream(new FileInputStream(f));
+				ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
-            IOUtils.copy(is, baos);
+			IOUtils.copy(is, baos);
 
-            return baos.toByteArray();
-        }
-    }
+			return baos.toByteArray();
+		}
+	}
 
-    public byte[] getIOIReaderData(File f) throws IOException {
-        try (FileReader r = new FileReader(f); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+	public byte[] getIOIReaderData(File f) throws IOException {
+		try (FileReader r = new FileReader(f); ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
-            IOUtils.copy(r, baos);
+			IOUtils.copy(r, baos);
 
-            return baos.toByteArray();
-        }
-    }
+			return baos.toByteArray();
+		}
+	}
 
-    public void copy(String inName, String outName) throws IOException {
-        byte[] buf = new byte[1024];
+	public void copy(String inName, String outName) throws IOException {
+		byte[] buf = new byte[1024];
 
-        try (InputStream is = new FileInputStream(inName); OutputStream os = new FileOutputStream(outName);) {
+		try (InputStream is = new FileInputStream(inName); OutputStream os = new FileOutputStream(outName);) {
 
-            int size = is.read(buf);
-            while (size >= 0) {
-                os.write(buf, 0, size);
-                size = is.read(buf);
-            }
-        }
-    }
+			int size = is.read(buf);
+			while (size >= 0) {
+				os.write(buf, 0, size);
+				size = is.read(buf);
+			}
+		}
+	}
 
-    class FPBofoStream extends FileInputStream {
+	public void deflatorMisuse(byte[] in, byte[] out) {
+		Deflater compresser = new Deflater();
+		compresser.setInput(b);
+		compresser.finish();
+		compresser.deflate(out);
+	}
 
-        public FPBofoStream(File f) throws IOException {
-            super(f);
-        }
+	public void inflatorMisuse(byte[] in, byte[] out) {
+		Inflater expander = new Inflater();
+		expander.setInput(b);
+		expander.inflate(out);
+	}
 
-    }
+	class FPBofoStream extends FileInputStream {
+
+		public FPBofoStream(File f) throws IOException {
+			super(f);
+		}
+
+	}
 }
