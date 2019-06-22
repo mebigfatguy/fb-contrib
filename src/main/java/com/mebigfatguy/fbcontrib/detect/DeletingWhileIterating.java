4038129b65ed90e31cec58bafdaec125260f0648
Dave Brosius <dbrosius@mebigfatguy.com>
2019-06-21 20:18:24 -0400

    find DWI occurences when the variable is a collection class, not interface for #352

--- src/main/java/com/mebigfatguy/fbcontrib/detect/DeletingWhileIterating.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/DeletingWhileIterating.java
@@ -175,7 +175,7 @@ public class DeletingWhileIterating extends AbstractCollectionScanningDetector {
         try {
             stack.precomputation(this);
 
-            if (seen == INVOKEINTERFACE) {
+            if (seen == INVOKEINTERFACE || seen == INVOKEVIRTUAL) {
                 String className = getClassConstantOperand();
                 String methodName = getNameConstantOperand();
                 String signature = getSigConstantOperand();
