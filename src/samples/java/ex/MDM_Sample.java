bdb9ca015a2aa25c50a8811f2c7d61fc5b071249
Dave Brosius <dbrosius@mebigfatguy.com>
2023-03-15 13:13:49 -0400

    add CyclicBarrier.await to MDM sample test

--- src/samples/java/ex/MDM_Sample.java
+++ src/samples/java/ex/MDM_Sample.java
@@ -1,10 +1,13 @@
 package ex;
+
 import java.math.BigDecimal;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.security.SecureRandom;
 import java.util.Locale;
 import java.util.Random;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,102 +16,107 @@ import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocketFactory;
 
 public class MDM_Sample implements Runnable {
-    private ReentrantLock myLock;
-    
-    
-    public MDM_Sample() throws Exception {
-        boolean b;
+	private ReentrantLock myLock;
+	private CyclicBarrier barrier;
 
-        { // Halt tests
-            Runtime r = Runtime.getRuntime();
-            r.exit(0); // WARNING
-            r.halt(0); // WARNING
-            r.runFinalization(); // WARNING
-            System.runFinalization(); // WARNING
-        }
+	public MDM_Sample() throws Exception {
+		boolean b;
 
-        { // equals() tests
-            BigDecimal bd1 = new BigDecimal(0);
-            BigDecimal bd2 = new BigDecimal(0);
-            b = bd1.equals(bd2); // WARNING
-        }
+		{ // Halt tests
+			Runtime r = Runtime.getRuntime();
+			r.exit(0); // WARNING
+			r.halt(0); // WARNING
+			r.runFinalization(); // WARNING
+			System.runFinalization(); // WARNING
+		}
 
-        { // Socket tests
-            InetAddress localhost = InetAddress.getLocalHost(); // WARNING
-            if (localhost == null) {
-                localhost = InetAddress.getByName("booya");
-            }
-            ServerSocket ss = new ServerSocket(0);
-            touch(ss); // WARNING
-            ss = new ServerSocket(0, 0);
-            touch(ss); // WARNING
-            ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
-            ss = ssf.createServerSocket(0);
-            touch(ss); // WARNING
-            ss = ssf.createServerSocket(0, 0);
-            touch(ss); // WARNING
-        }
+		{ // equals() tests
+			BigDecimal bd1 = new BigDecimal(0);
+			BigDecimal bd2 = new BigDecimal(0);
+			b = bd1.equals(bd2); // WARNING
+		}
 
-        { // RNG tests
-            Random r = new Random();// WARNING
-            touch(r);
-            byte[] seed = SecureRandom.getSeed(1); // WARNING (jdk 1.5 or older)
-            r = new SecureRandom(seed); // WARNING (jdk 1.5 or older)
-            touch(r);
-        }
+		{ // Socket tests
+			InetAddress localhost = InetAddress.getLocalHost(); // WARNING
+			if (localhost == null) {
+				localhost = InetAddress.getByName("booya");
+			}
+			ServerSocket ss = new ServerSocket(0);
+			touch(ss); // WARNING
+			ss = new ServerSocket(0, 0);
+			touch(ss); // WARNING
+			ServerSocketFactory ssf = SSLServerSocketFactory.getDefault();
+			ss = ssf.createServerSocket(0);
+			touch(ss); // WARNING
+			ss = ssf.createServerSocket(0, 0);
+			touch(ss); // WARNING
+		}
 
-        { // Thread tests
-            Thread t = new Thread(this);
-            int priority = t.getPriority(); // WARNING
-            t.setPriority(priority); // WARNING
-            t.join(); // WARNING
+		{ // RNG tests
+			Random r = new Random();// WARNING
+			touch(r);
+			byte[] seed = SecureRandom.getSeed(1); // WARNING (jdk 1.5 or older)
+			r = new SecureRandom(seed); // WARNING (jdk 1.5 or older)
+			touch(r);
+		}
 
-            Thread.sleep(0); // WARNING
-            Thread.sleep(0, 0); // WARNING
-            Thread.yield(); // WARNING
-        }
+		{ // Thread tests
+			Thread t = new Thread(this);
+			int priority = t.getPriority(); // WARNING
+			t.setPriority(priority); // WARNING
+			t.join(); // WARNING
 
-        { // Timeout tests
-            ReentrantLock rl = new ReentrantLock();
-            rl.lock(); // WARNING
-            rl.lockInterruptibly(); // WARNING
-            b = rl.isHeldByCurrentThread(); // WARNING
-            b = rl.isLocked(); // WARNING
+			Thread.sleep(0); // WARNING
+			Thread.sleep(0, 0); // WARNING
+			Thread.yield(); // WARNING
+		}
 
-            Object o = new Object();
-            do {
-                b = rl.tryLock(); // WARNING
-                o.wait(); // WARNING
-            } while (b);
+		{ // Timeout tests
+			ReentrantLock rl = new ReentrantLock();
+			rl.lock(); // WARNING
+			rl.lockInterruptibly(); // WARNING
+			b = rl.isHeldByCurrentThread(); // WARNING
+			b = rl.isLocked(); // WARNING
 
-            Lock l = rl;
-            l.lock(); // WARNING
-            b = l.tryLock();
-            touch(b); // WARNING
-            l.lockInterruptibly(); // WARNING
+			Object o = new Object();
+			do {
+				b = rl.tryLock(); // WARNING
+				o.wait(); // WARNING
+			} while (b);
 
-            Condition c = l.newCondition();
-            c.signal(); // WARNING
-            c.await(); // WARNING
-        }
+			Lock l = rl;
+			l.lock(); // WARNING
+			b = l.tryLock();
+			touch(b); // WARNING
+			l.lockInterruptibly(); // WARNING
 
-        { // String tests
-            byte[] bytes = "".getBytes(); // WARNING
-            String s = new String(bytes); // WARNING
-            bytes = s.getBytes("UTF-8");
+			Condition c = l.newCondition();
+			c.signal(); // WARNING
+			c.await(); // WARNING
+		}
 
-            Locale.setDefault(Locale.ENGLISH); // WARNING
-        }
-    }
+		{ // String tests
+			byte[] bytes = "".getBytes(); // WARNING
+			String s = new String(bytes); // WARNING
+			bytes = s.getBytes("UTF-8");
 
-    @Override
-    public void run() {
-    }
+			Locale.setDefault(Locale.ENGLISH); // WARNING
+		}
+	}
 
-    private static void touch(Object o) {
-    }
-    
-    private void fpAssertReentrantLock() {
-        assert myLock.isHeldByCurrentThread();
-    }
+	@Override
+	public void run() {
+	}
+
+	private static void touch(Object o) {
+	}
+
+	private void fpAssertReentrantLock() {
+		assert myLock.isHeldByCurrentThread();
+	}
+
+	private void fpCyclicBarrier() throws Exception {
+		barrier.await(1, TimeUnit.MINUTES);
+
+	}
 }
