1fc9e00584edd63ceb0ab7b4cdf7c8805c048bd2
Dave Brosius <dbrosius@mebigfatguy.com>
2023-03-15 14:40:27 -0400

    guard against calls from a subclass of ToStringBuilder

--- src/main/java/com/mebigfatguy/fbcontrib/detect/CommonsStringBuilderToString.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/CommonsStringBuilderToString.java
@@ -40,9 +40,11 @@ import edu.umd.cs.findbugs.OpcodeStack.Item;
 import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
 
 /**
- * Find usage of ToStringBuilder from Apache commons, where the code invokes toString() on the constructed object without invoking append().
+ * Find usage of ToStringBuilder from Apache commons, where the code invokes
+ * toString() on the constructed object without invoking append().
  *
- * Usage without invoking append is equivalent of using the Object.toString() method
+ * Usage without invoking append is equivalent of using the Object.toString()
+ * method
  *
  * <pre>
  * new ToStringBuilder(this).toString();
@@ -50,126 +52,140 @@ import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
  */
 public class CommonsStringBuilderToString extends OpcodeStackDetector {
 
-    private static final Set<String> TOSTRINGBUILDER_CTOR_SIGS = UnmodifiableSet.create(
-            new SignatureBuilder().withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT).toString(),
-            new SignatureBuilder().withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT, "org/apache/commons/lang/builder/ToStringStyle").toString(),
-            new SignatureBuilder().withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT, "org/apache/commons/lang3/builder/ToStringStyle").toString());
+	private static final Set<String> TOSTRINGBUILDER_CTOR_SIGS = UnmodifiableSet.create(
+			new SignatureBuilder().withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT).toString(),
+			new SignatureBuilder()
+					.withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT, "org/apache/commons/lang/builder/ToStringStyle")
+					.toString(),
+			new SignatureBuilder()
+					.withParamTypes(Values.SLASHED_JAVA_LANG_OBJECT, "org/apache/commons/lang3/builder/ToStringStyle")
+					.toString());
 
-    private final BugReporter bugReporter;
-    private final Stack<StringBuilderInvokedStatus> stackTracker = new Stack<>();
-    private final Map<Integer, Boolean> registerTracker = new HashMap<>(10);
+	private final BugReporter bugReporter;
+	private final Stack<StringBuilderInvokedStatus> stackTracker = new Stack<>();
+	private final Map<Integer, Boolean> registerTracker = new HashMap<>(10);
 
-    /**
-     * constructs a CSBTS detector given the reporter to report bugs on.
-     *
-     * @param bugReporter
-     *            the sync of bug reports
-     */
-    public CommonsStringBuilderToString(final BugReporter bugReporter) {
-        this.bugReporter = bugReporter;
-    }
+	/**
+	 * constructs a CSBTS detector given the reporter to report bugs on.
+	 *
+	 * @param bugReporter the sync of bug reports
+	 */
+	public CommonsStringBuilderToString(final BugReporter bugReporter) {
+		this.bugReporter = bugReporter;
+	}
 
-    @Override
-    public void visit(Code obj) {
-        registerTracker.clear();
-        stackTracker.clear();
-        super.visit(obj);
-    }
+	@Override
+	public void visit(Code obj) {
+		registerTracker.clear();
+		stackTracker.clear();
+		super.visit(obj);
+	}
 
-    @Override
-    public boolean shouldVisitCode(Code obj) {
-        LocalVariableTable lvt = getMethod().getLocalVariableTable();
-        return lvt != null;
-    }
+	@Override
+	public boolean shouldVisitCode(Code obj) {
+		LocalVariableTable lvt = getMethod().getLocalVariableTable();
+		return lvt != null;
+	}
 
-    @Override
-    public void sawOpcode(int seen) {
-        switch (seen) {
-            case ALOAD:
-            case ALOAD_0:
-            case ALOAD_1:
-            case ALOAD_2:
-            case ALOAD_3:
-                LocalVariable lv = getMethod().getLocalVariableTable().getLocalVariable(RegisterUtils.getALoadReg(this, seen), getNextPC());
-                if (lv != null) {
-                    String signature = lv.getSignature();
-                    if (isToStringBuilder(signature)) {
-                        Integer loadReg = Integer.valueOf(getRegisterOperand());
-                        Boolean appendInvoked = registerTracker.get(loadReg);
-                        if (appendInvoked != null) {
-                            stackTracker.add(new StringBuilderInvokedStatus(loadReg.intValue(), appendInvoked.booleanValue()));
-                        }
-                    }
-                }
-            break;
-            case ASTORE:
-            case ASTORE_0:
-            case ASTORE_1:
-            case ASTORE_2:
-            case ASTORE_3:
-                Item si = stack.getStackItem(0);
-                String signature = si.getSignature();
-                if (isToStringBuilder(signature)) {
-                    int storeReg = getRegisterOperand();
-                    StringBuilderInvokedStatus p = stackTracker.pop();
-                    registerTracker.put(Integer.valueOf(storeReg), p.register == -1 ? Boolean.FALSE : registerTracker.get(Integer.valueOf(p.register)));
-                }
-            break;
-            case POP:
-                si = stack.getStackItem(0);
-                signature = si.getSignature();
-                if (isToStringBuilder(signature) && !stackTracker.isEmpty()) {
-                    StringBuilderInvokedStatus p = stackTracker.pop();
-                    registerTracker.put(Integer.valueOf(p.register), Boolean.valueOf(p.appendInvoked));
-                }
-            break;
-            case INVOKESPECIAL:
-            case INVOKEVIRTUAL:
-                String loadClassName = getClassConstantOperand();
-                String calledMethodName = getNameConstantOperand();
+	@Override
+	public void sawOpcode(int seen) {
+		switch (seen) {
+		case ALOAD:
+		case ALOAD_0:
+		case ALOAD_1:
+		case ALOAD_2:
+		case ALOAD_3:
+			LocalVariable lv = getMethod().getLocalVariableTable()
+					.getLocalVariable(RegisterUtils.getALoadReg(this, seen), getNextPC());
+			if (lv != null) {
+				String signature = lv.getSignature();
+				if (isToStringBuilder(signature)) {
+					Integer loadReg = Integer.valueOf(getRegisterOperand());
+					Boolean appendInvoked = registerTracker.get(loadReg);
+					if (appendInvoked != null) {
+						stackTracker
+								.add(new StringBuilderInvokedStatus(loadReg.intValue(), appendInvoked.booleanValue()));
+					}
+				}
+			}
+			break;
+		case ASTORE:
+		case ASTORE_0:
+		case ASTORE_1:
+		case ASTORE_2:
+		case ASTORE_3:
+			Item si = stack.getStackItem(0);
+			String signature = si.getSignature();
+			if (isToStringBuilder(signature)) {
+				int storeReg = getRegisterOperand();
+				StringBuilderInvokedStatus p = stackTracker.pop();
+				registerTracker.put(Integer.valueOf(storeReg),
+						p.register == -1 ? Boolean.FALSE : registerTracker.get(Integer.valueOf(p.register)));
+			}
+			break;
+		case POP:
+			si = stack.getStackItem(0);
+			signature = si.getSignature();
+			if (isToStringBuilder(signature) && !stackTracker.isEmpty()) {
+				StringBuilderInvokedStatus p = stackTracker.pop();
+				registerTracker.put(Integer.valueOf(p.register), Boolean.valueOf(p.appendInvoked));
+			}
+			break;
+		case INVOKESPECIAL:
+		case INVOKEVIRTUAL:
+			String loadClassName = getClassConstantOperand();
+			String calledMethodName = getNameConstantOperand();
 
-                if ("org/apache/commons/lang3/builder/ToStringBuilder".equals(loadClassName)
-                        || "org/apache/commons/lang/builder/ToStringBuilder".equals(loadClassName)) {
-                    String calledMethodSig = getSigConstantOperand();
-                    if (Values.CONSTRUCTOR.equals(calledMethodName) && TOSTRINGBUILDER_CTOR_SIGS.contains(calledMethodSig)) {
-                        stackTracker.add(new StringBuilderInvokedStatus(-1, false));
-                    } else if ("append".equals(calledMethodName)) {
-                        StringBuilderInvokedStatus p = stackTracker.pop();
-                        stackTracker.add(new StringBuilderInvokedStatus(p.register, true));
-                    } else if (Values.TOSTRING.equals(calledMethodName) && SignatureBuilder.SIG_VOID_TO_STRING.equals(calledMethodSig)) {
-                        StringBuilderInvokedStatus p = stackTracker.pop();
-                        if (!p.appendInvoked) {
-                            bugReporter.reportBug(new BugInstance(this, "CSBTS_COMMONS_STRING_BUILDER_TOSTRING", HIGH_PRIORITY).addClass(this).addMethod(this)
-                                    .addSourceLine(this));
-                        }
-                    }
-                }
-            break;
+			if ("org/apache/commons/lang3/builder/ToStringBuilder".equals(loadClassName)
+					|| "org/apache/commons/lang/builder/ToStringBuilder".equals(loadClassName)) {
+				String calledMethodSig = getSigConstantOperand();
+				if (Values.CONSTRUCTOR.equals(calledMethodName)
+						&& TOSTRINGBUILDER_CTOR_SIGS.contains(calledMethodSig)) {
+					stackTracker.add(new StringBuilderInvokedStatus(-1, false));
+				} else if ("append".equals(calledMethodName)) {
+					if (stackTracker.size() > 0) {
+						StringBuilderInvokedStatus p = stackTracker.pop();
+						stackTracker.add(new StringBuilderInvokedStatus(p.register, true));
+					} else {
+						stackTracker.add(new StringBuilderInvokedStatus(0, true));
+					}
+				} else if (Values.TOSTRING.equals(calledMethodName)
+						&& SignatureBuilder.SIG_VOID_TO_STRING.equals(calledMethodSig)) {
+					StringBuilderInvokedStatus p = stackTracker.pop();
+					if (!p.appendInvoked) {
+						bugReporter
+								.reportBug(new BugInstance(this, "CSBTS_COMMONS_STRING_BUILDER_TOSTRING", HIGH_PRIORITY)
+										.addClass(this).addMethod(this).addSourceLine(this));
+					}
+				}
+			}
+			break;
 
-            default:
-            break;
-        }
-    }
+		default:
+			break;
+		}
+	}
 
-    private static boolean isToStringBuilder(String signature) {
-        return "Lorg/apache/commons/lang3/builder/ToStringBuilder;".equals(signature) || "Lorg/apache/commons/lang/builder/ToStringBuilder;".equals(signature);
-    }
+	private static boolean isToStringBuilder(String signature) {
+		return "Lorg/apache/commons/lang3/builder/ToStringBuilder;".equals(signature)
+				|| "Lorg/apache/commons/lang/builder/ToStringBuilder;".equals(signature);
+	}
 
-    /**
-     * represents an stack item that is an append of a StringBuilder
-     */
-    static final class StringBuilderInvokedStatus {
-        public final int register;
-        public final boolean appendInvoked;
+	/**
+	 * represents an stack item that is an append of a StringBuilder
+	 */
+	static final class StringBuilderInvokedStatus {
+		public final int register;
+		public final boolean appendInvoked;
 
-        StringBuilderInvokedStatus(int register, boolean appendInvoked) {
-            this.register = register;
-            this.appendInvoked = appendInvoked;
-        }
+		StringBuilderInvokedStatus(int register, boolean appendInvoked) {
+			this.register = register;
+			this.appendInvoked = appendInvoked;
+		}
 
-        @Override
-        public String toString() {
-            return ToString.build(this);
-        }
-    }
+		@Override
+		public String toString() {
+			return ToString.build(this);
+		}
+	}
 }
--- src/samples/java/ex/CSBTS_StringToStringSample.java
+++ src/samples/java/ex/CSBTS_StringToStringSample.java
@@ -1,55 +1,71 @@
 package ex;
+
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class CSBTS_StringToStringSample {
-    static class Person {
-        String name;
-        int age;
+	static class Person {
+		String name;
+		int age;
 
-        Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
+		Person(String name, int age) {
+			this.name = name;
+			this.age = age;
+		}
 
-        @Override
-        public String toString() {
-            // INCORRECT USAGE : The same as invoking Object.toString()
-            // return new ToStringBuilder(this).toString();
-            // Consider using for non final classes to support a
-            // string representation for derived types
-            // return ToStringBuilder.reflectionToString(this);
-            // Use for final classes most efficient solution
-            return new ToStringBuilder(this).append("name", name).append("age", age).toString();
-        }
-    }
+		@Override
+		public String toString() {
+			// INCORRECT USAGE : The same as invoking Object.toString()
+			// return new ToStringBuilder(this).toString();
+			// Consider using for non final classes to support a
+			// string representation for derived types
+			// return ToStringBuilder.reflectionToString(this);
+			// Use for final classes most efficient solution
+			return new ToStringBuilder(this).append("name", name).append("age", age).toString();
+		}
+	}
 
-    private enum SEX {
-        Male, Female;
-    }
+	private enum SEX {
+		Male, Female;
+	}
 
-    public final static class GenderPerson extends Person {
-        private SEX sex;
+	public final static class GenderPerson extends Person {
+		private SEX sex;
 
-        GenderPerson(String name, int age, SEX sex) {
-            super(name, age);
-            this.sex = sex;
-        }
-    }
+		GenderPerson(String name, int age, SEX sex) {
+			super(name, age);
+			this.sex = sex;
+		}
+	}
 
-    public static void main(String[] args) {
-        Person p = new Person("John Doe", 2);
-        ToStringBuilder x = new ToStringBuilder(p);
-        ToStringBuilder y = new ToStringBuilder(p);
-        // INCORRECT USAGE : The same as invoking Object.toString
-        System.out.println("P " + new ToStringBuilder(p).toString());
-        // Consider using for non final classes to support a string
-        // representation for derived types
-        System.out.println("P " + ToStringBuilder.reflectionToString(p));
-        GenderPerson p2 = new GenderPerson("Jane Doe", 2, SEX.Female);
-        System.out.println("GP " + new ToStringBuilder(p2).append("name", p2.name).append("age", p2.age).append("sex", p2.sex).toString());
-        // Y now has an append
-        y.append("name", p.name);
-        System.out.println("P - Once Again " + y.toString());
-        System.out.println("P - Again " + x.toString());
-    }
+	public static void main(String[] args) {
+		Person p = new Person("John Doe", 2);
+		ToStringBuilder x = new ToStringBuilder(p);
+		ToStringBuilder y = new ToStringBuilder(p);
+		// INCORRECT USAGE : The same as invoking Object.toString
+		System.out.println("P " + new ToStringBuilder(p).toString());
+		// Consider using for non final classes to support a string
+		// representation for derived types
+		System.out.println("P " + ToStringBuilder.reflectionToString(p));
+		GenderPerson p2 = new GenderPerson("Jane Doe", 2, SEX.Female);
+		System.out.println("GP " + new ToStringBuilder(p2).append("name", p2.name).append("age", p2.age)
+				.append("sex", p2.sex).toString());
+		// Y now has an append
+		y.append("name", p.name);
+		System.out.println("P - Once Again " + y.toString());
+		System.out.println("P - Again " + x.toString());
+	}
+
+	public class FPAppend extends ToStringBuilder {
+
+		FPAppend(Object object) {
+			super(object);
+		}
+
+		@Override
+		public ToStringBuilder append(Object obj) {
+			return super.append(obj);
+		}
+
+	}
+
 }
