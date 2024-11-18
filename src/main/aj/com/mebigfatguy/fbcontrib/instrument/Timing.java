ed6c5616fa28d901f49259c1b390661c417e90b4
Dave Brosius <dbrosius@mebigfatguy.com>
2024-11-17 22:11:22 -0500

    log timings for detectors

--- src/main/aj/com/mebigfatguy/fbcontrib/instrument/Timing.java
+++ src/main/aj/com/mebigfatguy/fbcontrib/instrument/Timing.java
@@ -1,7 +1,12 @@
 package com.mebigfatguy.fbcontrib.instrument;
 
-import java.util.HashMap;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -12,21 +17,54 @@ import edu.umd.cs.findbugs.ba.ClassContext;
 @Aspect
 public class Timing {
 
-	private static final Map<String, TimingInfo> timingInfo = new HashMap<>();
+	private static AtomicReference<Map<String, TimingInfo>> timingInfo = new AtomicReference<>();
+	private static AtomicLong updateTime = new AtomicLong(System.currentTimeMillis());
+	static {
+	    Thread t = new Thread(() -> {
+	    	try {
+		    	while (!Thread.interrupted()) {
+		    		Thread.sleep(16000L);
+		    		if (System.currentTimeMillis() - updateTime.get() > 15000L) {
+		    			Map<String, TimingInfo> infoMap = timingInfo.getAndSet(null);
+		    			if (infoMap != null && !infoMap.isEmpty()) {
+		    				Path p = Files.createTempFile("sb-contrib-timings",".csv");
+		    				try (PrintWriter pr = new PrintWriter(Files.newBufferedWriter(p))) {
+		    					pr.println("detector,number of calls,total time,longest time,longest input");
+		    					for (Map.Entry<String, TimingInfo> entry: infoMap.entrySet()) {
+		    						TimingInfo info = entry.getValue();
+		    						pr.println(entry.getKey() + "," + info.numberOfCalls + "," + info.totalTime+ "," + info.longestTime + "," + (info.longestInput == null ? "--none--" : info.longestInput));
+		    					}
+		    				}
+		    			}
+		    		}
+		    	}
+	    	} catch (Exception e) {
+	    	}
+	    });
+	    t.setDaemon(true);
+	    t.setPriority(Thread.MIN_PRIORITY+1);
+	    t.start();
+	}
 	
     @Around("execution(* com.mebigfatguy.fbcontrib.detect.*.visitClassContext(..)) && args(classContext)")
     public Object timings(ProceedingJoinPoint joinPoint,ClassContext classContext) throws Throwable {
     	long start = System.currentTimeMillis();
+		updateTime.set(start);
     	try {
             Object result = joinPoint.proceed();
             return result;
     	} finally {
     		long delta = System.currentTimeMillis() - start;
     		String detector = joinPoint.getSignature().getDeclaringTypeName();
-    		TimingInfo info = timingInfo.get(detector);
+    		Map<String, TimingInfo> infoMap = timingInfo.get();
+    		if (infoMap == null) {
+    			infoMap = new TreeMap<>();
+    			timingInfo.set(infoMap);
+    		}
+    		TimingInfo info = infoMap.get(detector);
     		if (info == null) {
     			info = new TimingInfo();
-    			timingInfo.put(detector, info);
+    			infoMap.put(detector, info);
     		}
     		info.numberOfCalls++;
     		info.totalTime += delta;
