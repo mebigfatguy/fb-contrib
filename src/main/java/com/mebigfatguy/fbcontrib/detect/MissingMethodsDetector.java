369a5be957cb7a997eb18493bd10954eb19a3142
Dave Brosius <dbrosius@mebigfatguy.com>
2025-07-05 23:10:39 -0400

    better variable naming

--- src/main/java/com/mebigfatguy/fbcontrib/detect/MissingMethodsDetector.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/MissingMethodsDetector.java
@@ -60,7 +60,7 @@ public abstract class MissingMethodsDetector extends BytecodeScanningDetector {
     private boolean sawTernary;
     private boolean isInnerClass;
     private Map<String, Set<FQField>> savedSpecialFields;
-    private String parentClassName;
+    private String outerClassName;
 
     protected MissingMethodsDetector(BugReporter bugReporter) {
         this.bugReporter = bugReporter;
@@ -80,9 +80,9 @@ public abstract class MissingMethodsDetector extends BytecodeScanningDetector {
             isInnerClass = innerPos >= 0;
             
             if (isInnerClass) {
-            	parentClassName = clsName.substring(0, innerPos);
+            	outerClassName = clsName.substring(0, innerPos);
             } else {
-            	parentClassName = null;
+            	outerClassName = null;
             }
 
             clsSignature = SignatureUtils.classToSignature(clsName);
@@ -193,7 +193,7 @@ public abstract class MissingMethodsDetector extends BytecodeScanningDetector {
                     OpcodeStack.Item item = stack.getStackItem(0);
                     XField xf = item.getXField();
                     if (xf != null) {
-                    	if (xf.getClassName() != null && xf.getClassName().equals(parentClassName)) {
+                    	if (xf.getClassName() != null && xf.getClassName().equals(outerClassName)) {
                     		saveSpecialFieldUse(xf.getClassName(), xf.getName(), xf.getSignature());
                     		break;
                     	}
@@ -286,7 +286,7 @@ public abstract class MissingMethodsDetector extends BytecodeScanningDetector {
     }
     
     protected String getParentClassName() {
-    	return parentClassName;
+    	return outerClassName;
     }
 
 
