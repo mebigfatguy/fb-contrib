ad89c117f8b32a75404b98f30a8712f1bbb4fbfb
Dave Brosius <dbrosius@mebigfatguy.com>
2025-11-02 00:39:48 -0400

    add MK_CAPTOR_IN_WHEN

--- src/main/java/com/mebigfatguy/fbcontrib/detect/MockitoIssues.java
+++ src/main/java/com/mebigfatguy/fbcontrib/detect/MockitoIssues.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.apache.bcel.Const;
 import org.apache.bcel.classfile.AnnotationEntry;
 import org.apache.bcel.classfile.ArrayElementValue;
+import org.apache.bcel.classfile.Code;
 import org.apache.bcel.classfile.ElementValue;
 import org.apache.bcel.classfile.ElementValuePair;
 import org.apache.bcel.classfile.Field;
@@ -17,12 +18,20 @@ import com.mebigfatguy.fbcontrib.utils.SignatureUtils;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.BugReporter;
 import edu.umd.cs.findbugs.BytecodeScanningDetector;
+import edu.umd.cs.findbugs.OpcodeStack;
+import edu.umd.cs.findbugs.OpcodeStack.CustomUserValue;
 import edu.umd.cs.findbugs.ba.ClassContext;
 
+@CustomUserValue
 public class MockitoIssues extends BytecodeScanningDetector {
+
+    enum MKUserValue {
+        CAPTOR
+    };
 
     private BugReporter bugReporter;
     private JavaClass cls;
+    private OpcodeStack stack;
     private boolean sawMockitoExtension;
     private boolean sawAnnotatedField;
     private Set<String> mockedFields;
@@ -49,6 +58,7 @@ public class MockitoIssues extends BytecodeScanningDetector {
     public void visitClassContext(ClassContext clsContext) {
         try {
             cls = clsContext.getJavaClass();
+            stack = new OpcodeStack();
             sawMockitoExtension = false;
             sawAnnotatedField = false;
             outer: for (AnnotationEntry ann : cls.getAnnotationEntries()) {
@@ -72,6 +82,7 @@ public class MockitoIssues extends BytecodeScanningDetector {
         } finally {
             mockedFields.clear();
             spiedFields.clear();
+            stack = null;
         }
     }
 
@@ -95,38 +106,99 @@ public class MockitoIssues extends BytecodeScanningDetector {
     }
 
     @Override
+    public void visitCode(Code obj) {
+        stack.resetForMethodEntry(this);
+        super.visitCode(obj);
+    }
+
+    @Override
     public void sawOpcode(int seen) {
-        switch (seen) {
-        case Const.INVOKESTATIC:
-            if (sawMockitoExtension) {
+        MKUserValue userValue = null;
+        try {
+            switch (seen) {
+            case Const.INVOKESTATIC: {
                 String clsName = getClassConstantOperand();
-                if ("org/mockito/MockitoAnnotations".equals(clsName)) {
+                if (sawMockitoExtension) {
+                    if ("org/mockito/MockitoAnnotations".equals(clsName)) {
+                        String methodName = getNameConstantOperand();
+                        if ("openMocks".equals(methodName)) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.MK_UNNEEDED_OPENMOCKS.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
+                                    .addSourceLine(this));
+                        }
+                    }
+                }
+                if ("org/mockito/Mockito".equals(clsName)) {
+                    if ("when".equals(getNameConstantOperand())) {
+                        userValue = processCaptor();
+                        if (userValue == MKUserValue.CAPTOR) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.MK_CAPTOR_IN_WHEN.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
+                                    .addSourceLine(this));
+                        }
+                    }
+                } else {
+                    userValue = processCaptor();
+                }
+                break;
+            }
+
+            case Const.INVOKEVIRTUAL: {
+                String clsName = getClassConstantOperand();
+                if ("org/mockito/ArgumentCaptor".equals(clsName)) {
                     String methodName = getNameConstantOperand();
-                    if ("openMocks".equals(methodName)) {
-                        bugReporter.reportBug(new BugInstance(this, BugType.MK_UNNEEDED_OPENMOCKS.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                                .addSourceLine(this));
+                    if ("capture".equals(methodName)) {
+                        userValue = MKUserValue.CAPTOR;
+                    }
+                } else {
+                    userValue = processCaptor();
+                }
+                break;
+            }
+
+            case Const.INVOKEINTERFACE:
+            case Const.INVOKESPECIAL: {
+                userValue = processCaptor();
+                break;
+            }
+
+            case Const.PUTFIELD: {
+                if (sawAnnotatedField) {
+                    String clsName = getClassConstantOperand().replace('/', '.');
+                    if (clsName.equals(cls.getClassName())) {
+                        String fieldName = getNameConstantOperand();
+                        if (mockedFields.contains(fieldName)) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.MK_MOCKED_FIELD_REASSIGNED.name(), NORMAL_PRIORITY).addClass(this)
+                                    .addMethod(this).addSourceLine(this));
+                        } else if (spiedFields.contains(fieldName)) {
+                            bugReporter.reportBug(new BugInstance(this, BugType.MK_SPIED_FIELD_REASSIGNED.name(), NORMAL_PRIORITY).addClass(this)
+                                    .addMethod(this).addSourceLine(this));
+
+                        }
                     }
                 }
+                break;
             }
-            break;
-
-        case Const.PUTFIELD:
-            if (sawAnnotatedField) {
-                String clsName = getClassConstantOperand().replace('/', '.');
-                if (clsName.equals(cls.getClassName())) {
-                    String fieldName = getNameConstantOperand();
-                    if (mockedFields.contains(fieldName)) {
-                        bugReporter.reportBug(new BugInstance(this, BugType.MK_MOCKED_FIELD_REASSIGNED.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                                .addSourceLine(this));
-                    } else if (spiedFields.contains(fieldName)) {
-                        bugReporter.reportBug(new BugInstance(this, BugType.MK_SPIED_FIELD_REASSIGNED.name(), NORMAL_PRIORITY).addClass(this).addMethod(this)
-                                .addSourceLine(this));
-
-                    }
-                }
             }
-            break;
+        } finally {
+            stack.sawOpcode(this, seen);
+            if ((userValue != null) && (stack.getStackDepth() > 0)) {
+                OpcodeStack.Item item = stack.getStackItem(0);
+                item.setUserValue(userValue);
+            }
         }
+    }
+
+    private MKUserValue processCaptor() {
+        int numParms = SignatureUtils.getNumParameters(getSigConstantOperand());
+        if (numParms > 0 && stack.getStackDepth() >= numParms) {
+            for (int i = 0; i < numParms; i++) {
+                OpcodeStack.Item itm = stack.getStackItem(i);
+                if (itm.getUserValue() == MKUserValue.CAPTOR) {
+                    return MKUserValue.CAPTOR;
+                }
+            }
+        }
+
+        return null;
     }
 
 }
