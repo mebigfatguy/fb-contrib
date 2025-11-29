263fe76fb60c9d62cc2e086eff4348307ef7628a
Dave Brosius <dbrosius@mebigfatguy.com>
2025-11-29 15:46:47 -0500

    fix RegisterUtilsTest

--- src/test/java/com/mebigfatguy/fbcontrib/utils/RegisterUtilsTest.java
+++ src/test/java/com/mebigfatguy/fbcontrib/utils/RegisterUtilsTest.java
@@ -74,6 +74,7 @@ import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
 
 import org.apache.bcel.classfile.Constant;
+import org.apache.bcel.classfile.ConstantClass;
 import org.apache.bcel.classfile.ConstantPool;
 import org.apache.bcel.classfile.ConstantUtf8;
 import org.apache.bcel.classfile.Method;
@@ -96,7 +97,7 @@ public class RegisterUtilsTest {
 
     @BeforeSuite
     public void setUpClass() {
-        FindBugs.setHome("target/spotbugs-3.1.0.RC3.jar");
+        FindBugs.setHome("lib/spotbugs-4.9.8.jar");
     }
 
     @BeforeMethod
@@ -111,41 +112,35 @@ public class RegisterUtilsTest {
     Object[][] parameterRegisters() {
         return new Object[][] {
                 // access flags, signature, expected registers
-                { ACC_PUBLIC | ACC_STATIC, "(JLjava/lang/String;[[I)V", new int[] { 0, 2, 3 } },
-                { ACC_STATIC, "(Ljava/lang/Object;)Z", new int[] { 0 } },
-                { ACC_PUBLIC, "(DJ[D[J)V", new int[] { 1, 3, 5, 6 } },
-                { 0, "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", new int[] { 1, 2 } }, };
+                { ACC_PUBLIC | ACC_STATIC, "(JLjava/lang/String;[[I)V", new int[] { 0, 2, 3 } }, { ACC_STATIC, "(Ljava/lang/Object;)Z", new int[] { 0 } },
+                { ACC_PUBLIC, "(DJ[D[J)V", new int[] { 1, 3, 5, 6 } }, { 0, "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;", new int[] { 1, 2 } }, };
     }
 
     @DataProvider(name = "reg_ASTORE")
     Object[][] reg_ASTORE() {
-        return new Object[][] { { ASTORE, OPERAND }, { ASTORE_0, 0 }, { ASTORE_1, 1 }, { ASTORE_2, 2 }, { ASTORE_3, 3 },
-                { ASTORE_0 - 1, -1 }, { ASTORE_3 + 1, -1 }, };
+        return new Object[][] { { ASTORE, OPERAND }, { ASTORE_0, 0 }, { ASTORE_1, 1 }, { ASTORE_2, 2 }, { ASTORE_3, 3 }, { ASTORE_0 - 1, -1 },
+                { ASTORE_3 + 1, -1 }, };
     }
 
     @DataProvider(name = "reg_ALOAD")
     Object[][] reg_ALOAD() {
-        return new Object[][] { { ALOAD_0, 0 }, { ALOAD_1, 1 }, { ALOAD_2, 2 }, { ALOAD_3, 3 }, { ALOAD_0 - 1, -1 },
-                { ALOAD_3 + 1, -1 }, };
+        return new Object[][] { { ALOAD_0, 0 }, { ALOAD_1, 1 }, { ALOAD_2, 2 }, { ALOAD_3, 3 }, { ALOAD_0 - 1, -1 }, { ALOAD_3 + 1, -1 }, };
     }
 
     @DataProvider(name = "regStore")
     Object[][] regStore() {
-        return new Object[][] { { ASTORE, OPERAND }, { ISTORE, OPERAND }, { LSTORE, OPERAND }, { FSTORE, OPERAND },
-                { DSTORE, OPERAND }, { ASTORE_0, 0 }, { ASTORE_1, 1 }, { ASTORE_2, 2 }, { ASTORE_3, 3 },
-                { ISTORE_0, 0 }, { ISTORE_1, 1 }, { ISTORE_2, 2 }, { ISTORE_3, 3 }, { LSTORE_0, 0 }, { LSTORE_1, 1 },
-                { LSTORE_2, 2 }, { LSTORE_3, 3 }, { FSTORE_0, 0 }, { FSTORE_1, 1 }, { FSTORE_2, 2 }, { FSTORE_3, 3 },
-                { DSTORE_0, 0 }, { DSTORE_1, 1 }, { DSTORE_2, 2 }, { DSTORE_3, 3 }, { Integer.MIN_VALUE, -1 },
-                { Integer.MAX_VALUE, -1 }, };
+        return new Object[][] { { ASTORE, OPERAND }, { ISTORE, OPERAND }, { LSTORE, OPERAND }, { FSTORE, OPERAND }, { DSTORE, OPERAND }, { ASTORE_0, 0 },
+                { ASTORE_1, 1 }, { ASTORE_2, 2 }, { ASTORE_3, 3 }, { ISTORE_0, 0 }, { ISTORE_1, 1 }, { ISTORE_2, 2 }, { ISTORE_3, 3 }, { LSTORE_0, 0 },
+                { LSTORE_1, 1 }, { LSTORE_2, 2 }, { LSTORE_3, 3 }, { FSTORE_0, 0 }, { FSTORE_1, 1 }, { FSTORE_2, 2 }, { FSTORE_3, 3 }, { DSTORE_0, 0 },
+                { DSTORE_1, 1 }, { DSTORE_2, 2 }, { DSTORE_3, 3 }, { Integer.MIN_VALUE, -1 }, { Integer.MAX_VALUE, -1 }, };
     }
 
     @DataProvider(name = "regLoad")
     Object[][] regLoad() {
-        return new Object[][] { { ALOAD, OPERAND }, { ILOAD, OPERAND }, { LLOAD, OPERAND }, { FLOAD, OPERAND },
-                { DLOAD, OPERAND }, { ALOAD_0, 0 }, { ALOAD_1, 1 }, { ALOAD_2, 2 }, { ALOAD_3, 3 }, { ILOAD_0, 0 },
-                { ILOAD_1, 1 }, { ILOAD_2, 2 }, { ILOAD_3, 3 }, { LLOAD_0, 0 }, { LLOAD_1, 1 }, { LLOAD_2, 2 },
-                { LLOAD_3, 3 }, { FLOAD_0, 0 }, { FLOAD_1, 1 }, { FLOAD_2, 2 }, { FLOAD_3, 3 }, { DLOAD_0, 0 },
-                { DLOAD_1, 1 }, { DLOAD_2, 2 }, { DLOAD_3, 3 }, { Integer.MIN_VALUE, -1 }, { Integer.MAX_VALUE, -1 }, };
+        return new Object[][] { { ALOAD, OPERAND }, { ILOAD, OPERAND }, { LLOAD, OPERAND }, { FLOAD, OPERAND }, { DLOAD, OPERAND }, { ALOAD_0, 0 },
+                { ALOAD_1, 1 }, { ALOAD_2, 2 }, { ALOAD_3, 3 }, { ILOAD_0, 0 }, { ILOAD_1, 1 }, { ILOAD_2, 2 }, { ILOAD_3, 3 }, { LLOAD_0, 0 }, { LLOAD_1, 1 },
+                { LLOAD_2, 2 }, { LLOAD_3, 3 }, { FLOAD_0, 0 }, { FLOAD_1, 1 }, { FLOAD_2, 2 }, { FLOAD_3, 3 }, { DLOAD_0, 0 }, { DLOAD_1, 1 }, { DLOAD_2, 2 },
+                { DLOAD_3, 3 }, { Integer.MIN_VALUE, -1 }, { Integer.MAX_VALUE, -1 }, };
     }
 
     @Test(dataProvider = "parameterRegisters")
@@ -153,7 +148,7 @@ public class RegisterUtilsTest {
         Method method = new Method();
         method.setAccessFlags(accessFlags);
 
-        Constant[] cnst = new Constant[] { new ConstantUtf8(signature) };
+        Constant[] cnst = new Constant[] { new ConstantClass(1), new ConstantUtf8("ex/RegisterUtilsTestData"), new ConstantUtf8(signature) };
         ConstantPool cp = new ConstantPool(cnst) {
 
             @Override
@@ -163,7 +158,7 @@ public class RegisterUtilsTest {
 
         };
         method.setConstantPool(cp);
-        method.setSignatureIndex(0);
+        method.setSignatureIndex(2);
 
         int[] regs = RegisterUtils.getParameterRegisters(method);
 
