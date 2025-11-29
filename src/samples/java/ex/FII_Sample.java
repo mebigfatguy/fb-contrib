2d43a1c9af4f2d5b42c2ed3cc9194d8691e55ed4
Dave Brosius <dbrosius@mebigfatguy.com>
2025-11-28 20:42:29 -0500

    comment out sample, for now

--- src/samples/java/ex/FII_Sample.java
+++ src/samples/java/ex/FII_Sample.java
@@ -153,14 +153,14 @@ public class FII_Sample {
     public void put(Function<Map<String, Object>, Map<String, Object>> updateFunction) {
     }
 
-    public class Issue503<P, T> {
-
-        private Function<P, T> valueFunction;
-
-        public T get(P parameter) {
-            return get(() -> valueFunction.apply(parameter));
-        }
-    }
+    /*
+     * public class Issue503<P, T> {
+     * 
+     * private Function<P, T> valueFunction;
+     * 
+     * public T get(P parameter) { return get(() -> valueFunction.apply(parameter));
+     * } }
+     */
 
     public static class Bauble {
 
